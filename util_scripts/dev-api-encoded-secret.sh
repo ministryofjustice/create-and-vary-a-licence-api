7088e12f57e647b3a131be43de9377316e2bdd8c
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2021-07-27 13:13:16 +0100

    CVL-27: Updating gradle springboot plugin plus minor config for local running.

--- util_scripts/dev-api-encoded-secret.sh
+++ util_scripts/dev-api-encoded-secret.sh
@@ -14,6 +14,7 @@
 CLIENT=${1?No encoded client details found}
 AUTH_HOST="https://sign-in-dev.hmpps.service.justice.gov.uk"
 API_HOST="https://api-dev.prison.service.justice.gov.uk"
+COMMUNITY_HOST="https://community-api-secure.test.delius.probation.hmpps.dsd.io"
 
 TOKEN_RESPONSE=$(curl -s -k -d "" -X POST "$AUTH_HOST/auth/oauth/token?grant_type=client_credentials" -H "Authorization: Basic $CLIENT")
 TOKEN=$(echo "$TOKEN_RESPONSE" | jq -er .access_token)
@@ -29,6 +30,16 @@ echo "---------------------------------------------------------------"
 echo $AUTH_TOKEN
 echo "---------------------------------------------------------------"
 
-curl -X GET --location "$API_HOST/api/offenders/A5170DY" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+# curl -X GET --location "$API_HOST/api/offenders/A5170DY" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+# curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X356288/all" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+
+
+# curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X344165/allOffenderManagers" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+curl -X GET --location  "$COMMUNITY_HOST/secure/staff/staffIdentifier/2500012436/managedOffenders?current=true" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+
+
+# curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X356288/allOffenderManagers" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+# curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X356288/contact-summary" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+# curl -X GET --location  "$COMMUNITY_HOST/secure/staff/staffIdentifier/2500002049/managedOffenders?current=true" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 
 # End
