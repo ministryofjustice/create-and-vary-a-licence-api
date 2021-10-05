17102dafbeae254b69eac6ad56e9e21b16b287f2
Stephen McVeigh <smcveigh941@gmail.com>
2021-10-05 10:53:09 +0100

    CVSL-134: Add nomisID, surname, CRN and DOB to licence summary response

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/LicenceSummary.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/LicenceSummary.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceType
+import java.time.LocalDate
 
 @Schema(description = "Response object which summarises a licence")
 data class LicenceSummary(
@@ -14,4 +16,17 @@ data class LicenceSummary(
 
   @Schema(description = "The status of this licence", example = "IN_PROGRESS")
   val licenceStatus: LicenceStatus,
+
+  @Schema(description = "The prison nomis identifier for this offender", example = "A1234AA")
+  val nomisId: String?,
+
+  @Schema(description = "The offender surname", example = "Smith")
+  val surname: String?,
+
+  @Schema(description = "The case reference number (CRN) of this person, from either prison or probation service", example = "X12344")
+  val crn: String?,
+
+  @Schema(description = "The offender's date of birth, from either prison or probation services", example = "12/12/2001")
+  @JsonFormat(pattern = "dd/MM/yyyy")
+  val dateOfBirth: LocalDate?
 )
