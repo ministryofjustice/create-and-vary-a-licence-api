04f0215665fffe639022d8541fe3425f9a2d26ae
Stephen McVeigh <smcveigh941@gmail.com>
2022-01-25 09:52:33 +0000

    CVSL-341: Unit tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/LicenceServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/LicenceServiceTest.kt
@@ -705,14 +705,14 @@ class LicenceServiceTest {
       topupSupervisionExpiryDate = LocalDate.of(2021, 10, 22),
       comFirstName = "Stephen",
       comLastName = "Mills",
-      comUsername = "X12345",
+      comUsername = "smills",
       comStaffId = 12345,
       comEmail = "stephen.mills@nps.gov.uk",
       comTelephone = "0116 2788777",
       probationAreaCode = "N01",
       probationLduCode = "LDU1",
       dateCreated = LocalDateTime.now(),
-      createdByUsername = "X12345",
+      createdByUsername = "smills",
       standardConditions = someEntityStandardConditions,
     )
 
@@ -742,7 +742,7 @@ class LicenceServiceTest {
       prisonDescription = "Moorland (HMP)",
       conditionalReleaseDate = LocalDate.of(2021, 10, 22),
       actualReleaseDate = LocalDate.of(2021, 10, 22),
-      comUsername = "jsmith",
+      comUsername = "smills",
     )
   }
 }
