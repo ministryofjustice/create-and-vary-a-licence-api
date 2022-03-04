036e6216534cc7fd7a1e4c8f7a30ce44bb1e28cc
Stephen McVeigh <smcveigh941@gmail.com>
2022-03-04 16:49:49 +0000

    CVSL-429: Update licence column lengths

---
+++ src/main/resources/migration/common/V15__update_column_size.sql
@@ -0,0 +1,6 @@
+ALTER TABLE licence ALTER COLUMN pnc TYPE varchar(20);
+ALTER TABLE licence ALTER COLUMN crn TYPE varchar(10);
+ALTER TABLE licence ALTER COLUMN booking_no TYPE varchar(10);
+ALTER TABLE licence ALTER COLUMN noms_id TYPE varchar(10);
+ALTER TABLE licence ALTER COLUMN prison_code TYPE varchar(5);
+ALTER TABLE licence ALTER COLUMN prison_description TYPE varchar(100);
