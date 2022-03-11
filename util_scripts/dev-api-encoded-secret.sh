5c2cec16db49ef8127eb6ae15923f00725a67e32
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2022-03-11 14:24:07 +0000

    CVSL-432: Script updates for trying endpoints.

--- util_scripts/dev-api-encoded-secret.sh
+++ util_scripts/dev-api-encoded-secret.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #
-# NAME:  dev-api.sh
 # Script to test APIs in the DEV environment
 # 
 # Parameters:
@@ -8,7 +7,7 @@
 #
 # Example:
 # 
-# $ ./dev-api.sh <encoded-secret>
+# $ ./dev-api-encoded-secret.sh <encoded-secret>
 #
 
 CLIENT=${1?No encoded client details found}
@@ -16,7 +15,12 @@ AUTH_HOST="https://sign-in-dev.hmpps.service.justice.gov.uk"
 API_HOST="https://api-dev.prison.service.justice.gov.uk"
 COMMUNITY_HOST="https://community-api-secure.test.delius.probation.hmpps.dsd.io"
 PRISON_REGISTER_HOST="https://prison-register-dev.hmpps.service.justice.gov.uk"
+
+# For running against DEV
 CVL_API="https://create-and-vary-a-licence-api-dev.hmpps.service.justice.gov.uk"
+
+# For running against localhost:8089
+# CVL_API="http://localhost:8089"
 
 TOKEN_RESPONSE=$(curl -s -k -d "" -X POST "$AUTH_HOST/auth/oauth/token?grant_type=client_credentials" -H "Authorization: Basic $CLIENT")
 TOKEN=$(echo "$TOKEN_RESPONSE" | jq -er .access_token)
@@ -32,29 +36,37 @@ echo "---------------------------------------------------------------"
 echo $AUTH_TOKEN
 echo "---------------------------------------------------------------"
 
+# Prison API
+
 # curl -X GET --location "$API_HOST/api/offenders/A5170DY" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 # curl -X GET --location "$API_HOST/api/offenders/G9786GC" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 # curl -X GET --location "$API_HOST/api/offenders/G1086GO" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+
+# Community API
+
 # curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X356288/all" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
-
-
 # curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X344165/allOffenderManagers" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 # curl -X GET --location  "$COMMUNITY_HOST/secure/staff/staffIdentifier/2500012436/managedOffenders?current=true" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 # curl -X GET --location  "$COMMUNITY_HOST/secure/staff/staffIdentifier/2500012436" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
-
-
 # curl -X GET --location  "$COMMUNITY_HOST/secure/staff/username/lucybennett" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 # curl -X GET --location  "$COMMUNITY_HOST/secure/staff/username/timharrison" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
-
 # curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X356288/allOffenderManagers" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 # curl -X GET --location  "$COMMUNITY_HOST/secure/offenders/crn/X356288/contact-summary" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 # curl -X GET --location  "$COMMUNITY_HOST/secure/staff/staffIdentifier/2500002049/managedOffenders?current=true" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 
 # Prison register
-# curl -v --location  "$PRISON_REGISTER_HOST/prisons/id/PBI" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
-# curl -v --location  "$PRISON_REGISTER_HOST/secure/prisons/id/PBI/offender-management-unit/email-address" -H "Authorization: $AUTH_TOKEN" -H "Accept: text/plain"
 
-# CVL API
-curl -v --location  "$CVL_API/licence/match?prison=MDI&status=APPROVED&nomsId=A7814DY" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+# curl -v -X GET "$PRISON_REGISTER_HOST/prisons" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+# curl -v -X GET "$PRISON_REGISTER_HOST/secure/prisons/id/CFI/offender-management-unit/email-address" -H "Authorization: $AUTH_TOKEN" -H "Accept: text/plain"
+# curl -v -X PUT "$PRISON_REGISTER_HOST/secure/prisons/id/SWI/offender-management-unit/email-address" -d "test@test.com" -H "Authorization: $AUTH_TOKEN" -H "Content-Type: text/plain" -H "Accept: text/plain"
+# curl -v -X GET "$PRISON_REGISTER_HOST/secure/prisons/id/SWI/offender-management-unit/email-address" -H "Authorization: $AUTH_TOKEN" -H "Accept: text/plain"
+# curl -v -X GET "$PRISON_REGISTER_HOST/secure/prisons/id/UKI/offender-management-unit/email-address" -H "Authorization: $AUTH_TOKEN" -H "Accept: text/plain"
+# curl -v -X GET "$PRISON_REGISTER_HOST/secure/prisons/id/UPI/offender-management-unit/email-address" -H "Authorization: $AUTH_TOKEN" -H "Accept: text/plain"
+# curl -v -X GET "$PRISON_REGISTER_HOST/secure/prisons/id/PRI/offender-management-unit/email-address" -H "Authorization: $AUTH_TOKEN" -H "Accept: text/plain"
+
+# Create and vary a licence API
+
+# curl -v -X GET "$CVL_API/licence/match?prison=MDI&status=APPROVED&nomsId=A7814DY" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
+# curl -X GET "$CVL_API/config/pdu-contacts" -H "Authorization: $AUTH_TOKEN" -H "Accept: application/json" | jq .
 
 # End
