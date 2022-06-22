30d3b90e1d4a2321d89e908fa2386aa20e9a7ee3
Matthew Green <matthew.green@digital.justice.gov.uk>
2022-06-22 16:17:09 +0100

    CVSL-603
    Fix lint errors

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/DateFunctionsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/DateFunctionsTest.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
 
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Assertions.assertFalse
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
@@ -10,34 +9,34 @@ class DateFunctionsTest {
   fun `validate null date and actual date`() {
     val date1 = null
     val date2 = LocalDate.of(2021, 10, 22)
-    assertTrue(nullableDatesDiffer(date1, date2))
+    assertThat(nullableDatesDiffer(date1, date2)).isTrue
   }
 
   @Test
   fun `validate actual date and null date`() {
     val date1 = LocalDate.of(2021, 10, 22)
     val date2 = null
-    assertTrue(nullableDatesDiffer(date1, date2))
+    assertThat(nullableDatesDiffer(date1, date2)).isTrue
   }
 
   @Test
   fun `validate two different dates`() {
     val date1 = LocalDate.of(2021, 10, 22)
     val date2 = LocalDate.of(2021, 10, 21)
-    assertTrue(nullableDatesDiffer(date1, date2))
+    assertThat(nullableDatesDiffer(date1, date2)).isTrue
   }
 
   @Test
   fun `validate two identical dates`() {
     val date1 = LocalDate.of(2021, 10, 22)
     val date2 = LocalDate.of(2021, 10, 22)
-    assertFalse(nullableDatesDiffer(date1, date2))
+    assertThat(nullableDatesDiffer(date1, date2)).isFalse
   }
 
   @Test
   fun `validate two null dates`() {
     val date1 = null
     val date2 = null
-    assertFalse(nullableDatesDiffer(date1, date2))
+    assertThat(nullableDatesDiffer(date1, date2)).isFalse
   }
 }
