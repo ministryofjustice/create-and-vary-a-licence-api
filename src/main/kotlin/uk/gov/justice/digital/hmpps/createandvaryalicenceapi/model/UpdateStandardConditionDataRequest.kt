d9fff60da91b92ba313e11b5265d06fb0ccc7c0a
Matthew Green <matthew.green@digital.justice.gov.uk>
2022-07-19 11:03:08 +0100

    CVSL-610 Add endpoint for updating standard conditions against an existing licence.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/UpdateStandardConditionDataRequest.kt
@@ -0,0 +1,14 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model
+
+import io.swagger.v3.oas.annotations.media.Schema
+import javax.validation.constraints.NotNull
+
+data class UpdateStandardConditionDataRequest(
+  @Schema(description = "The list of standard licence conditions from service configuration")
+  @field:NotNull
+  val standardLicenceConditions: List<StandardCondition> = emptyList(),
+
+  @Schema(description = "The list of standard post sentence supervision conditions from service configuration")
+  @field:NotNull
+  val standardPssConditions: List<StandardCondition> = emptyList(),
+)
