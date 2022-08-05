bcc1b6560bfdddcdf6022cbd7cc24d55d04f4fbf
Matthew Green <matthew.green@digital.justice.gov.uk>
2022-08-05 15:57:40 +0100

    CVSL-138 Work in progress

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/LicencePolicyController.kt
@@ -0,0 +1,145 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.LicencePolicyDto
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.LicencePolicyService
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.PolicyChanges
+
+@RestController
+@RequestMapping("/licence-policy", produces = [MediaType.APPLICATION_JSON_VALUE])
+class LicencePolicyController(private val licenceConditionsService: LicencePolicyService) {
+
+  @GetMapping(value = ["/active"])
+  @PreAuthorize("hasAnyRole('SYSTEM_USER', 'CVL_ADMIN')")
+  @ResponseBody
+  @Operation(
+    summary = "Get the active licence policy",
+    description = "Returns the active policy using its unique identifier. " +
+      "Requires ROLE_SYSTEM_USER or ROLE_CVL_ADMIN.",
+    security = [SecurityRequirement(name = "ROLE_SYSTEM_USER"), SecurityRequirement(name = "ROLE_CVL_ADMIN")],
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Licence Policy found",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = LicencePolicyDto::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      )
+    ]
+  )
+  fun getCurrentPolicy(): LicencePolicyDto? {
+    return licenceConditionsService.currentPolicy()
+  }
+
+  @GetMapping(value = ["/version/{version}"])
+  @PreAuthorize("hasAnyRole('SYSTEM_USER', 'CVL_ADMIN')")
+  @ResponseBody
+  @Operation(
+    summary = "Get licence policy be version number",
+    description = "Returns a single policy using its unique identifier. " +
+      "Requires ROLE_SYSTEM_USER or ROLE_CVL_ADMIN.",
+    security = [SecurityRequirement(name = "ROLE_SYSTEM_USER"), SecurityRequirement(name = "ROLE_CVL_ADMIN")],
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Licence Policy found",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = LicencePolicyDto::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "The licence for this ID was not found.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      )
+    ]
+  )
+  fun getPolicyByVersion(@PathVariable("version") version: String): LicencePolicyDto? {
+    return licenceConditionsService.policyByVersion(version)
+  }
+
+  @GetMapping(value = ["/"])
+  @PreAuthorize("hasAnyRole('SYSTEM_USER', 'CVL_ADMIN')")
+  @ResponseBody
+  @Operation(
+    summary = "Get all licence policy versions",
+    description = "Returns a list of policies, active and present" +
+      "Requires ROLE_SYSTEM_USER or ROLE_CVL_ADMIN.",
+    security = [SecurityRequirement(name = "ROLE_SYSTEM_USER"), SecurityRequirement(name = "ROLE_CVL_ADMIN")],
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Licence Policy found",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = LicencePolicyDto::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "The licence for this ID was not found.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      )
+    ]
+  )
+  fun getPolicies(): List<LicencePolicyDto> {
+    return licenceConditionsService.allPolicies()
+  }
+
+  @GetMapping(value = ["/compare/{version1}/{version2}"])
+  @PreAuthorize("hasAnyRole('SYSTEM_USER', 'CVL_ADMIN')")
+  @ResponseBody
+  @Operation(
+    summary = "Get licence policy differences between two versions",
+    description = "Returns a list of added, remove or amended policy conditions between two versions" +
+      "Requires ROLE_SYSTEM_USER or ROLE_CVL_ADMIN.",
+    security = [SecurityRequirement(name = "ROLE_SYSTEM_USER"), SecurityRequirement(name = "ROLE_CVL_ADMIN")],
+  )
+  fun compare(@PathVariable("version1") version1: String, @PathVariable("version2") version2: String): PolicyChanges? {
+    val policy1 = licenceConditionsService.policyByVersion("1.0")
+    val policy2 = licenceConditionsService.policyByVersion("2.0")
+    return licenceConditionsService.compare(policy1, policy2)
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/PolicyFunctions.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/PolicyFunctions.kt
@@ -1,17 +1,15 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
 
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.Ap
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.ILicenceCondition
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.Pss
 
 fun <T : ILicenceCondition> removedConditions(current: List<T>, other: List<T>): List<T> =
-  other.filter { pssElement ->
-    current.findLast { pss -> pss.code == pssElement.code } == null
+  current.filter { pssElement ->
+    other.findLast { pss -> pss.code == pssElement.code } == null
   }
 
 fun <T : ILicenceCondition> addedConditions(current: List<T>, other: List<T>): List<T> =
-  current.filter { pssElement ->
-    other.findLast { pss -> pss.code == pssElement.code } == null
+  other.filter { pssElement ->
+    current.findLast { pss -> pss.code == pssElement.code } == null
   }
 
 fun <T : ILicenceCondition> amendedConditions(
@@ -25,8 +23,3 @@ fun <T : ILicenceCondition> conditionChanges(current: List<T>, other: List<T>) =
   addedConditions(current, other),
   amendedConditions(current, other)
 )
-
-fun policyChanges(
-  apPolicyChanges: ConditionChanges<Ap>,
-  pssPolicyChanges: ConditionChanges<Pss>
-) = PolicyChanges(standardConditions = apPolicyChanges, additionalConditions = pssPolicyChanges)
