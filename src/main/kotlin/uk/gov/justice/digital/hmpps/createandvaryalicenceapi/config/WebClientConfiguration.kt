eb9c935b43c268f586b4a862870641ae383cb5ee
Matthew Green <matthew.green@digital.justice.gov.uk>
2022-09-05 13:05:12 +0100

    CVSL-618 Refactor WebClientConfiguration.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/config/WebClientConfiguration.kt
@@ -3,21 +3,19 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService
-import org.springframework.security.oauth2.client.registration.ClientRegistration
-import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction
-import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class WebClientConfiguration(
   @Value("\${hmpps.auth.url}") private val oauthApiUrl: String,
   @Value("\${hmpps.prison.api.url}") private val prisonApiUrl: String,
-  @Value("\${oauth.client.token-url}") private val oauthTokenUrl: String,
-  @Value("\${oauth.client.id}") private val oauthClientId: String,
-  @Value("\${oauth.client.secret}") private val oauthSecret: String,
 ) {
 
   @Bean
@@ -26,27 +24,32 @@ class WebClientConfiguration(
   }
 
   @Bean
-  fun oauthPrisonClient(): WebClient {
-    val clientRegistryRepo = InMemoryReactiveClientRegistrationRepository(
-      ClientRegistration
-        .withRegistrationId("hmpps-auth")
-        .tokenUri(oauthTokenUrl)
-        .clientId(oauthClientId)
-        .clientSecret(oauthSecret)
-        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-        .build()
-    )
-
-    val clientService = InMemoryReactiveOAuth2AuthorizedClientService(clientRegistryRepo)
-
+  fun authorizedClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+    oAuth2AuthorizedClientService: OAuth2AuthorizedClientService
+  ): OAuth2AuthorizedClientManager? {
+    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
     val authorizedClientManager =
-      AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistryRepo, clientService)
+      AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientService)
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+    return authorizedClientManager
+  }
 
-    val oauthFilter = ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
-    oauthFilter.setDefaultClientRegistrationId("hmpps-auth")
+  @Bean
+  fun oauthPrisonClient(authorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
+    val oauth2Client = ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
+    oauth2Client.setDefaultClientRegistrationId("hmpps-auth")
 
-    return WebClient.builder().baseUrl(prisonApiUrl)
-      .filter(oauthFilter)
-      .build()
+    return WebClient.builder()
+      .baseUrl(prisonApiUrl)
+      .apply(oauth2Client.oauth2Configuration())
+      .exchangeStrategies(
+        ExchangeStrategies.builder()
+          .codecs { configurer ->
+            configurer.defaultCodecs()
+              .maxInMemorySize(-1)
+          }
+          .build()
+      ).build()
   }
 }
