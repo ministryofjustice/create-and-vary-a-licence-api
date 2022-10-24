f4699c737101ade28d24e4ef42eb90cca71cc5e3
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-10-24 11:42:11 +0100

    CVSL-138 Fix migration

--- src/main/resources/migration/common/V22__store_version_on_additional_condition.sql
+++ src/main/resources/migration/common/V22__store_version_on_additional_condition.sql
@@ -2,7 +2,7 @@ ALTER TABLE additional_condition
     ADD COLUMN condition_version VARCHAR(6);
 
 update additional_condition
-    set additional_condition.condition_version = (select licence.version from licence where licence.id = additional_condition.licence_id)
+set condition_version = (select licence.version from licence where licence.id = additional_condition.licence_id)
 where exists(select * from licence where licence.id = additional_condition.licence_id);
 
 ALTER TABLE additional_condition
