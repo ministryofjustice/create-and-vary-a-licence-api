99c189d4932704109c30f761420271daa5f473fe
Matthew Green <93097983+mgreen-mojhub@users.noreply.github.com>
2022-12-05 10:25:37 +0000

    CVSL-738-licence-override-service (#188)
    * CVSL-738-licence-override-service
    
    Licence override service with initial functionality to change allocated status code for a licence. Only allows one status to be used against each CRN number, unless making a licence INACTIVE.
    
    * CVSL-738-licence-override-service
    
    Licence override service with initial functionality to change allocated status code for a licence. Only allows one status to be used against each CRN number, unless making a licence INACTIVE.
    
    * CVSL-738-licence-override-service
    
    Licence override service with initial functionality to change allocated status code for a licence. Only allows one status to be used against each CRN number, unless making a licence INACTIVE.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/request/OverrideLicenceStatusRequest.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request
+
+import io.swagger.v3.oas.annotations.media.Schema
+import org.jetbrains.annotations.NotNull
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
+import javax.validation.constraints.NotEmpty
+
+@Schema(description = "Request object for overriding a licence status")
+data class OverrideLicenceStatusRequest(
+  @Schema(description = "The new status code to assign to the licence")
+  @NotNull
+  val statusCode: LicenceStatus,
+  @Schema(description = "Reason for overriding the licence status")
+  @NotEmpty
+  val reason: String
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/util/LicenceStatus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/util/LicenceStatus.kt
@@ -11,5 +11,23 @@ enum class LicenceStatus {
   VARIATION_IN_PROGRESS,
   VARIATION_SUBMITTED,
   VARIATION_REJECTED,
-  VARIATION_APPROVED,
+  VARIATION_APPROVED;
+
+  companion object {
+    fun lookupLicenceEventByStatus(status: LicenceStatus): LicenceEventType {
+      return when (status) {
+        SUBMITTED -> LicenceEventType.SUBMITTED
+        IN_PROGRESS -> LicenceEventType.BACK_IN_PROGRESS
+        APPROVED -> LicenceEventType.APPROVED
+        ACTIVE -> LicenceEventType.ACTIVATED
+        REJECTED -> LicenceEventType.VARIATION_REFERRED
+        VARIATION_IN_PROGRESS -> LicenceEventType.VARIATION_IN_PROGRESS
+        VARIATION_SUBMITTED -> LicenceEventType.VARIATION_SUBMITTED
+        VARIATION_REJECTED -> LicenceEventType.VARIATION_REFERRED
+        VARIATION_APPROVED -> LicenceEventType.VARIATION_APPROVED
+        INACTIVE -> LicenceEventType.INACTIVE
+        RECALLED -> LicenceEventType.RECALLED
+      }
+    }
+  }
 }
