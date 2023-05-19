a715d52fe25b889a62b2946aefc5fcfe3878eff0
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2023-05-19 13:32:23 +0100

    CVSL-1030 retrieve url from yaml file

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/ComIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/ComIntegrationTest.kt
@@ -4,20 +4,24 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.CommunityApiMockServer
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.User
 
-const val COMMUNITY_API_WIREMOCK_URL = "http://localhost:8093"
+
 
 class ComIntegrationTest : IntegrationTestBase() {
+
+  @Value("\${hmpps.community.api.url}")
+  val communityApiWiremockUrl: String = ""
 
   @Test
   fun `Get team codes for user with valid staff identifier`() {
     communityApiMockServer.stubGetTeamCodesForUser()
 
     val result = webTestClient.get()
-      .uri("$COMMUNITY_API_WIREMOCK_URL/secure/staff/staffIdentifier/123456")
+      .uri("$communityApiWiremockUrl/secure/staff/staffIdentifier/123456")
       .accept(MediaType.APPLICATION_JSON)
       .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
       .exchange()
