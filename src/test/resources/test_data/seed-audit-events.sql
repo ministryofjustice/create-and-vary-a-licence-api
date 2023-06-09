d928cd884a2cfddb49471b65013649bf547912ba
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2023-06-09 14:48:43 +0100

    Revert "CVSL-887 add null values for seed sql for integration test"
    This reverts commit 8291cf99bb46f91a385d5a6197653476e77822c6.

--- src/test/resources/test_data/seed-audit-events.sql
+++ src/test/resources/test_data/seed-audit-events.sql
@@ -1,8 +1,8 @@
-insert into audit_event (id, licence_id, username, full_name, event_type, summary, detail, changes)
+insert into audit_event (id, licence_id, username, full_name, event_type, summary, detail)
 values
-  (1, 1, 'USER', 'Bob Smith', 'USER_EVENT', 'Summary1', 'Detail1', null),
-  (2, 1, 'USER', 'Bob Smith', 'USER_EVENT', 'Summary2', 'Detail2', null),
-  (3, 1, 'USER', 'Bob Smith', 'USER_EVENT', 'Summary3', 'Detail3', null);
+  (1, 1, 'USER', 'Bob Smith', 'USER_EVENT', 'Summary1', 'Detail1'),
+  (2, 1, 'USER', 'Bob Smith', 'USER_EVENT', 'Summary2', 'Detail2'),
+  (3, 1, 'USER', 'Bob Smith', 'USER_EVENT', 'Summary3', 'Detail3');
 
 
 INSERT INTO audit_event (
@@ -13,16 +13,15 @@ username,
 full_name,
 event_type,
 summary,
-detail,
-changes)
+detail)
 VALUES
- (4,1,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for jim smith','ID 1 type AP status APPROVED version 2.0', null),
- (5,2,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for harry hope','ID 2 type AP status INACTIVE version 2.0', null),
- (6,3,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for terry towel','ID 3 type AP status APPROVED version 2.0', null),
- (7,2,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for harry hope','ID 2 type AP status SUBMITTED version 2.0', null),
- (8,3,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for terry towel','ID 3 type AP status APPROVED version 2.0', null),
- (9,5,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner five','ID 5 type AP status SUBMITTED version 2.0', null),
- (10,5,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner five','ID 5 type AP status APPROVED version 2.0', null),
- (11,6,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner six','ID 6 type AP status SUBMITTED version 2.0', null),
- (12,6,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner six','ID 6 type AP status APPROVED version 2.0', null),
- (13,4,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for bob pint','ID 4 type AP status ACTIVE version 2.0', null);
+ (4,1,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for jim smith','ID 1 type AP status APPROVED version 2.0'),
+ (5,2,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for harry hope','ID 2 type AP status INACTIVE version 2.0'),
+ (6,3,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for terry towel','ID 3 type AP status APPROVED version 2.0'),
+ (7,2,'2022-09-08 10:24:22.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for harry hope','ID 2 type AP status SUBMITTED version 2.0'),
+ (8,3,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for terry towel','ID 3 type AP status APPROVED version 2.0'),
+ (9,5,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner five','ID 5 type AP status SUBMITTED version 2.0'),
+ (10,5,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner five','ID 5 type AP status APPROVED version 2.0'),
+ (11,6,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner six','ID 6 type AP status SUBMITTED version 2.0'),
+ (12,6,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for prisoner six','ID 6 type AP status APPROVED version 2.0'),
+ (13,4,'2022-09-10 03:00:00.000','CVL_OMU','Omu User','USER_EVENT','Licence viewed for bob pint','ID 4 type AP status ACTIVE version 2.0');
