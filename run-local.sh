9bcf49efbb30d8b938b368b896126ff61e558d83
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2023-06-15 16:12:29 +0100

    FB change docker compose file and script to automate the building of the API locally

--- run-local.sh
+++ run-local.sh
@@ -15,8 +15,8 @@ export SERVER_PORT=8089
 
 # Client id/secret to access local container-hosted services
 # Matches with the seeded client details in hmpps-auth for its dev profile
-export SYSTEM_CLIENT_ID=create-and-vary-a-licence-admin
-export SYSTEM_CLIENT_SECRET=client_secret
+export SYSTEM_CLIENT_ID=$(kubectl -n create-and-vary-a-licence-api-dev get secrets create-and-vary-a-licence-api -o json  | jq '.data.SYSTEM_CLIENT_ID | @base64d')
+export SYSTEM_CLIENT_SECRET=$(kubectl -n create-and-vary-a-licence-api-dev get secrets create-and-vary-a-licence-api -o json  | jq '.data.SYSTEM_CLIENT_SECRET | @base64d')
 
 # Provide the DB connection details to local container-hosted Postgresql DB
 # Match with the credentials set in create-and-vary-a-licence/docker-compose.yml
@@ -27,10 +27,19 @@ export DB_PASS=create-and-vary-a-licence
 
 # Provide URLs to other local container-based dependent services
 # Match with ports defined in docker-compose.yml
-export HMPPS_AUTH_URL=http://localhost:9090/auth
+export HMPPS_AUTH_URL=https://sign-in-dev.hmpps.service.justice.gov.uk/auth
 
 # Make the connection without specifying the sslmode=verify-full requirement
 export SPRING_DATASOURCE_URL='jdbc:postgresql://${DB_SERVER}/${DB_NAME}'
+
+# Bring up the licences-db container needed for the application
+docker-compose down --remove-orphans && docker-compose pull
+docker-compose -f docker-compose.yml up -d
+echo "Waiting for container to start..."
+until [ "`docker inspect -f {{.State.Health.Status}} licences-db`" == "healthy" ]; do
+    sleep 0.1;
+done;
+echo "Licences database container has been started"
 
 # Run the application with stdout and dev profiles active
 SPRING_PROFILES_ACTIVE=stdout,dev ./gradlew bootRun
