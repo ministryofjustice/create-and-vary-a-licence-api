98668732b2cadd3c313f8e40599bd4c547e46be3
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-06-21 10:33:05 +0100

    CVSL-1127 Adding condition formatter to the backend (#248)
    This also fixes a couple of bugs:
    
    * Incorrect casing of some inputs due to typo - captilise vs capitalised
    
    * Incorrectly capitalise the joining word: or vs Or
    
    This still needs to be hooked up, may duel run for a bit to identify any issues

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/policy/Conditional.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/policy/Conditional.kt
@@ -2,4 +2,6 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.policy
 
 data class Conditional(
   val inputs: List<ConditionalInput>,
-)
+) : HasInputs {
+  override fun getConditionInputs() = inputs.map { it.toInput() }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/policy/Input.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/policy/Input.kt
@@ -1,17 +1,82 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.policy
 
+import com.fasterxml.jackson.annotation.JsonValue
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.AdditionalConditionData
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.policy.InputType.ADDRESS
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.policy.InputType.TEXT
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.Case
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.adjustCase
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.formatAddress
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.formatUsing
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.startsWithVowel
+
+enum class InputType(@JsonValue val description: String) {
+  RADIO("radio"),
+  ADDRESS("address"),
+  TIME_PICKER("timePicker"),
+  DATE_PICKER("datePicker"),
+  FILE_UPLOAD("fileUpload"),
+  TEXT("text"),
+  CHECK("check"),
+}
+
 data class Input(
-  val type: String,
+  override val type: InputType,
   val label: String,
   val name: String,
-  val listType: String? = null,
+  override val listType: String? = null,
   val options: List<Option>? = null,
-  val case: String? = null,
-  val handleIndefiniteArticle: Boolean? = null,
+  override val case: Case? = null,
+  override val handleIndefiniteArticle: Boolean? = null,
   val addAnother: AddAnother? = null,
-  val includeBefore: String? = null,
+  override val includeBefore: String? = null,
   val subtext: String? = null,
-)
+) : FormattingRule
+
+interface FormattingRule {
+  val type: InputType
+  val case: Case?
+  val listType: String?
+  val includeBefore: String?
+  val handleIndefiniteArticle: Boolean?
+
+  companion object {
+    val DEFAULT = object : FormattingRule {
+      override val type = TEXT
+      override val case = null
+      override val listType = "AND"
+      override val includeBefore = null
+      override val handleIndefiniteArticle = null
+    }
+  }
+}
+
+fun FormattingRule.format(data: List<AdditionalConditionData>) = when (data.size) {
+  0 -> ""
+  1 -> formatSingleValue(data.first().dataValue!!)
+  else -> formatMultipleValues(data)
+}
+
+private fun FormattingRule.formatSingleValue(item: String) = item
+  .adjustCase(this.case)
+  .let { this.applyFormattingRules(it) }
+
+private fun FormattingRule.formatMultipleValues(data: List<AdditionalConditionData>) = data
+  .mapNotNull { it.dataValue }
+  .map { it.adjustCase(this.case) }
+  .formatUsing(this.listType ?: "AND")
+  .let { this.applyFormattingRules(it) }
+
+fun FormattingRule.applyFormattingRules(value: String) = value
+  .let { if (this.includeBefore != null) "${this.includeBefore}$it" else it }
+  .let { if (this.type == ADDRESS) formatAddress(it) else it }
+  .let {
+    if (this.handleIndefiniteArticle != null) {
+      if (it.startsWithVowel()) "an $it" else "a $it"
+    } else {
+      it
+    }
+  }
 
 data class AddAnother(
   val label: String,
