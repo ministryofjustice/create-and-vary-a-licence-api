0e93f9bbcd80479da5157d35069650fce0efe91e
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2023-06-21 16:50:20 +0100

    FB Automate Building API Extension (#250)
    * FB one stop script to buiold front and back end containers and run the back end
    
    * FB split script in two one for ui and one for API
    
    * FB comment out prune step
    
    * FB remove readme now we have split the scripts

--- run-local.sh
+++ run-local.sh
@@ -1,6 +1,6 @@
 #
-# This script is used to run the Create and Vary a licence API locally, to interact with
-# existing Postresql, localstack, prison-api and hmpps-auth containers.
+# This script is used to run the Create and Vary a licence API locally, to create the
+# licences-db Postresql container and to interact with localstack, prison-api and hmpps-auth containers from the UI.
 #
 # It runs with a combination of properties from the default spring profile (in application.yaml) and supplemented
 # with the -dev profile (from application-dev.yml). The latter overrides some of the defaults.
@@ -22,8 +22,8 @@ export SYSTEM_CLIENT_SECRET=$(kubectl -n create-and-vary-a-licence-api-dev get s
 # Match with the credentials set in create-and-vary-a-licence/docker-compose.yml
 export DB_SERVER=localhost
 export DB_NAME=create-and-vary-a-licence-db
-export DB_USER=create-and-vary-a-licence
-export DB_PASS=create-and-vary-a-licence
+export DB_USER=cvl
+export DB_PASS=cvl
 
 # Provide URLs to other local container-based dependent services
 # Match with ports defined in docker-compose.yml
@@ -32,17 +32,29 @@ export HMPPS_AUTH_URL=https://sign-in-dev.hmpps.service.justice.gov.uk/auth
 # Make the connection without specifying the sslmode=verify-full requirement
 export SPRING_DATASOURCE_URL='jdbc:postgresql://${DB_SERVER}/${DB_NAME}'
 
-# Bring up the licences-db container needed for the application
-docker compose down --remove-orphans && docker compose pull
+# Bring up the back end containers needed for the application
+echo "Bringing down current containers ..."
+docker compose down --remove-orphans
 
+#Prune existing containers
+#Comment in if you wish to perform a fresh install of all containers where all containers are removed and deleted
+#You will be prompted to continue with the deletion in the terminal
+#docker system prune --all
+
+echo "Pulling back end containers ..."
+docker compose pull
 docker compose -f docker-compose.yml up -d
-echo "Waiting for container to be ready ..."
+
+echo "Waiting for back end containers to be ready ..."
 until [ "`docker inspect -f {{.State.Health.Status}} licences-db`" == "healthy" ]; do
     sleep 0.1;
 done;
-echo "Licences database container is now ready"
+
+echo "Back end containers are now ready"
 
 # Run the application with stdout and dev profiles active
+echo "Starting the API locally"
+
 SPRING_PROFILES_ACTIVE=stdout,dev ./gradlew bootRun
 
 # End
