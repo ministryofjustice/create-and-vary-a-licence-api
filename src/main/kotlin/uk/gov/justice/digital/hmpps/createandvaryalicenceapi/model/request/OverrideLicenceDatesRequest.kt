284906fae1d6030b8d4339713894d3fcd56d2ccb
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2023-06-22 12:22:31 +0100

    Update @schema descriptions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/request/OverrideLicenceDatesRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/request/OverrideLicenceDatesRequest.kt
@@ -8,39 +8,39 @@ import java.time.LocalDate
 @Schema(description = "Request object for overriding licence dates")
 data class OverrideLicenceDatesRequest(
 
-  @Schema(description = "The conditional release date, from prison services", example = "18/06/2022")
+  @Schema(description = "The conditional release date", example = "18/06/2022")
   @JsonFormat(pattern = "dd/MM/yyyy")
   val conditionalReleaseDate: LocalDate? = null,
 
-  @Schema(description = "The actual release date, from prison services", example = "18/07/2022")
+  @Schema(description = "The actual release date", example = "18/07/2022")
   @JsonFormat(pattern = "dd/MM/yyyy")
   val actualReleaseDate: LocalDate? = null,
 
-  @Schema(description = "The sentence start date, from prison services", example = "06/05/2019")
+  @Schema(description = "The sentence start date", example = "06/05/2019")
   @JsonFormat(pattern = "dd/MM/yyyy")
   val sentenceStartDate: LocalDate? = null,
 
-  @Schema(description = "The sentence end date, from prison services", example = "06/05/2023")
+  @Schema(description = "The sentence end date", example = "06/05/2023")
   @JsonFormat(pattern = "dd/MM/yyyy")
   val sentenceEndDate: LocalDate? = null,
 
-  @Schema(description = "The licence start date, from prison services", example = "06/05/2021")
+  @Schema(description = "The licence start date", example = "06/05/2021")
   @JsonFormat(pattern = "dd/MM/yyyy")
   val licenceStartDate: LocalDate? = null,
 
-  @Schema(description = "The licence end date, from prison services", example = "06/05/2023")
+  @Schema(description = "The licence expiry date", example = "06/05/2023")
   @JsonFormat(pattern = "dd/MM/yyyy")
   val licenceExpiryDate: LocalDate? = null,
 
   @Schema(
-    description = "The date when the post sentence supervision period starts, from prison services",
+    description = "The date when the post sentence supervision period starts",
     example = "06/05/2023",
   )
   @JsonFormat(pattern = "dd/MM/yyyy")
   val topupSupervisionStartDate: LocalDate? = null,
 
   @Schema(
-    description = "The date when the post sentence supervision period ends, from prison services",
+    description = "The date when the post sentence supervision period ends",
     example = "06/06/2023",
   )
   @JsonFormat(pattern = "dd/MM/yyyy")
