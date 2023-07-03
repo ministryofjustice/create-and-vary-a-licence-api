f2c7d7a46dc883dc9ecef503d617ce6610847b01
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2023-07-03 08:50:53 +0100

    added logs for job

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/RemoveApConditionsInPssPeriodService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/RemoveApConditionsInPssPeriodService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.jobs
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceRepository
@@ -11,13 +12,20 @@ class RemoveApConditionsInPssPeriodService(
   private val licenceRepository: LicenceRepository,
   private val licenceConditionService: LicenceConditionService,
 ) {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   @Transactional
   fun removeAPConditions() {
+    log.info("Job to removeApConditions in PSS period started")
     val licencesInPSSPeriod = licenceRepository.getAllVariedLicencesInPSSPeriod()
 
     licencesInPSSPeriod?.forEach { licence ->
       val aPConditionIds = licence.additionalConditions.filter { LicenceType.valueOf(it.conditionType!!) == LicenceType.AP }.map { it.id }
       this.licenceConditionService.deleteAdditionalConditions(licence.id, aPConditionIds)
     }
+    log.info("Job removeApConditions in PSS period deleted AP conditions on ${licencesInPSSPeriod.size} licences")
   }
 }
