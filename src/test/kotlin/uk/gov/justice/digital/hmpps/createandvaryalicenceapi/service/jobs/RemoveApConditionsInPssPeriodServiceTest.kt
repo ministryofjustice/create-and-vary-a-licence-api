a7f372f01e1826e5581db951409b6297f7f4245e
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2023-07-25 13:44:02 +0100

    CVSL-465 Adding new job to remove AP conditions in PSS period

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/RemoveApConditionsInPssPeriodServiceTest.kt
@@ -0,0 +1,246 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.AdditionalCondition
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.AdditionalConditionData
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.CommunityOffenderManager
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.Licence
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceRepository
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.jobs.RemoveApConditionsInPssPeriodService
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceType
+import java.time.LocalDate
+import java.time.LocalDateTime
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.Licence as EntityLicence
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.StandardCondition as EntityStandardCondition
+
+class RemoveApConditionsInPssPeriodServiceTest {
+  private val licenceRepository = mock<LicenceRepository>()
+  private val licenceConditionService = mock<LicenceConditionService>()
+
+  private val service = Mockito.spy(
+    RemoveApConditionsInPssPeriodService(
+      licenceRepository,
+      licenceConditionService,
+    ),
+  )
+
+  @BeforeEach
+  fun reset() {
+    val authentication = mock<Authentication>()
+    val securityContext = mock<SecurityContext>()
+
+    whenever(authentication.name).thenReturn("smills")
+    whenever(securityContext.authentication).thenReturn(authentication)
+    SecurityContextHolder.setContext(securityContext)
+
+    reset(
+      licenceRepository,
+      licenceConditionService,
+    )
+  }
+
+  @Test
+  fun `remove AP condition for in PSS period licence`() {
+    whenever(licenceRepository.getAllVariedLicencesInPSSPeriod()).thenReturn(
+      listOf(
+        aLicenceEntity.copy(
+          licenceExpiryDate = LocalDate.now().minusDays(1),
+          topupSupervisionExpiryDate = LocalDate.now().plusDays(1),
+          additionalConditions = listOf(
+            AdditionalCondition(
+              id = 1L,
+              conditionVersion = "1.0",
+              conditionCode = "code",
+              conditionSequence = 5,
+              conditionCategory = "oldCategory",
+              conditionText = "oldText",
+              additionalConditionData = someAdditionalConditionData,
+              licence = aLicenceEntity,
+              conditionType = "AP",
+            ),
+            AdditionalCondition(
+              id = 3L,
+              conditionVersion = "1.0",
+              conditionCode = "code3",
+              conditionSequence = 6,
+              conditionCategory = "pssCategory",
+              conditionText = "pssText",
+              additionalConditionData = someAdditionalConditionData,
+              licence = aLicenceEntity,
+              conditionType = "PSS",
+            ),
+          ),
+        ),
+      ),
+    )
+
+    service.removeAPConditions()
+
+    val licenceEntityCaptor = argumentCaptor<Licence>()
+    val additionalConditionIdCaptor = argumentCaptor<List<Long>>()
+    val standardConditionIdCaptor = argumentCaptor<List<Long>>()
+
+    verify(licenceConditionService, times(1)).deleteConditions(
+      licenceEntityCaptor.capture(),
+      additionalConditionIdCaptor.capture(),
+      standardConditionIdCaptor.capture(),
+    )
+
+    assertThat(licenceEntityCaptor.firstValue.id).isEqualTo(aLicenceEntity.id)
+    assertThat(additionalConditionIdCaptor.allValues[0]).isEqualTo(listOf(1L))
+    assertThat(standardConditionIdCaptor.allValues[0]).isEqualTo(listOf(1L, 2L))
+  }
+
+  @Test
+  fun `remove AP conditions for in PSS period licence`() {
+    whenever(licenceRepository.getAllVariedLicencesInPSSPeriod()).thenReturn(
+      listOf(
+        aLicenceEntity.copy(
+          licenceExpiryDate = LocalDate.now().minusDays(1),
+          topupSupervisionExpiryDate = LocalDate.now().plusDays(1),
+          additionalConditions = listOf(
+            AdditionalCondition(
+              id = 1L,
+              conditionVersion = "1.0",
+              conditionCode = "code",
+              conditionSequence = 5,
+              conditionCategory = "oldCategory",
+              conditionText = "oldText",
+              additionalConditionData = someAdditionalConditionData,
+              licence = aLicenceEntity,
+              conditionType = "AP",
+            ),
+            AdditionalCondition(
+              id = 3L,
+              conditionVersion = "1.0",
+              conditionCode = "code3",
+              conditionSequence = 6,
+              conditionCategory = "pssCategory",
+              conditionText = "pssText",
+              additionalConditionData = someAdditionalConditionData,
+              licence = aLicenceEntity,
+              conditionType = "AP",
+            ),
+          ),
+        ),
+      ),
+    )
+
+    service.removeAPConditions()
+
+    val licenceEntityCaptor = argumentCaptor<Licence>()
+    val additionalConditionIdCaptor = argumentCaptor<List<Long>>()
+    val standardConditionIdCaptor = argumentCaptor<List<Long>>()
+
+    verify(licenceConditionService, times(1)).deleteConditions(
+      licenceEntityCaptor.capture(),
+      additionalConditionIdCaptor.capture(),
+      standardConditionIdCaptor.capture(),
+    )
+
+    assertThat(licenceEntityCaptor.firstValue.id).isEqualTo(aLicenceEntity.id)
+    assertThat(additionalConditionIdCaptor.allValues[0]).isEqualTo(listOf(1L, 3L))
+    assertThat(standardConditionIdCaptor.allValues[0]).isEqualTo(listOf(1L, 2L))
+  }
+
+  private companion object {
+    val someEntityStandardConditions = listOf(
+      EntityStandardCondition(
+        id = 1,
+        conditionCode = "goodBehaviour",
+        conditionSequence = 1,
+        conditionText = "Be of good behaviour",
+        conditionType = "AP",
+        licence = mock(),
+      ),
+      EntityStandardCondition(
+        id = 2,
+        conditionCode = "notBreakLaw",
+        conditionSequence = 2,
+        conditionText = "Do not break any law",
+        conditionType = "AP",
+        licence = mock(),
+      ),
+      EntityStandardCondition(
+        id = 3,
+        conditionCode = "attendMeetings",
+        conditionSequence = 3,
+        conditionText = "Attend meetings",
+        conditionType = "PSS",
+        licence = mock(),
+      ),
+    )
+
+    val aLicenceEntity = EntityLicence(
+      id = 1,
+      typeCode = LicenceType.AP,
+      version = "1.1",
+      statusCode = LicenceStatus.IN_PROGRESS,
+      nomsId = "A1234AA",
+      bookingNo = "123456",
+      bookingId = 54321,
+      crn = "X12345",
+      pnc = "2019/123445",
+      cro = "12345",
+      prisonCode = "MDI",
+      prisonDescription = "Moorland (HMP)",
+      forename = "Bob",
+      surname = "Mortimer",
+      dateOfBirth = LocalDate.of(1985, 12, 28),
+      conditionalReleaseDate = LocalDate.of(2021, 10, 22),
+      actualReleaseDate = LocalDate.of(2021, 10, 22),
+      sentenceStartDate = LocalDate.of(2018, 10, 22),
+      sentenceEndDate = LocalDate.of(2021, 10, 22),
+      licenceStartDate = LocalDate.of(2021, 10, 22),
+      licenceExpiryDate = LocalDate.of(2021, 10, 22),
+      topupSupervisionStartDate = LocalDate.of(2021, 10, 22),
+      topupSupervisionExpiryDate = LocalDate.of(2021, 10, 22),
+      probationAreaCode = "N01",
+      probationAreaDescription = "Wales",
+      probationPduCode = "N01A",
+      probationPduDescription = "Cardiff",
+      probationLauCode = "N01A2",
+      probationLauDescription = "Cardiff South",
+      probationTeamCode = "NA01A2-A",
+      probationTeamDescription = "Cardiff South Team A",
+      dateCreated = LocalDateTime.of(2022, 7, 27, 15, 0, 0),
+      standardConditions = someEntityStandardConditions,
+      responsibleCom = CommunityOffenderManager(
+        staffIdentifier = 2000,
+        username = "smills",
+        email = "testemail@probation.gov.uk",
+        firstName = "X",
+        lastName = "Y",
+      ),
+      createdBy = CommunityOffenderManager(
+        staffIdentifier = 2000,
+        username = "smills",
+        email = "testemail@probation.gov.uk",
+        firstName = "X",
+        lastName = "Y",
+      ),
+    )
+
+    val someAdditionalConditionData = listOf(
+      AdditionalConditionData(
+        id = 1,
+        dataField = "dataField",
+        dataValue = "dataValue",
+        additionalCondition = AdditionalCondition(licence = aLicenceEntity, conditionVersion = "1.0"),
+      ),
+    )
+  }
+}
---
+++ src/test/resources/test_data/seed-licence-id-4.sql
@@ -0,0 +1,264 @@
+insert into licence (
+    id,
+    type_code,
+    version,
+    status_code,
+    noms_id,
+    booking_no,
+    booking_id,
+    crn,
+    pnc,
+    cro,
+    prison_code,
+    prison_description,
+    forename,
+    surname,
+    date_of_birth,
+    conditional_release_date,
+    actual_release_date,
+    sentence_start_date,
+    sentence_end_date,
+    licence_start_date,
+    licence_expiry_date,
+    topup_supervision_expiry_date,
+    probation_area_code,
+    probation_pdu_code,
+    probation_lau_code,
+    probation_team_code,
+    responsible_com_id,
+    created_by_com_id
+)
+values (
+    3,
+    'AP_PSS',
+    '1.0',
+    'VARIATION_IN_PROGRESS',
+    'A1234AA',
+    'BOOKNO',
+    12345,
+    'CRN1',
+    '2015/1234',
+    'CRO1',
+    'MDI',
+    'Moorland (HMP)',
+    'Bob',
+    'Mortimer',
+    '2020-10-25',
+    '2022-02-12',
+    '2022-02-25',
+    '2020-10-11',
+    '2022-02-25',
+    '2022-02-25',
+    CURRENT_DATE - 1,
+    CURRENT_DATE + 1,
+    'N01',
+    'PDU1',
+    'LAU1',
+    'TEAM1',
+    1,
+    1
+ ),
+   (
+       4,
+       'AP_PSS',
+       '1.0',
+       'VARIATION_IN_PROGRESS',
+       'A1234AA',
+       'BOOKNO',
+       12345,
+       'CRN1',
+       '2015/1234',
+       'CRO1',
+       'MDI',
+       'Moorland (HMP)',
+       'Bob',
+       'Mortimer',
+       '2020-10-25',
+       '2022-02-12',
+       '2022-02-25',
+       '2020-10-11',
+       '2022-02-25',
+       '2022-02-25',
+       CURRENT_DATE - 1,
+       CURRENT_DATE + 1,
+       'N01',
+       'PDU1',
+       'LAU1',
+       'TEAM1',
+       1,
+       1),
+       (5,
+        'AP_PSS',
+        '1.0',
+        'VARIATION_IN_PROGRESS',
+        'A1234AA',
+        'BOOKNO',
+        12345,
+        'CRN1',
+        '2015/1234',
+        'CRO1',
+        'MDI',
+        'Moorland (HMP)',
+        'Bob',
+        'Mortimer',
+        '2020-10-25',
+        '2022-02-12',
+        '2022-02-25',
+        '2020-10-11',
+        '2022-02-25',
+        '2022-02-25',
+        CURRENT_DATE + 1,
+        CURRENT_DATE + 1,
+        'N01',
+        'PDU1',
+        'LAU1',
+        'TEAM1',
+        1,
+        1),
+       (6,
+        'AP_PSS',
+        '1.0',
+        'ACTIVE',
+        'A1234AA',
+        'BOOKNO',
+        12345,
+        'CRN1',
+        '2015/1234',
+        'CRO1',
+        'MDI',
+        'Moorland (HMP)',
+        'Bob',
+        'Mortimer',
+        '2020-10-25',
+        '2022-02-12',
+        '2022-02-25',
+        '2020-10-11',
+        '2022-02-25',
+        '2022-02-25',
+        CURRENT_DATE - 1,
+        CURRENT_DATE + 1,
+        'N01',
+        'PDU1',
+        'LAU1',
+        'TEAM1',
+        1,
+        1);
+
+-- Create the AP and PSS standard condition
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (3, 'goodBehaviour', 1, 'Be of generally good behaviour', 'AP');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (3, 'notBreakLaw', 2, 'Do not break the law', 'AP');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (3, 'attendMeetings', 1, 'Attend meetings', 'PSS');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (4, 'goodBehaviour', 1, 'Be of generally good behaviour', 'AP');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (4, 'notBreakLaw', 1, 'Do not break the law', 'PSS');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (4, 'attendMeetings', 2, 'Attend meetings', 'PSS');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (5, 'goodBehaviour', 1, 'Be of generally good behaviour', 'AP');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (5, 'notBreakLaw', 2, 'Do not break the law', 'AP');
+
+insert into standard_condition (licence_id, condition_code, condition_sequence, condition_text, condition_type)
+values (5, 'attendMeetings', 1, 'Attend meetings', 'PSS');
+
+-- Create the AP and PSS additional condition
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (1, 3, '1.0', 'Freedom of movement', 'code-1', 1, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'AP');
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (2, 3, '1.0', 'Freedom of movement', 'code-1', 1, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'PSS');
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (3, 3, '1.0', 'Freedom of movement', 'code-1', 2, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'AP');
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (4, 3, '1.0', 'Freedom of movement', 'code-1', 2, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'PSS');
+
+
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (5, 3, '1.0', 'Freedom of movement', 'code-1', 1, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'AP');
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (6, 4, '1.0', 'Freedom of movement', 'code-1', 2, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'AP');
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (7, 4, '1.0', 'Freedom of movement', 'code-1', 1, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'PSS');
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (8, 5, '1.0', 'Freedom of movement', 'code-1', 3, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'AP');
+
+insert into additional_condition (id, licence_id, condition_version, condition_category, condition_code,
+                                  condition_sequence, condition_text, condition_type)
+values (9, 5, '1.0', 'Freedom of movement', 'code-1', 2, 'Not to enter exclusion zone [EXCLUSION ZONE DESCRIPTION]',
+        'PSS');
+
+-- Create the data for the exclusion zone condition
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (1, 1, 1, 'outOfBoundArea', 'Plymouth town centre');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (2, 1, 2, 'outOfBoundFile', 'Test_map_2021-12-06_112550.pdf');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (3, 2, 1, 'outOfBoundArea', 'Plymouth town centre');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (4, 2, 2, 'outOfBoundFile', 'Test_map_2021-12-06_112550.pdf');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (5, 3, 1, 'outOfBoundArea', 'Plymouth town centre');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (6, 3, 2, 'outOfBoundFile', 'Test_map_2021-12-06_112550.pdf');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (7, 4, 1, 'outOfBoundArea', 'Plymouth town centre');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (8, 4, 2, 'outOfBoundFile', 'Test_map_2021-12-06_112550.pdf');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (9, 5, 1, 'outOfBoundArea', 'Plymouth town centre');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (10, 5, 2, 'outOfBoundFile', 'Test_map_2021-12-06_112550.pdf');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (11, 6, 1, 'outOfBoundArea', 'Plymouth town centre');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (12, 7, 2, 'outOfBoundFile', 'Test_map_2021-12-06_112550.pdf');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (13, 8, 1, 'outOfBoundArea', 'Plymouth town centre');
+
+insert into additional_condition_data (id, additional_condition_id, data_sequence, data_field, data_value)
+values (14, 9, 2, 'outOfBoundFile', 'Test_map_2021-12-06_112550.pdf');
