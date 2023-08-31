dd408777669ef5de6d6a0a2bc751487772e7eb47
Seb Negahban <102144566+SebNegahbanMoJ@users.noreply.github.com>
2023-08-31 08:50:13 +0100

    CVSL-1298 Adding logging to licence activation job to show offence histories (#302)
    * CVSL-1298 Adding logging to licence activation job to show offence histories
    
    * CVSL-1298 Removing unnecessary toString() calls

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/IS91DeterminationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/IS91DeterminationService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.PrisonApiClient
 
@@ -11,6 +12,7 @@ class IS91DeterminationService(
   private companion object IS91Constants {
     val resultCodes = setOf("5500", "4022", "3006", "5502")
     const val offenceCode = "IA99000-001N"
+    private val log = LoggerFactory.getLogger(this::class.java)
   }
 
   fun getIS91AndExtraditionBookingIds(bookingIds: List<Long>): List<Long> {
@@ -20,6 +22,8 @@ class IS91DeterminationService(
         resultCodes.contains(it.secondaryResultCode) ||
         it.offenceCode == offenceCode
     }
+    log.info("All offence histories: $offenceHistories")
+    log.info("IS91 offence histories: $is91AndExtraditionOffenceHistories")
     return is91AndExtraditionOffenceHistories.map { it.bookingId }
   }
 }
