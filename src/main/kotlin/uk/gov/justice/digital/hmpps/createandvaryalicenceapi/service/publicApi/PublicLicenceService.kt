6a643220e1b2b31f0bd44ffeb4707bb2e8afb855
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2023-10-13 14:46:17 +0100

    CVSL-1278 create endpoint to get licences based on prison number (#336)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/publicApi/PublicLicenceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/publicApi/PublicLicenceService.kt
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceRepository
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.resource.publicApi.model.licence.LicenceSummary
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceType
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.resource.publicApi.model.licence.LicenceStatus as PublicLicenceStatus
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.resource.publicApi.model.licence.LicenceType as PublicLicenceType
 
 @Service
 class PublicLicenceService(
@@ -18,32 +15,13 @@ class PublicLicenceService(
   fun getAllLicencesByCrn(crn: String): List<LicenceSummary> {
     val licences = licenceRepository.findAllByCrnAndStatusCodeIn(crn, LicenceStatus.IN_FLIGHT_LICENCES)
 
-    val modelLicenceSummary = licences.map {
-      it.transformToPublicLicenceSummary(it.typeCode.mapToPublicLicenceType(), it.statusCode.mapToPublicLicenceStatus())
-    }
-
-    return modelLicenceSummary
+    return licences.map { it.transformToPublicLicenceSummary() }
   }
 
-  private fun LicenceType.mapToPublicLicenceType() =
-    when {
-      this == LicenceType.AP -> PublicLicenceType.AP
-      this == LicenceType.PSS -> PublicLicenceType.PSS
-      this == LicenceType.AP_PSS -> PublicLicenceType.AP_PSS
-      else -> error("No matching licence type found")
-    }
+  @Transactional
+  fun getAllLicencesByPrisonNumber(prisonNumber: String): List<LicenceSummary> {
+    val licences = licenceRepository.findAllByNomsIdAndStatusCodeIn(prisonNumber, LicenceStatus.IN_FLIGHT_LICENCES)
 
-  private fun LicenceStatus.mapToPublicLicenceStatus() =
-    when {
-      this == LicenceStatus.IN_PROGRESS -> PublicLicenceStatus.IN_PROGRESS
-      this == LicenceStatus.SUBMITTED -> PublicLicenceStatus.SUBMITTED
-      this == LicenceStatus.APPROVED -> PublicLicenceStatus.APPROVED
-      this == LicenceStatus.ACTIVE -> PublicLicenceStatus.ACTIVE
-      this == LicenceStatus.REJECTED -> PublicLicenceStatus.REJECTED
-      this == LicenceStatus.INACTIVE -> PublicLicenceStatus.INACTIVE
-      this == LicenceStatus.VARIATION_IN_PROGRESS -> PublicLicenceStatus.VARIATION_IN_PROGRESS
-      this == LicenceStatus.VARIATION_SUBMITTED -> PublicLicenceStatus.VARIATION_SUBMITTED
-      this == LicenceStatus.VARIATION_APPROVED -> PublicLicenceStatus.VARIATION_APPROVED
-      else -> error("No matching licence status found")
-    }
+    return licences.map { it.transformToPublicLicenceSummary() }
+  }
 }
