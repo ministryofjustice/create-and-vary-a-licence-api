f2528a8c1693264a5fec9dff38caf0ba59ead3a6
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2023-10-26 09:23:27 +0100

    CVSL-1368: Make outcomeReasonCode on CourtEventOutcome nullable.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/prison/CourtEventOutcome.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/prison/CourtEventOutcome.kt
@@ -3,5 +3,5 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison
 data class CourtEventOutcome(
   val bookingId: Long,
   val eventId: Long,
-  val outcomeReasonCode: String,
+  val outcomeReasonCode: String?,
 )
