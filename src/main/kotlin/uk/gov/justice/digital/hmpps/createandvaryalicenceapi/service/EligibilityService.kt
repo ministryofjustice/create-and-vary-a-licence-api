55b2f51354f1972553b52b90a4c864f38696b99c
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2023-11-14 21:38:34 +0000

    CVSL-1412 Exposing reasons for eligibility failure

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/EligibilityService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/EligibilityService.kt
@@ -5,87 +5,96 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.Pris
 import java.time.Clock
 import java.time.LocalDate
 
+typealias EligibilityCheck = (PrisonerSearchPrisoner) -> Boolean
+operator fun EligibilityCheck.not(): EligibilityCheck = { !this(it) }
+infix fun  EligibilityCheck.describedAs(message: String): Pair<EligibilityCheck, String> = this to message
+
 @Service
 class EligibilityService(
   private val clock: Clock,
 ) {
 
+  val checks = listOf(
+    !isPersonParoleEligible() describedAs "is eligible for parole",
+    hasCorrectLegalStatus() describedAs "has incorrect legal status",
+    !isOnIndeterminateSentence() describedAs "is on indeterminate sentence",
+    hasConditionalReleaseDate() describedAs "has no conditional release date",
+    isEligibleIfOnAnExtendedDeterminateSentence() describedAs "is on non-eligible EDS",
+    hasActivePrisonStatus() describedAs "is not active in prison",
+    hasReleaseDateInTheFuture() describedAs "release date in past",
+    !isRecallCase() describedAs "is a recall case",
+  )
+
   fun isEligibleForCvl(prisoner: PrisonerSearchPrisoner): Boolean {
-    return !prisoner.isPersonParoleEligible() &&
-      prisoner.hasCorrectLegalStatus() &&
-      !prisoner.isOnIndeterminateSentence() &&
-      prisoner.hasConditionalReleaseDate() &&
-      prisoner.isEligibleIfOnAnExtendedDeterminateSentence() &&
-      prisoner.hasActivePrisonStatus() &&
-      prisoner.hasEligibleReleaseDate() &&
-      !prisoner.isRecallCase()
+    return getIneligiblityReasons(prisoner).isEmpty()
   }
 
-  private fun PrisonerSearchPrisoner.isPersonParoleEligible(): Boolean {
-    if (this.paroleEligibilityDate != null) {
-      if (this.paroleEligibilityDate.isAfter(LocalDate.now(clock))) {
-        return true
+  fun getIneligiblityReasons(prisoner: PrisonerSearchPrisoner): List<String> {
+    return checks.mapNotNull { (test, message) -> if (!test(prisoner)) message else null }
+  }
+
+
+  private fun isPersonParoleEligible(): EligibilityCheck = early@{
+    if (it.paroleEligibilityDate != null) {
+      if (it.paroleEligibilityDate.isAfter(LocalDate.now(clock))) {
+        return@early true
       }
     }
-    return false
+    return@early false
   }
 
-  private fun PrisonerSearchPrisoner.hasCorrectLegalStatus(): Boolean {
-    return this.legalStatus != "DEAD"
-  }
 
-  private fun PrisonerSearchPrisoner.isOnIndeterminateSentence(): Boolean {
-    return this.indeterminateSentence
-  }
+  private fun hasCorrectLegalStatus(): EligibilityCheck = { it.legalStatus != "DEAD" }
 
-  private fun PrisonerSearchPrisoner.hasConditionalReleaseDate(): Boolean {
-    return this.conditionalReleaseDate != null
-  }
+  private fun isOnIndeterminateSentence(): EligibilityCheck = { it.indeterminateSentence }
 
-  private fun PrisonerSearchPrisoner.isEligibleIfOnAnExtendedDeterminateSentence(): Boolean {
+  private fun hasConditionalReleaseDate(): EligibilityCheck = { it.conditionalReleaseDate != null }
+
+  private fun isEligibleIfOnAnExtendedDeterminateSentence(): EligibilityCheck = early@{
     // If you don’t have a PED, you automatically pass this check as you’re not an EDS case
-    if (this.paroleEligibilityDate == null) {
-      return true
+    if (it.paroleEligibilityDate == null) {
+      return@early true
     }
 
     // if ARD is not between CRD - 4 days and CRD inclusive (to account for bank holidays and weekends), not eligible
-    if (this.confirmedReleaseDate != null) {
-      val dateStart = this.conditionalReleaseDate!!.minusDays(4)
-      if (this.confirmedReleaseDate.isBefore(dateStart) || this.confirmedReleaseDate.isAfter(this.conditionalReleaseDate)) {
-        return false
+    if (it.confirmedReleaseDate != null) {
+      val dateStart = it.conditionalReleaseDate!!.minusDays(4)
+      if (it.confirmedReleaseDate.isBefore(dateStart) || it.confirmedReleaseDate.isAfter(it.conditionalReleaseDate)) {
+        return@early false
       }
     }
 
     // an APD with a PED in the past means they were a successful parole applicant on a later attempt, so not eligible
-    if (this.actualParoleDate != null) {
-      return false
+    if (it.actualParoleDate != null) {
+      return@early false
     }
 
-    return true
+    return@early true
   }
 
-  private fun PrisonerSearchPrisoner.hasActivePrisonStatus() = status?.let {
-    it.startsWith("ACTIVE") || it == "INACTIVE TRN"
-  } ?: false
-
-  private fun PrisonerSearchPrisoner.hasEligibleReleaseDate(): Boolean {
-    val releaseDate = this.confirmedReleaseDate ?: this.conditionalReleaseDate
-
-    return releaseDate!!.isEqual(LocalDate.now(clock)) || releaseDate.isAfter(LocalDate.now(clock))
+  private fun hasActivePrisonStatus(): EligibilityCheck = { prisoner ->
+    prisoner.status?.let {
+      it.startsWith("ACTIVE") || it == "INACTIVE TRN"
+    } ?: false
   }
 
-  private fun PrisonerSearchPrisoner.isRecallCase(): Boolean {
+  private fun hasReleaseDateInTheFuture(): EligibilityCheck = early@{
+    val releaseDate = it.confirmedReleaseDate ?: it.conditionalReleaseDate ?: return@early true
+    releaseDate.isEqual(LocalDate.now(clock)) || releaseDate.isAfter(LocalDate.now(clock))
+  }
+
+  private fun isRecallCase(): EligibilityCheck =  early@{
     // If a CRD but no PRRD it should NOT be treated as a recall
-    if (this.conditionalReleaseDate != null && this.postRecallReleaseDate == null) {
-      return false
+    if (it.conditionalReleaseDate != null && it.postRecallReleaseDate == null) {
+      return@early false
     }
 
-    if (this.conditionalReleaseDate != null && this.postRecallReleaseDate != null) {
+    if (it.conditionalReleaseDate != null && it.postRecallReleaseDate != null) {
       // If the PRRD > CRD - it should be treated as a recall otherwise it is not treated as a recall
-      return this.postRecallReleaseDate.isAfter(this.conditionalReleaseDate)
+      return@early it.postRecallReleaseDate.isAfter(it.conditionalReleaseDate)
     }
 
     // Trust the Nomis recall flag as a fallback position - the above rules should always override
-    return this.recall
+    return@early it.recall
   }
 }
