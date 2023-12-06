b9327b2294603ed10177420c2099c3b2b3fd57b1
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2023-12-06 15:42:49 +0000

    CVSL-1505 add call to api client

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/prison/PhoneDetail.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison
+
+class PhoneDetail(
+  val phoneId: Int,
+  val number: String,
+  val type: String,
+  val ext: String,
+)
