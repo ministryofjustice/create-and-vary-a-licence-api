856fd8422b8652e622ab58e4b86c4e244f356ae8
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-12-06 07:54:01 +0000

    CVSL-1505 Adding CRD and Variation subtypes (#384)
    Co-authored-by: Gurps Singh <60613823+gurps-singh@users.noreply.github.com>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/LicenceActivationServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/LicenceActivationServiceTest.kt
@@ -320,7 +320,6 @@ class LicenceActivationServiceTest {
   }
 
   private val aLicenceEntity = createCrdLicence().copy(
-    kind = LicenceKind.CRD,
     statusCode = LicenceStatus.APPROVED,
     dateOfBirth = LocalDate.of(1985, 12, 28),
     conditionalReleaseDate = LocalDate.of(2021, 10, 22),
