ee9edc78969b3bd241e51d94daac47e3fcc2d7eb
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2023-12-06 14:48:30 +0000

    CVSL-1505 add probation search call to api client (#387)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/ProbationSearchApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/ProbationSearchApiClient.kt
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.typeReference
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.model.request.LicenceCaseloadSearchRequest
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.model.request.OffenderSearchRequest
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.model.request.ProbationSearchSortByRequest
 
 @Component
@@ -34,5 +36,19 @@ class ProbationSearchApiClient(@Qualifier("oauthProbationSearchApiClient") val p
       .block()
 
     return probationOffenderSearchResponse?.content ?: error("Unexpected null response from API")
+  }
+
+  fun searchForPersonOnProbation(
+    nomisId: String,
+  ): OffenderDetail {
+    val probationOffenderSearchResponse = probationSearchApiClient
+      .post()
+      .uri("/search")
+      .bodyValue(OffenderSearchRequest(nomsNumber = nomisId))
+      .accept(MediaType.APPLICATION_JSON)
+      .retrieve()
+      .bodyToMono(typeReference<List<OffenderDetail>>())
+      .block()
+    return probationOffenderSearchResponse?.get(0) ?: error("Unexpected null response from API")
   }
 }
