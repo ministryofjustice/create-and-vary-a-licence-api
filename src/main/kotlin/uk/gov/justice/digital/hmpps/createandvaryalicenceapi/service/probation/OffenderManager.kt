1fbfd021a9e5258c4f98f3c2de25f8eada2dc721
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2023-12-07 12:52:17 +0000

    CVSL-1505 add call to api client (#388)
    * CVSL-1505 add call to api client
    
    * CVSL-1505 change classes to data classes
    
    * CVSL-1505 add additional call to community api

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/OffenderManager.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/OffenderManager.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-class OffenderManager(
+data class OffenderManager(
   @JsonProperty("staff")
   val staffDetail: StaffDetail,
   val active: Boolean,
--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/OtherIds.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/OtherIds.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation
 
-class OtherIds(
+data class OtherIds(
   val crn: String,
   val croNumber: String,
   val pncNumber: String,
