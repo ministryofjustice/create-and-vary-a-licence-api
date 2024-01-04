92def82b292a5d2cafb5ee0dbb1b592aa3f21d3b
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2024-01-04 10:18:29 +0000

    CVSL-1505 creating responsible COM in DB if they have not previously logged in

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/User.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/User.kt
@@ -1,5 +1,14 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation
 
 data class User(
+  val email: String?,
+  val username: String?,
+  val staffIdentifier: Long?,
+  val staff: StaffHuman?,
   val teams: List<Detail>,
+)
+
+data class StaffHuman(
+  val forenames: String?,
+  val surname: String?,
 )
