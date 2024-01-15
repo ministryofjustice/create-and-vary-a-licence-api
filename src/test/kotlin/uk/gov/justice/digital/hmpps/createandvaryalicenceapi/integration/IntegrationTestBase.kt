e29ad84635d2fbfa8aedb1b5220317e3cd18a223
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2024-01-15 17:29:25 +0000

    CVSL-1516 add localstack (#431)
    * CVSL-1515 add localstack
    
    * CVSL-1516 update localstack version
    
    * CVSL-1516 add queue creation config to test base
    
    * CVSL-1516 make changes to circle config
    
    * CVSL-1516 add config to integration test step in circle ci
    
    * CVSL-1516 add localstack tag
    
    * CVSL-1516 add wait for localstack
    
    * CVSL-1516 change executor name back
    
    * CVSL-1516 add correct tags
    
    * CVSL-1516 add correct executor
    
    * CVSL-1516 remove undeeded comment and config timeout for localstack

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/IntegrationTestBase.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.gson.GsonBuilder
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -13,8 +14,13 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.jdbc.SqlMergeMode
 import org.springframework.test.web.reactive.server.WebTestClient
+import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.helpers.JwtAuthHelper
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.OAuthExtension
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.HmppsSqsProperties
+import uk.gov.justice.hmpps.sqs.MissingQueueException
+import uk.gov.justice.hmpps.sqs.MissingTopicException
 
 /*
 ** The abstract parent class for integration tests.
@@ -38,6 +44,13 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremoc
 )
 abstract class IntegrationTestBase {
 
+  @BeforeEach
+  fun `Clear queues`() {
+    domainEventsQueue.sqsClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(domainEventsQueue.queueUrl).build())
+  }
+
+  private val domainEventsQueue by lazy { hmppsQueueService.findByQueueId("domaineventsqueue") ?: throw MissingQueueException("HmppsQueue domaineventsqueue not found") }
+
   @Suppress("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   lateinit var webTestClient: WebTestClient
@@ -48,7 +61,14 @@ abstract class IntegrationTestBase {
   @Autowired
   lateinit var mapper: ObjectMapper
 
+  @Autowired
+  protected lateinit var hmppsQueueService: HmppsQueueService
+
   internal val gson = GsonBuilder().setPrettyPrinting().create()
+
+  fun HmppsSqsProperties.domaineventsTopicConfig() =
+    topics["domainevents"]
+      ?: throw MissingTopicException("domainevents has not been loaded from configuration properties")
 
   internal fun setAuthorisation(
     user: String = "test-client",
