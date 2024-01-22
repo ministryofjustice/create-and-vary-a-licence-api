edfb997cbe971affbb6ea88e0b74c73210aa8f3f
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2024-01-22 18:57:24 +0000

    CVSL-1607 supporting submit for approval for hardstop licences

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/TestData.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/TestData.kt
@@ -1,10 +1,14 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
 
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.AdditionalCondition
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.CommunityOffenderManager
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.CrdLicence
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.HardStopLicence
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.Licence
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.PrisonCaseAdministrator
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.StandardCondition
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.VariationLicence
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.policies.HARD_STOP_CONDITION
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceType
 import java.time.LocalDate
@@ -18,6 +22,20 @@ object TestData {
     email = "testemail@probation.gov.uk",
     firstName = "X",
     lastName = "Y",
+  )
+
+  fun hardStopAdditionalCondition(licence: Licence) = AdditionalCondition(
+    licence = licence,
+    id = 2L,
+    conditionSequence = 1,
+    conditionType = HARD_STOP_CONDITION.type,
+    conditionCode = HARD_STOP_CONDITION.code,
+    conditionText = HARD_STOP_CONDITION.text,
+    expandedConditionText = HARD_STOP_CONDITION.text,
+    conditionVersion = licence.version!!,
+    additionalConditionData = emptyList(),
+    additionalConditionUploadSummary = emptyList(),
+    conditionCategory = HARD_STOP_CONDITION.categoryShort,
   )
 
   fun someEntityStandardConditions(licence: Licence) =
@@ -98,6 +116,60 @@ object TestData {
     ),
   ).let {
     it.copy(standardConditions = someEntityStandardConditions(it))
+  }
+
+  fun createHardStopLicence() = HardStopLicence(
+    id = 1,
+    typeCode = LicenceType.AP,
+    version = "1.1",
+    statusCode = LicenceStatus.IN_PROGRESS,
+    nomsId = "A1234AA",
+    bookingNo = "123456",
+    bookingId = 54321,
+    crn = "X12345",
+    pnc = "2019/123445",
+    cro = "12345",
+    prisonCode = "MDI",
+    prisonDescription = "Moorland (HMP)",
+    forename = "John",
+    surname = "Smith",
+    dateOfBirth = LocalDate.of(1985, 12, 28),
+    conditionalReleaseDate = LocalDate.of(2021, 10, 22),
+    actualReleaseDate = LocalDate.of(2021, 10, 22),
+    sentenceStartDate = LocalDate.of(2018, 10, 22),
+    sentenceEndDate = LocalDate.of(2021, 10, 22),
+    licenceStartDate = LocalDate.of(2021, 10, 22),
+    licenceExpiryDate = LocalDate.of(2021, 10, 22),
+    topupSupervisionStartDate = LocalDate.of(2021, 10, 22),
+    topupSupervisionExpiryDate = LocalDate.of(2021, 10, 22),
+    probationAreaCode = "N01",
+    probationAreaDescription = "Wales",
+    probationPduCode = "N01A",
+    probationPduDescription = "Cardiff",
+    probationLauCode = "N01A2",
+    probationLauDescription = "Cardiff South",
+    probationTeamCode = "NA01A2-A",
+    probationTeamDescription = "Cardiff South Team A",
+    dateCreated = LocalDateTime.of(2022, 7, 27, 15, 0, 0),
+    standardConditions = emptyList(),
+    responsibleCom = CommunityOffenderManager(
+      staffIdentifier = 2000,
+      username = "smills",
+      email = "testemail@probation.gov.uk",
+      firstName = "X",
+      lastName = "Y",
+    ),
+    createdBy = PrisonCaseAdministrator(
+      username = "smills",
+      email = "testemail@probation.gov.uk",
+      firstName = "X",
+      lastName = "Y",
+    ),
+  ).let {
+    it.copy(
+      standardConditions = someEntityStandardConditions(it),
+      additionalConditions = listOf(hardStopAdditionalCondition(it)),
+    )
   }
 
   fun createVariationLicence() = VariationLicence(
