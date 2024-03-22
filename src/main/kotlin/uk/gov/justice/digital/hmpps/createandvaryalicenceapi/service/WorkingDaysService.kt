eb8b0be03395064e16a3b458730946a12ff9660f
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2024-03-22 16:44:51 +0000

    CVSL-1724 add date range and refactor for early release weekends

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/WorkingDaysService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/WorkingDaysService.kt
@@ -7,29 +7,72 @@ import java.time.LocalDate
 @Service
 class WorkingDaysService(private val bankHolidayService: BankHolidayService) {
 
-  fun addWorkingDays(date: LocalDate, workingDays: Int): LocalDate {
-    val bankHolidays = bankHolidayService.getBankHolidaysForEnglandAndWales()
+  fun addWorkingDays(date: LocalDate, workingDays: Int, isEarlyReleaseWeekend: Boolean = false): LocalDate {
     var adjustedDate = date
-    for (i in 0 until workingDays) {
-      adjustedDate = getNextWorkingDay(bankHolidays, adjustedDate)
+    repeat(workingDays) {
+      adjustedDate = getNextWorkingDay(adjustedDate, isEarlyReleaseWeekend)
     }
     return adjustedDate
   }
 
-  fun isWeekend(date: LocalDate): Boolean {
-    val weekend = listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
+  fun subWorkingDays(date: LocalDate, workingDays: Int, isEarlyReleaseWeekend: Boolean = false): LocalDate {
+    var adjustedDate = date
+    repeat(workingDays) {
+      adjustedDate = getPreviousWorkingDay(adjustedDate, isEarlyReleaseWeekend)
+    }
+    return adjustedDate
+  }
+
+  fun getWorkingDaysRange(
+    date: LocalDate,
+    workingDaysRequired: Int,
+    isPastRangeRequired: Boolean = false,
+    isEarlyReleaseWeekend: Boolean = false,
+  ): List<LocalDate> {
+    var adjustedDate = date
+    val workingDays = mutableListOf<LocalDate>()
+
+    repeat(workingDaysRequired) {
+      adjustedDate = if (isPastRangeRequired) {
+        getPreviousWorkingDay(adjustedDate, isEarlyReleaseWeekend)
+      } else {
+        getNextWorkingDay(adjustedDate, isEarlyReleaseWeekend)
+      }
+      workingDays.add(adjustedDate)
+    }
+
+    return workingDays.sorted().toList()
+  }
+
+  fun isWeekend(date: LocalDate, isEarlyReleaseWeekend: Boolean = false): Boolean {
+    val weekend = when (isEarlyReleaseWeekend) {
+      true -> listOf(DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
+      else -> listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
+    }
     return date.dayOfWeek in weekend
   }
 
-  fun isNonWorkingDay(bankHolidays: List<LocalDate>, date: LocalDate): Boolean {
-    return isWeekend(date) || bankHolidays.contains(date)
+  fun isNonWorkingDay(date: LocalDate, isEarlyReleaseWeekend: Boolean = false): Boolean {
+    return isWeekend(date, isEarlyReleaseWeekend) || getBankHolidays().contains(date)
   }
 
-  fun getNextWorkingDay(bankHolidays: List<LocalDate>, date: LocalDate): LocalDate {
+  fun getNextWorkingDay(date: LocalDate, isEarlyReleaseWeekend: Boolean = false): LocalDate {
     var adjustedDate = date.plusDays(1)
-    while (isNonWorkingDay(bankHolidays, adjustedDate)) {
+    while (isNonWorkingDay(adjustedDate, isEarlyReleaseWeekend)) {
       adjustedDate = adjustedDate.plusDays(1)
     }
     return adjustedDate
+  }
+
+  fun getPreviousWorkingDay(date: LocalDate, isEarlyReleaseWeekend: Boolean = false): LocalDate {
+    var adjustedDate = date.minusDays(1)
+    while (isNonWorkingDay(adjustedDate, isEarlyReleaseWeekend)) {
+      adjustedDate = adjustedDate.minusDays(1)
+    }
+    return adjustedDate
+  }
+
+  private fun getBankHolidays(): List<LocalDate> {
+    return bankHolidayService.getBankHolidaysForEnglandAndWales()
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/WorkingDaysServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/WorkingDaysServiceTest.kt
@@ -1,9 +1,8 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.reset
@@ -20,95 +19,242 @@ class WorkingDaysServiceTest {
     whenever(bankHolidayService.getBankHolidaysForEnglandAndWales()).thenReturn(someBankHolidays)
   }
 
-  @Test
-  fun `is date on the weekend`() {
-    val today = LocalDate.of(2024, 3, 23)
-    val isWeekend = service.isWeekend(today)
-    assertTrue(isWeekend)
+  @Nested
+  inner class `weekends` {
+    @Test
+    fun `is date on the weekend`() {
+      val today = LocalDate.of(2024, 3, 23)
+      val isWeekend = service.isWeekend(today)
+      assertThat(isWeekend).isTrue()
+    }
+
+    @Test
+    fun `is date not on the weekend`() {
+      val today = LocalDate.of(2024, 3, 21)
+      val isWeekend = service.isWeekend(today)
+      assertThat(isWeekend).isFalse()
+    }
+
+    @Test
+    fun `is date on the early release weekend`() {
+      val today = LocalDate.of(2024, 3, 22)
+      val isERWeekend = service.isWeekend(today, true)
+      assertThat(isERWeekend).isTrue()
+    }
+
+    @Test
+    fun `is date not on the early release weekend`() {
+      val today = LocalDate.of(2024, 3, 21)
+      val isERWeekend = service.isWeekend(today, true)
+      assertThat(isERWeekend).isFalse()
+    }
   }
 
-  @Test
-  fun `is date not on the weekend`() {
-    val today = LocalDate.of(2024, 3, 21)
-    val isWeekend = service.isWeekend(today)
-    assertFalse(isWeekend)
+  @Nested
+  inner class `is working day or not` {
+    @Test
+    fun `is date a non working day`() {
+      val today = LocalDate.of(2024, 3, 23)
+      val isNonWorkingDay = service.isNonWorkingDay(today)
+      assertThat(isNonWorkingDay).isTrue()
+    }
+
+    @Test
+    fun `is date a working day`() {
+      val today = LocalDate.of(2024, 3, 21)
+      val isNonWorkingDay = service.isNonWorkingDay(today)
+      assertThat(isNonWorkingDay).isFalse()
+    }
+
+    @Test
+    fun `is bank holiday a non working day`() {
+      val today = LocalDate.of(2024, 12, 25)
+      val isBankHoliday = service.isNonWorkingDay(today)
+      assertThat(isBankHoliday).isTrue()
+    }
   }
 
-  @Test
-  fun `is date a non working day`() {
-    val today = LocalDate.of(2024, 3, 23)
-    val isNonWorkingDay = service.isNonWorkingDay(someBankHolidays, today)
-    assertTrue(isNonWorkingDay)
+  @Nested
+  inner class `next working day` {
+    @Test
+    fun `get next working day on a weekday`() {
+      val today = LocalDate.of(2024, 3, 21)
+      val nextWorkingDay = service.getNextWorkingDay(today)
+      assertThat(LocalDate.of(2024, 3, 22)).isEqualTo(nextWorkingDay)
+    }
+
+    @Test
+    fun `get next working day on a Friday`() {
+      val today = LocalDate.of(2024, 3, 22)
+      val nextWorkingDay = service.getNextWorkingDay(today)
+      assertThat(LocalDate.of(2024, 3, 25)).isEqualTo(nextWorkingDay)
+    }
+
+    @Test
+    fun `get next working day on a bank holiday`() {
+      val today = LocalDate.of(2024, 5, 3)
+      val nextWorkingDay = service.getNextWorkingDay(today)
+      assertThat(LocalDate.of(2024, 5, 7)).isEqualTo(nextWorkingDay)
+    }
+
+    @Test
+    fun `get next working day on a multi day bank holiday`() {
+      val today = LocalDate.of(2024, 3, 28)
+      val nextWorkingDay = service.getNextWorkingDay(today)
+      assertThat(LocalDate.of(2024, 4, 2)).isEqualTo(nextWorkingDay)
+    }
   }
 
-  @Test
-  fun `is date a working day`() {
-    val today = LocalDate.of(2024, 3, 21)
-    val isWorkingDay = service.isNonWorkingDay(someBankHolidays, today)
-    assertFalse(isWorkingDay)
+  @Nested
+  inner class `previous working day` {
+    @Test
+    fun `get previous working day on a weekday`() {
+      val today = LocalDate.of(2024, 3, 21)
+      val previousWorkingDay = service.getPreviousWorkingDay(today)
+      assertThat(LocalDate.of(2024, 3, 20)).isEqualTo(previousWorkingDay)
+    }
+
+    @Test
+    fun `get previous working day on a Monday`() {
+      val today = LocalDate.of(2024, 3, 25)
+      val previousWorkingDay = service.getPreviousWorkingDay(today)
+      assertThat(LocalDate.of(2024, 3, 22)).isEqualTo(previousWorkingDay)
+    }
+
+    @Test
+    fun `get previous working day on a bank holiday`() {
+      val today = LocalDate.of(2024, 5, 7)
+      val previousWorkingDay = service.getPreviousWorkingDay(today)
+      assertThat(LocalDate.of(2024, 5, 3)).isEqualTo(previousWorkingDay)
+    }
+
+    @Test
+    fun `get previous working day on a multi day bank holiday`() {
+      val today = LocalDate.of(2024, 4, 2)
+      val previousWorkingDay = service.getPreviousWorkingDay(today)
+      assertThat(LocalDate.of(2024, 3, 28)).isEqualTo(previousWorkingDay)
+    }
   }
 
-  @Test
-  fun `get next working day on a weekday`() {
-    val today = LocalDate.of(2024, 3, 21)
-    val nextWorkingDay = service.getNextWorkingDay(someBankHolidays, today)
-    assertEquals(nextWorkingDay, LocalDate.of(2024, 3, 22))
+  @Nested
+  inner class `add working days` {
+    @Test
+    fun `add working days`() {
+      val today = LocalDate.of(2024, 3, 20)
+      val date = service.addWorkingDays(today, 2)
+      assertThat(LocalDate.of(2024, 3, 22)).isEqualTo(date)
+    }
+
+    @Test
+    fun `add working days on Friday`() {
+      val today = LocalDate.of(2024, 3, 22)
+      val date = service.addWorkingDays(today, 2)
+      assertThat(LocalDate.of(2024, 3, 26)).isEqualTo(date)
+    }
+
+    @Test
+    fun `add working days over Easter bank holiday`() {
+      val today = LocalDate.of(2024, 3, 28)
+      val date = service.addWorkingDays(today, 2)
+      assertThat(LocalDate.of(2024, 4, 3)).isEqualTo(date)
+    }
+
+    @Test
+    fun `add working days over multiple Christmas bank holidays`() {
+      val today = LocalDate.of(2024, 12, 20)
+      val date = service.addWorkingDays(today, 10)
+      assertThat(LocalDate.of(2025, 1, 8)).isEqualTo(date)
+    }
   }
 
-  @Test
-  fun `get next working day on a Friday`() {
-    val today = LocalDate.of(2024, 3, 22)
-    val nextWorkingDay = service.getNextWorkingDay(someBankHolidays, today)
-    assertEquals(nextWorkingDay, LocalDate.of(2024, 3, 25))
+  @Nested
+  inner class `subtract working days` {
+    @Test
+    fun `subtract working days`() {
+      val today = LocalDate.of(2024, 3, 20)
+      val date = service.subWorkingDays(today, 2)
+      assertThat(LocalDate.of(2024, 3, 18)).isEqualTo(date)
+    }
+
+    @Test
+    fun `subtract working days on Monday`() {
+      val today = LocalDate.of(2024, 3, 18)
+      val date = service.subWorkingDays(today, 2)
+      assertThat(LocalDate.of(2024, 3, 14)).isEqualTo(date)
+    }
+
+    @Test
+    fun `subtract working days over Easter bank holiday`() {
+      val today = LocalDate.of(2024, 4, 3)
+      val date = service.subWorkingDays(today, 2)
+      assertThat(LocalDate.of(2024, 3, 28)).isEqualTo(date)
+    }
+
+    @Test
+    fun `subtract working days over multiple Christmas bank holidays`() {
+      val today = LocalDate.of(2025, 1, 8)
+      val date = service.subWorkingDays(today, 10)
+      assertThat(LocalDate.of(2024, 12, 20)).isEqualTo(date)
+    }
   }
 
-  @Test
-  fun `get next working day on a bank holiday`() {
-    val today = LocalDate.of(2024, 5, 3)
-    val nextWorkingDay = service.getNextWorkingDay(someBankHolidays, today)
-    assertEquals(nextWorkingDay, LocalDate.of(2024, 5, 7))
-  }
+  @Nested
+  inner class `working date ranges` {
+    @Test
+    fun `get working days range in the future`() {
+      val today = LocalDate.of(2024, 3, 20)
+      val dates = service.getWorkingDaysRange(today, 2)
+      assertThat(listOf(LocalDate.of(2024, 3, 21), LocalDate.of(2024, 3, 22))).isEqualTo(dates)
+    }
 
-  @Test
-  fun `get next working day on a multi day bank holiday`() {
-    val today = LocalDate.of(2024, 3, 28)
-    val nextWorkingDay = service.getNextWorkingDay(someBankHolidays, today)
-    assertEquals(nextWorkingDay, LocalDate.of(2024, 4, 2))
-  }
+    @Test
+    fun `get working days range in the past`() {
+      val today = LocalDate.of(2024, 3, 20)
+      val dates = service.getWorkingDaysRange(today, 2, true)
+      assertThat(listOf(LocalDate.of(2024, 3, 18), LocalDate.of(2024, 3, 19))).isEqualTo(dates)
+    }
 
-  @Test
-  fun `is bank holiday a non working day`() {
-    val today = LocalDate.of(2024, 12, 25)
-    val isBankHoliday = service.isNonWorkingDay(someBankHolidays, today)
-    assertTrue(isBankHoliday)
-  }
+    @Test
+    fun `get working days range in the future on Friday`() {
+      val today = LocalDate.of(2024, 3, 22)
+      val dates = service.getWorkingDaysRange(today, 2)
+      assertThat(listOf(LocalDate.of(2024, 3, 25), LocalDate.of(2024, 3, 26))).isEqualTo(dates)
+    }
 
-  @Test
-  fun `add working days`() {
-    val today = LocalDate.of(2024, 3, 20)
-    val date = service.addWorkingDays(today, 2)
-    assertEquals(date, LocalDate.of(2024, 3, 22))
-  }
+    @Test
+    fun `get working days range in the past on Monday`() {
+      val today = LocalDate.of(2024, 3, 25)
+      val dates = service.getWorkingDaysRange(today, 2, true)
+      assertThat(listOf(LocalDate.of(2024, 3, 21), LocalDate.of(2024, 3, 22))).isEqualTo(dates)
+    }
 
-  @Test
-  fun `add working days on Friday`() {
-    val today = LocalDate.of(2024, 3, 22)
-    val date = service.addWorkingDays(today, 2)
-    assertEquals(date, LocalDate.of(2024, 3, 26))
-  }
+    @Test
+    fun `get working days range in the future over a bank holiday`() {
+      val today = LocalDate.of(2024, 5, 3)
+      val dates = service.getWorkingDaysRange(today, 2)
+      assertThat(listOf(LocalDate.of(2024, 5, 7), LocalDate.of(2024, 5, 8))).isEqualTo(dates)
+    }
 
-  @Test
-  fun `add working days on Easter bank holiday`() {
-    val today = LocalDate.of(2024, 3, 28)
-    val date = service.addWorkingDays(today, 2)
-    assertEquals(date, LocalDate.of(2024, 4, 3))
-  }
+    @Test
+    fun `get working days range in the past over a bank holiday`() {
+      val today = LocalDate.of(2024, 5, 7)
+      val dates = service.getWorkingDaysRange(today, 2, true)
+      assertThat(listOf(LocalDate.of(2024, 5, 2), LocalDate.of(2024, 5, 3))).isEqualTo(dates)
+    }
 
-  @Test
-  fun `add working days on multiple Christmas bank holidays`() {
-    val today = LocalDate.of(2024, 12, 20)
-    val date = service.addWorkingDays(today, 10)
-    assertEquals(date, LocalDate.of(2025, 1, 8))
+    @Test
+    fun `get working days range in the future over a multi bank holiday`() {
+      val today = LocalDate.of(2024, 3, 28)
+      val dates = service.getWorkingDaysRange(today, 2)
+      assertThat(listOf(LocalDate.of(2024, 4, 2), LocalDate.of(2024, 4, 3))).isEqualTo(dates)
+    }
+
+    @Test
+    fun `get working days range in the past over a multi bank holiday`() {
+      val today = LocalDate.of(2024, 4, 2)
+      val dates = service.getWorkingDaysRange(today, 2, true)
+      assertThat(listOf(LocalDate.of(2024, 3, 27), LocalDate.of(2024, 3, 28))).isEqualTo(dates)
+    }
   }
 
   private companion object {
