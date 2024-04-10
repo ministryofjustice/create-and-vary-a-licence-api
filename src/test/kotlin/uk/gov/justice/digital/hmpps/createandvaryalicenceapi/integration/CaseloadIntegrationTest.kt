dfe722d41a426b8749dfd9495b923a7fad374699
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-04-10 18:30:18 +0100

    CVSL-1786 Adding proxy method to prisoner offender search
    This proxies the method to find all prisoners with a certain release date in a set of given prisons and enriches it with some cvl derived info around hard stop dates

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/CaseloadIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/CaseloadIntegrationTest.kt
@@ -11,13 +11,17 @@ import org.springframework.http.HttpStatus.OK
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.http.MediaType.APPLICATION_JSON
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.GovUkMockServer
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.PrisonerSearchMockServer
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.CaseloadItem
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.PrisonerNumbers
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.ReleaseDateSearch
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceType
+import java.time.LocalDate
 
 private const val GET_PRISONER = "/prisoner-search/nomisid/A1234AA"
 private const val FIND_PRISONERS = "/prisoner-search/prisoner-numbers"
+private const val FIND_PRISONERS_BY_RELEASE_DATE = "/prisoner-search/release-date-by-prison?size=2000"
 
 class CaseloadIntegrationTest : IntegrationTestBase() {
 
@@ -157,19 +161,80 @@ class CaseloadIntegrationTest : IntegrationTestBase() {
     }
   }
 
+  @Nested
+  inner class GetPrisonersByReleaseDate {
+    val releaseDateSearch = ReleaseDateSearch(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 2), setOf("MDI"))
+
+    @Test
+    fun `Get forbidden (403) when incorrect roles are supplied`() {
+      val result = webTestClient.post()
+        .uri(FIND_PRISONERS_BY_RELEASE_DATE)
+        .bodyValue(releaseDateSearch)
+        .accept(APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_CVL_WRONG ROLE")))
+        .exchange()
+        .expectStatus().isForbidden
+        .expectStatus().isEqualTo(FORBIDDEN.value())
+        .expectBody(ErrorResponse::class.java)
+        .returnResult().responseBody
+
+      assertThat(result?.userMessage).contains("Access Denied")
+    }
+
+    @Test
+    fun `Unauthorized (401) when no token is supplied`() {
+      webTestClient.post()
+        .uri(FIND_PRISONERS_BY_RELEASE_DATE)
+        .bodyValue(releaseDateSearch)
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isEqualTo(UNAUTHORIZED.value())
+    }
+
+    @Test
+    fun success() {
+      prisonerSearchApiMockServer.stubSearchPrisonersByReleaseDate()
+
+      val caseload = webTestClient.post()
+        .uri(FIND_PRISONERS_BY_RELEASE_DATE)
+        .bodyValue(releaseDateSearch)
+        .accept(APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
+        .exchange()
+        .expectStatus().isEqualTo(OK.value())
+        .expectHeader().contentType(APPLICATION_JSON)
+        .expectBodyList(CaseloadItem::class.java)
+        .returnResult().responseBody!!
+
+      assertThat(caseload).hasSize(5)
+      with(caseload.first()) {
+        assertThat(prisoner).isNotNull()
+        with(cvl) {
+          assertThat(licenceType).isEqualTo(LicenceType.AP)
+          assertThat(hardStopDate).isNotNull
+          assertThat(hardStopWarningDate).isNotNull
+        }
+      }
+    }
+  }
+
   private companion object {
     val prisonerSearchApiMockServer = PrisonerSearchMockServer()
+    val govUkMockServer = GovUkMockServer()
 
     @JvmStatic
     @BeforeAll
     fun startMocks() {
       prisonerSearchApiMockServer.start()
+      govUkMockServer.start()
+      govUkMockServer.stubGetBankHolidaysForEnglandAndWales()
     }
 
     @JvmStatic
     @AfterAll
     fun stopMocks() {
       prisonerSearchApiMockServer.stop()
+      govUkMockServer.stop()
     }
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/CaseloadServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/CaseloadServiceTest.kt
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
@@ -28,8 +29,10 @@ class CaseloadServiceTest {
   fun reset() {
     reset(prisonerSearchApiClient)
     whenever(prisonerSearchApiClient.searchPrisonersByNomisIds(any())).thenReturn(listOf(prisonerSearchResult()))
+    whenever(prisonerSearchApiClient.searchPrisonersByReleaseDate(any(), any(), any())).thenReturn(listOf(prisonerSearchResult()))
     whenever(releaseDateService.getHardStopDate(any())).thenReturn(LocalDate.of(2023, 10, 12))
     whenever(releaseDateService.getHardStopWarningDate(any())).thenReturn(LocalDate.of(2023, 10, 11))
+    whenever(releaseDateService.isInHardStopPeriod(any(), anyOrNull())).thenReturn(true)
   }
 
   @Test
@@ -42,6 +45,7 @@ class CaseloadServiceTest {
           licenceType = LicenceType.AP,
           hardStopDate = LocalDate.of(2023, 10, 12),
           hardStopWarningDate = LocalDate.of(2023, 10, 11),
+          isInHardStopPeriod = true,
         ),
         prisoner = Prisoner(
           prisonerNumber = "A1234AA",
@@ -93,6 +97,7 @@ class CaseloadServiceTest {
           licenceType = LicenceType.AP,
           hardStopDate = LocalDate.of(2023, 10, 12),
           hardStopWarningDate = LocalDate.of(2023, 10, 11),
+          isInHardStopPeriod = true,
         ),
         prisoner = Prisoner(
           prisonerNumber = "A1234AA",
@@ -139,8 +144,58 @@ class CaseloadServiceTest {
   fun getPrisonerFailsToFind() {
     whenever(prisonerSearchApiClient.searchPrisonersByNomisIds(any())).thenReturn(emptyList())
 
-    assertThatThrownBy { service.getPrisoner("A1234AA") }
-      .isInstanceOf(EntityNotFoundException::class.java)
-      .hasMessage("A1234AA")
+    assertThatThrownBy { service.getPrisoner("A1234AA") }.isInstanceOf(EntityNotFoundException::class.java).hasMessage("A1234AA")
+  }
+
+  @Test
+  fun getPrisonersByReleaseDate() {
+    val response = service.getPrisonersByReleaseDate(LocalDate.of(2023, 1, 2), LocalDate.of(2023, 1, 4), setOf("MDI"))
+    assertThat(response).containsExactly(
+      CaseloadItem(
+        cvl = CvlFields(
+          licenceType = LicenceType.AP,
+          hardStopDate = LocalDate.of(2023, 10, 12),
+          hardStopWarningDate = LocalDate.of(2023, 10, 11),
+          isInHardStopPeriod = true,
+        ),
+        prisoner = Prisoner(
+          prisonerNumber = "A1234AA",
+          pncNumber = null,
+          croNumber = null,
+          bookingId = "123456",
+          bookNumber = "12345A",
+          firstName = "Bob",
+          middleNames = null,
+          lastName = "Mortimar",
+          dateOfBirth = LocalDate.of(1985, 12, 28),
+          status = "ACTIVE IN",
+          inOutStatus = null,
+          prisonId = "MDI",
+          prisonName = null,
+          legalStatus = "SENTENCED",
+          imprisonmentStatus = null,
+          imprisonmentStatusDescription = null,
+          mostSeriousOffence = "Robbery",
+          recall = false,
+          indeterminateSentence = false,
+          sentenceStartDate = LocalDate.of(2018, 10, 22),
+          releaseDate = LocalDate.of(2021, 10, 22),
+          confirmedReleaseDate = LocalDate.of(2021, 10, 22),
+          sentenceExpiryDate = LocalDate.of(2021, 10, 22),
+          licenceExpiryDate = LocalDate.of(2021, 10, 22),
+          homeDetentionCurfewEligibilityDate = null,
+          homeDetentionCurfewActualDate = null,
+          homeDetentionCurfewEndDate = null,
+          topupSupervisionStartDate = null,
+          topupSupervisionExpiryDate = LocalDate.of(2021, 10, 22),
+          paroleEligibilityDate = null,
+          postRecallReleaseDate = null,
+          conditionalReleaseDate = LocalDate.of(2021, 10, 22),
+          actualParoleDate = null,
+          releaseOnTemporaryLicenceDate = null,
+        ),
+      ),
+    )
+    verify(prisonerSearchApiClient).searchPrisonersByReleaseDate(LocalDate.of(2023, 1, 2), LocalDate.of(2023, 1, 4), setOf("MDI"))
   }
 }
