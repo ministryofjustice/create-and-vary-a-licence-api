7639b66daec59fe36e3c8e0a09e7b10a3cd95e14
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2024-05-28 12:24:39 +0100

    CVSL-1876-Bug-Amend-hard-stop-rules

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/NotifyAttentionNeededLicencesService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/NotifyAttentionNeededLicencesService.kt
@@ -13,7 +13,7 @@ import java.time.LocalDate
 
 @Service
 class NotifyAttentionNeededLicencesService(
-  @Value("\${attentionNeededLicencesEmailAddress}") private val emailAddress: String,
+  @Value("\${internalEmailAddress}") private val emailAddress: String,
   private val licenceRepository: LicenceRepository,
   private val prisonerSearchApiClient: PrisonerSearchApiClient,
   private val notifyService: NotifyService,
