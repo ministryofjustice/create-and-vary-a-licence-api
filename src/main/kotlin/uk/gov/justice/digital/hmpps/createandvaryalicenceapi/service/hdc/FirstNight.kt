d3223b662418b13e134fc0d2dd2e9c69be9d0130
Louise N <louise.norris@digital.justice.gov.uk>
2024-09-06 09:57:35 +0100

    Cvsl 2035 alter fromUntil variable name (#587)
    * CVSL-2035 alter postcode variable name
    
    * CVSL-2035 alter postcode variable name in integration test
    
    * CVSL-2035 alter fromUntil variable name

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/hdc/FirstNight.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/hdc/FirstNight.kt
@@ -2,5 +2,5 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.hdc
 
 data class FirstNight(
   val firstNightFrom: String,
-  val firstNightTo: String,
+  val firstNightUntil: String,
 )
