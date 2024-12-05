e3ac6909a5f11fcc423fd00d00ad1579b24af895
justin gardiner <justin.gardiner@digital.justice.gov.uk>
2024-12-05 11:34:40 +0000

    Dependency updates (#612)
    * Update gradle version and some dependencies. Fix using === instead of == when comparing Long's.
    
    * More dependency updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
-  kotlin("plugin.spring") version "2.0.10"
-  kotlin("plugin.jpa") version "2.0.10"
-  id("io.gitlab.arturbosch.detekt") version "1.23.6"
+  kotlin("plugin.spring") version "2.0.21"
+  kotlin("plugin.jpa") version "2.0.21"
+  id("io.gitlab.arturbosch.detekt") version "1.23.7"
 }
 
 configurations {
@@ -18,7 +18,7 @@ val integrationTest = task<Test>("integrationTest") {
 tasks.register<Copy>("installLocalGitHook") {
   from(File(rootProject.rootDir, ".scripts/pre-commit"))
   into(File(rootProject.rootDir, ".git/hooks"))
-  fileMode = "755".toInt(radix = 8)
+  filePermissions { unix(755) }
 }
 
 tasks.named<Test>("integrationTest") {
@@ -48,10 +48,10 @@ dependencies {
   implementation("org.springframework.security:spring-security-config:6.3.2")
 
   // GOVUK Notify:
-  implementation("uk.gov.service.notify:notifications-java-client:5.1.0-RELEASE")
+  implementation("uk.gov.service.notify:notifications-java-client:5.2.1-RELEASE")
 
   // PDF Box - for processing MapMaker file upload to get image / text for exclusion zone
-  implementation("org.apache.pdfbox:pdfbox:3.0.2")
+  implementation("org.apache.pdfbox:pdfbox:3.0.3")
   implementation("org.apache.pdfbox:jbig2-imageio:3.0.4")
 
   // Database dependencies
@@ -60,10 +60,10 @@ dependencies {
 
   implementation("com.google.code.gson:gson:2.11.0")
   implementation("io.arrow-kt:arrow-core:1.2.4")
-  implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.8.1")
+  implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.0")
 
   // SQS/SNS dependencies
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.2.0")
 
   // OpenAPI
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
@@ -71,12 +71,12 @@ dependencies {
   // Test dependencies
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
-  testImplementation("io.jsonwebtoken:jjwt-api:0.12.5")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
+  testImplementation("io.jsonwebtoken:jjwt-api:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-orgjson:0.12.6")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
-  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.21")
+  testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.1.24")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("com.h2database:h2")
