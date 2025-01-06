888652ba19590e6cd335518f91a6072ed53fddf6
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-01-06 15:47:56 +0000

    CVSL-2402 updating dependencies (#622)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/UpdateSentenceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/UpdateSentenceIntegrationTest.kt
@@ -9,8 +9,8 @@ import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.context.jdbc.Sql
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.GovUkMockServer
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.PrisonApiMockServer
@@ -36,7 +36,7 @@ class UpdateSentenceIntegrationTest : IntegrationTestBase() {
   @Autowired
   lateinit var licenceEventRepository: LicenceEventRepository
 
-  @MockBean
+  @MockitoBean
   private lateinit var eventsPublisher: OutboundEventsPublisher
 
   @BeforeEach
