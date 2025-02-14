bc77fb7635b40b6a7b88368ddc1d940ee2d5a7fc
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2025-02-14 13:05:48 +0000

    CVSL-2505 add integration test seed

--- src/test/resources/test_data/seed-licences-for-deactivate-licences-past-release-date.sql
+++ src/test/resources/test_data/seed-licences-for-deactivate-licences-past-release-date.sql
@@ -17,5 +17,6 @@ values (1, 'CRD', '1.0', 1, 1, 'AP', 'G7285UT', 'IN_PROGRESS', 456, current_date
        (5, 'CRD', '1.0', 1, 1, 'AP', 'G4169UO', 'SUBMITTED', 432, current_date + 1, '1.1', 3),
        (6, 'CRD', '1.0', 1, 1, 'AP', 'G4169UO', 'SUBMITTED', 432, current_date - 1, '1.1', 3),
        (7, 'HARD_STOP', '1.0', 1, 1, 'AP', 'G4169UO', 'SUBMITTED', 432, current_date - 1, '1.1', 3),
-       (8, 'CRD', '1.0', 1, 1, 'AP', 'G4169UO', 'TIMED_OUT', 432, current_date - 1, '1.1', 3);
+       (8, 'CRD', '1.0', 1, 1, 'AP', 'G4169UO', 'TIMED_OUT', 432, current_date - 1, '1.1', 3),
+       (9, 'HDC', '1.0', 1, 1, 'AP', 'A1234BC', 'IN_PROGRESS', 123, current_date - 1, '1.1', null);
 
