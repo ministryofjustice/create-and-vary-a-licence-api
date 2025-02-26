156de7c06404260c15aa1bd2b19e5fbf79ee9d08
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2025-02-26 16:48:05 +0000

    CVSL-2268 Fixing spec

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/LsdRecalculationServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/LsdRecalculationServiceTest.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
 
-import LsdRecalculationService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
