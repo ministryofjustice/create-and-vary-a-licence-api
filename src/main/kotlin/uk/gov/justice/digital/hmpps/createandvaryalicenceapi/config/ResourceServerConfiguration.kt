9a692c784b90aae183304053382ce01521859148
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-03-03 09:05:52 +0000

    CVSL-2478 Enabling async
    Job keeps failing as it times out as async is not enabled

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/config/ResourceServerConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -20,6 +21,7 @@ import org.springframework.security.web.SecurityFilterChain
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 @EnableCaching
+@EnableAsync
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 class ResourceServerConfiguration {
 
