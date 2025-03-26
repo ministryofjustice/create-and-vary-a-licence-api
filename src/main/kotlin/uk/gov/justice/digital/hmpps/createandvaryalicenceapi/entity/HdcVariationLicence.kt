7f01e99e89c026e6982a4fcc246206e68b1d318c
Louise Norris <louise.norris@digital.justice.gov.uk>
2025-03-26 15:43:47 +0000

    CVSL-2560 lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/entity/HdcVariationLicence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/entity/HdcVariationLicence.kt
@@ -1,6 +1,14 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.CascadeType
+import jakarta.persistence.DiscriminatorValue
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+import jakarta.persistence.OneToOne
+import jakarta.persistence.OrderBy
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.AppointmentPersonType
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.AppointmentTimeType
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceKind
