3c8a6cca6777757be6e0ddc933290c6e537bdf14
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-04-29 13:38:30 +0100

    Revert "Revert "PI-2987 Migrate Delius search endpoints to integration service"" (#709)
    * Revert "Revert "PI-2987 Migrate Delius search endpoints to integration servic…"
    
    This reverts commit e5aee6c83b9bfeeb7c90d3469bc70deb375dba1a.
    
    * use batch

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/CaseloadResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/CaseloadResponse.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation
 
+import org.springframework.data.web.PagedModel
+
 data class CaseloadResponse(
   val content: List<CaseloadResult>,
+  val page: PagedModel.PageMetadata? = null,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/DeliusApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/probation/DeliusApiClient.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation
 
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
@@ -11,10 +12,34 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.typeReference
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.Batching.batchRequests
 
 @Component
-class DeliusApiClient(@Qualifier("oauthDeliusApiClient") val communityApiClient: WebClient) {
+class DeliusApiClient(@Qualifier("oauthDeliusApiClient") val deliusApiWebClient: WebClient) {
   companion object {
     private const val STAFF_EMAIL_BATCH = 500
     private const val STAFF_USERNAME_BATCH = 500
+    private const val PROBATION_CASE_BATCH_SIZE = 500
+    const val CASELOAD_PAGE_SIZE = 2000
+  }
+
+  fun getProbationCase(crnOrNomisId: String): ProbationCase = deliusApiWebClient
+    .get()
+    .uri("/probation-case/{crnOrNomisId}", crnOrNomisId)
+    .accept(MediaType.APPLICATION_JSON)
+    .retrieve()
+    .bodyToMono(ProbationCase::class.java)
+    .block() ?: error("Unexpected null response from API")
+
+  fun getProbationCases(
+    crnsOrNomisIds: List<String>,
+    batchSize: Int = PROBATION_CASE_BATCH_SIZE,
+  ): List<ProbationCase> = batchRequests(batchSize, crnsOrNomisIds) { batch ->
+    deliusApiWebClient
+      .post()
+      .uri("/probation-case")
+      .bodyValue(batch)
+      .accept(MediaType.APPLICATION_JSON)
+      .retrieve()
+      .bodyToMono(typeReference<List<ProbationCase>>())
+      .block()
   }
 
   fun getTeamsCodesForUser(staffIdentifier: Long): List<String> {
@@ -23,37 +48,47 @@ class DeliusApiClient(@Qualifier("oauthDeliusApiClient") val communityApiClient:
       ?: error("Unexpected null response from API")
   }
 
-  fun getStaffByIdentifier(staffIdentifier: Long): User? = communityApiClient
+  fun getStaffByIdentifier(staffIdentifier: Long): User? = deliusApiWebClient
     .get()
-    .uri("/staff/byid/$staffIdentifier", staffIdentifier)
+    .uri("/staff/byid/{staffIdentifier}", staffIdentifier)
     .accept(MediaType.APPLICATION_JSON)
     .retrieve()
     .bodyToMono(User::class.java)
     .block()
 
-  fun getOffenderManager(crn: String): CommunityOrPrisonOffenderManager? {
-    val communityApiResponse = communityApiClient
-      .get()
-      .uri("/probation-case/{crn}/responsible-community-manager", crn)
+  fun getOffenderManager(crnOrNomisId: String): CommunityManager? = deliusApiWebClient
+    .get()
+    .uri("/probation-case/{crnOrNomisId}/responsible-community-manager", crnOrNomisId)
+    .accept(MediaType.APPLICATION_JSON)
+    .retrieve()
+    .bodyToMono(typeReference<CommunityManager>())
+    .onErrorResume {
+      when {
+        it is WebClientResponseException && it.statusCode == HttpStatus.NOT_FOUND -> {
+          Mono.empty()
+        }
+
+        else -> Mono.error(it)
+      }
+    }
+    .block() ?: error("Unexpected null response from API")
+
+  fun getOffenderManagers(
+    crnsOrNomisIds: List<String>,
+    batchSize: Int = PROBATION_CASE_BATCH_SIZE,
+  ): List<CommunityManager> = batchRequests(batchSize, crnsOrNomisIds) { batch ->
+    deliusApiWebClient
+      .post()
+      .uri("/probation-case/responsible-community-manager")
+      .bodyValue(batch)
       .accept(MediaType.APPLICATION_JSON)
       .retrieve()
-      .bodyToMono(typeReference<CommunityOrPrisonOffenderManager>())
-      .onErrorResume {
-        when {
-          it is WebClientResponseException && it.statusCode == HttpStatus.NOT_FOUND -> {
-            Mono.empty()
-          }
-
-          else -> Mono.error(it)
-        }
-      }
+      .bodyToMono(typeReference<List<CommunityManager>>())
       .block()
-    return communityApiResponse
-      ?: error("Unexpected null response from API")
   }
 
   fun getStaffEmails(crns: List<String>) = batchRequests(STAFF_EMAIL_BATCH, crns) { batch ->
-    communityApiClient
+    deliusApiWebClient
       .post()
       .uri("/probation-case/responsible-community-manager")
       .bodyValue(batch)
@@ -64,7 +99,7 @@ class DeliusApiClient(@Qualifier("oauthDeliusApiClient") val communityApiClient:
   }
 
   fun getStaffDetailsByUsername(usernames: List<String>) = batchRequests(STAFF_USERNAME_BATCH, usernames) { batch ->
-    communityApiClient
+    deliusApiWebClient
       .post()
       .uri("/staff")
       .accept(MediaType.APPLICATION_JSON)
@@ -74,25 +109,42 @@ class DeliusApiClient(@Qualifier("oauthDeliusApiClient") val communityApiClient:
       .block()
   }
 
-  fun getManagedOffenders(staffIdentifier: Long): List<ManagedOffenderCrn> {
-    val communityApiResponse = communityApiClient
-      .get()
-      .uri("/staff/byid/$staffIdentifier/caseload/managed-offenders", staffIdentifier)
-      .accept(MediaType.APPLICATION_JSON)
-      .retrieve()
-      .bodyToMono(typeReference<List<ManagedOffenderCrn>>())
-      .block()
-    return communityApiResponse ?: error("Unexpected null response from community API get staff caseload")
-  }
+  fun getManagedOffenders(staffIdentifier: Long): List<ManagedOffenderCrn> = deliusApiWebClient
+    .get()
+    .uri("/staff/byid/{staffIdentifier}/caseload/managed-offenders", staffIdentifier)
+    .accept(MediaType.APPLICATION_JSON)
+    .retrieve()
+    .bodyToMono(typeReference<List<ManagedOffenderCrn>>())
+    .block() ?: error("Unexpected null response from Delius staff caseload")
 
-  fun getManagedOffendersByTeam(teamCode: String): List<ManagedOffenderCrn> {
-    val communityApiResponse = communityApiClient
-      .get()
-      .uri("/team/$teamCode/caseload/managed-offenders")
-      .accept(MediaType.APPLICATION_JSON)
-      .retrieve()
-      .bodyToMono(typeReference<List<ManagedOffenderCrn>>())
-      .block()
-    return communityApiResponse ?: error("Unexpected null response from community API get staff caseload")
-  }
+  fun getManagedOffendersByTeam(teamCode: String): List<ManagedOffenderCrn> = deliusApiWebClient
+    .get()
+    .uri("/team/$teamCode/caseload/managed-offenders")
+    .accept(MediaType.APPLICATION_JSON)
+    .retrieve()
+    .bodyToMono(typeReference<List<ManagedOffenderCrn>>())
+    .block() ?: error("Unexpected null response from Delius staff caseload")
+
+  fun getTeamManagedOffenders(
+    staffIdentifier: Long,
+    query: String,
+    page: PageRequest = PageRequest.of(0, CASELOAD_PAGE_SIZE),
+  ): CaseloadResponse = deliusApiWebClient
+    .post()
+    .uri {
+      it.path("/staff/byid/{staffIdentifier}/caseload/team-managed-offenders")
+        .apply {
+          with(page) {
+            queryParam("page", pageNumber)
+            queryParam("size", pageSize)
+            sort.forEach { order -> queryParam("sort", "${order.property},${order.direction.name}") }
+          }
+        }
+        .build(staffIdentifier)
+    }
+    .bodyValue(query)
+    .accept(MediaType.APPLICATION_JSON)
+    .retrieve()
+    .bodyToMono(typeReference<CaseloadResponse>())
+    .block() ?: error("Unexpected null response from Delius staff caseload")
 }
