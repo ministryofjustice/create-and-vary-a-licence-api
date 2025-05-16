0a4975a63b434a46c6ade4d2261f0f5cd8961ab3
AledWEvans <aled.evans1@justice.gov.uk>
2025-05-16 11:55:13 +0100

    CVSL-2882 Server side - address search results pagination

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/AddressSearchResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/AddressSearchResource.kt
@@ -1,18 +1,22 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.resource.privateApi
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.Size
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.AddressSearchResponse
@@ -65,7 +69,26 @@ class AddressSearchResource(private val addressSearchService: AddressSearchServi
       ),
     ],
   )
-  fun searchForAddresses(@Size(min = 5, max = 100) @PathVariable(name = "searchQuery") searchQuery: String): List<AddressSearchResponse> = addressSearchService.searchForAddressesByText(searchQuery)
+  fun searchForAddresses(
+    @Size(min = 5, max = 100)
+    @PathVariable(name = "searchQuery")
+    searchQuery: String,
+    @RequestParam(value = "page", required = false)
+    @Parameter(
+      description = "Pagination page number, starting at zero",
+      example = "0",
+    )
+    @Min(0)
+    page: Int = 0,
+    @RequestParam(value = "pageSize", required = false)
+    @Parameter(
+      description = "Pagination size per page, min/max = 2/200",
+      example = "50",
+    )
+    @Min(2)
+    @Max(200)
+    pageSize: Int = 50,
+  ): List<AddressSearchResponse> = addressSearchService.searchForAddressesByText(searchQuery, page, pageSize)
 
   @GetMapping("/address/search/by/postcode/{postcode}")
   @PreAuthorize("hasAnyRole('CVL_ADMIN')")
@@ -108,7 +131,26 @@ class AddressSearchResource(private val addressSearchService: AddressSearchServi
       ),
     ],
   )
-  fun searchForAddressesByPostcode(@Size(min = 5, max = 10) @PathVariable(name = "postcode") postcode: String): List<AddressSearchResponse> = addressSearchService.searchForAddressesByPostcode(postcode)
+  fun searchForAddressesByPostcode(
+    @Size(min = 5, max = 10)
+    @PathVariable(name = "postcode")
+    postcode: String,
+    @RequestParam(value = "page", required = false)
+    @Parameter(
+      description = "Pagination page number, starting at zero",
+      example = "0",
+    )
+    @Min(0)
+    page: Int = 0,
+    @RequestParam(value = "pageSize", required = false)
+    @Parameter(
+      description = "Pagination size per page, min/max = 2/200",
+      example = "50",
+    )
+    @Min(2)
+    @Max(200)
+    pageSize: Int = 50,
+  ): List<AddressSearchResponse> = addressSearchService.searchForAddressesByPostcode(postcode, page, pageSize)
 
   @GetMapping("/address/search/by/reference/{reference}")
   @PreAuthorize("hasAnyRole('CVL_ADMIN')")
