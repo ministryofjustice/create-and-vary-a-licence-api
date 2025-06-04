49344a3fb9174e5063c51bbccc7b507a090933e6
Louise Norris <louise.norris@digital.justice.gov.uk>
2025-06-04 09:26:15 +0100

    CVSL-2078 lint fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/caseload/CaCaseloadIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/caseload/CaCaseloadIntegrationTest.kt
@@ -170,7 +170,7 @@ class CaCaseloadIntegrationTest : IntegrationTestBase() {
   inner class CaseAdminSearch {
     val request = PrisonUserSearchRequest(
       query = "Person",
-      prisonCaseload = "BAI"
+      prisonCaseload = "BAI",
     )
 
     @Test
