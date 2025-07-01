2d794f3c257aa724cb8f00a01aafe0b6b1f613d0
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-07-01 13:41:38 +0100

    CVSL-1097-Programme-name-lenght-fix

--- src/main/resources/migration/common/V47__create_pathfinder_tagging_table.sql
+++ src/main/resources/migration/common/V47__create_pathfinder_tagging_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE electronic_monitoring_provider (
                                 id SERIAL NOT NULL CONSTRAINT electronic_monitoring_provider_pk PRIMARY KEY,
                                 licence_id INTEGER NOT NULL CONSTRAINT electronic_monitoring_provider_licence_fk REFERENCES licence(id) UNIQUE,
                                 is_to_be_tagged_for_programme BOOLEAN,
-                                programme_name VARCHAR(100),
+                                programme_name TEXT,
                                 date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                                 date_last_updated TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
