1f27a296103c80a316152aec99c8fa6803c3e3fb
Ryan Kendall <ryankendallt@gmail.com>
2025-07-04 13:54:44 +0100

    test coverage for job controller

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/jobs/MigrateExclusionZoneMapsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/jobs/MigrateExclusionZoneMapsController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.constraints.Min
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -40,5 +41,5 @@ class MigrateExclusionZoneMapsController(
       ),
     ],
   )
-  fun runJob(@RequestParam(required = true) batchSize: Int) = exclusionZoneUploadsMigration.perform(batchSize)
+  fun runJob(@RequestParam @Min(1) batchSize: Int = 1) = exclusionZoneUploadsMigration.perform(batchSize)
 }
