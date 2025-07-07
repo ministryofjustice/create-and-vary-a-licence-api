cbd524ee85ec761d23325f50b2b3f0e17423b69a
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-07-07 11:08:05 +0100

    CVSL-3056-CVL-Electronic-monitoring-highlighting-licence-has-EM-update-logic-to-handle-existing-licences

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/entity/PrrdLicence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/entity/PrrdLicence.kt
@@ -83,7 +83,7 @@ class PrrdLicence(
   @JoinColumn(name = "created_by_com_id", nullable = false)
   var createdBy: CommunityOffenderManager? = null,
 
-  @OneToOne(mappedBy = "licence", cascade = [CascadeType.ALL], fetch = FetchType.LAZY, optional = true, orphanRemoval = true)
+  @OneToOne(mappedBy = "licence", cascade = [CascadeType.ALL], fetch = FetchType.EAGER, optional = true, orphanRemoval = true)
   var electronicMonitoringProvider: ElectronicMonitoringProvider? = null,
 ) : Licence(
   id = id,
