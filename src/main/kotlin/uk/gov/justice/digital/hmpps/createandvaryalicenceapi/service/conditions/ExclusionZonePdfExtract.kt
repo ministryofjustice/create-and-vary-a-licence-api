6136fb7f624b65d3f6e377eccbc60f0c3cae172a
Ryan Kendall <ryankendallt@gmail.com>
2025-07-10 13:26:41 +0100

    explicit error handling in pdf extraction

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/conditions/ExclusionZonePdfExtract.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/conditions/ExclusionZonePdfExtract.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.conditions
 import org.apache.pdfbox.Loader
 import org.apache.pdfbox.io.RandomAccessReadBuffer
 import org.apache.pdfbox.pdmodel.PDDocument
-import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException
 import org.apache.pdfbox.rendering.PDFRenderer
 import org.apache.pdfbox.text.PDFTextStripper
 import org.slf4j.LoggerFactory
@@ -11,75 +10,60 @@ import org.springframework.web.multipart.MultipartFile
 import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
-import java.io.IOException
 import javax.imageio.ImageIO
 
 data class ExclusionZonePdfExtract(
-  var thumbnailImage: ByteArray,
-  var fullSizeImage: ByteArray,
   var description: String,
+  var fullSizeImage: ByteArray,
+  var thumbnailImage: ByteArray,
 ) {
   companion object {
-    fun fromMultipartFile(file: MultipartFile): ExclusionZonePdfExtract? = Loader.loadPDF(RandomAccessReadBuffer(file.inputStream)).use { pdfDoc ->
-      return ExclusionZonePdfExtractBuilder(pdfDoc).build()
+    fun fromMultipartFile(file: MultipartFile): ExclusionZonePdfExtract {
+      Loader.loadPDF(RandomAccessReadBuffer(file.inputStream)).use { pdfDoc ->
+        return ExclusionZonePdfExtractBuilder(pdfDoc).build()
+      }
     }
   }
 }
 
 class ExclusionZonePdfExtractBuilder(private var pdfDoc: PDDocument) {
-  fun build(): ExclusionZonePdfExtract? {
-    try {
-      val description = description()
-      val fullSizeImage = fullSizeImage()
-      val thumbnailImage = thumbnailImage(fullSizeImage)
 
-      if (!listOf(description, thumbnailImage, fullSizeImage).contains(null)) {
-        return ExclusionZonePdfExtract(
-          description = description!!,
-          fullSizeImage = fullSizeImage,
-          thumbnailImage = thumbnailImage,
-        )
-      }
-    } catch (io: IOException) {
-      log.error("IO error ${io.message}")
-    } catch (ipe: InvalidPasswordException) {
-      log.error("Invalid password ${ipe.message}")
-    } catch (iae: IllegalArgumentException) {
-      log.error("Illegal Argument ${iae.message}")
+  fun build(): ExclusionZonePdfExtract = runCatching {
+    val fullSizeImage = fullSizeImage()
+    val thumbnailImage = thumbnailImage(fullSizeImage)
+
+    return ExclusionZonePdfExtract(description(), fullSizeImage, thumbnailImage)
+  }.getOrElse { throw ExclusionZonePdfExtractionError("Unable to extract exclusion zone pdf details", it) }
+
+  private fun description(): String = runCatching {
+    with(PDFTextStripper()) {
+      sortByPosition = true
+      startPage = 2
+      getText(pdfDoc)
     }
+  }.onFailure { log.error("Failed to extract description", it) }.getOrThrow()
 
-    return null
-  }
-
-  private fun description(): String? = with(PDFTextStripper()) {
-    sortByPosition = true
-    startPage = 2
-    getText(pdfDoc)
-  }
-
-  private fun fullSizeImage(): ByteArray = with(PDFRenderer(pdfDoc)) {
-    val firstImage = renderImage(0)
-    val croppedImage = firstImage
-      .getSubimage(
+  private fun fullSizeImage(): ByteArray = runCatching {
+    with(PDFRenderer(pdfDoc)) {
+      val firstPageAsImage = renderImage(0)
+      val mapImage = firstPageAsImage.getSubimage(
         IMAGE_LOCATION_X,
         IMAGE_LOCATION_Y,
-        firstImage.width - IMAGE_PADDING,
-        firstImage.height - IMAGE_PADDING,
+        firstPageAsImage.width - IMAGE_PADDING,
+        firstPageAsImage.height - IMAGE_PADDING,
       )
 
-    with(ByteArrayOutputStream()) {
-      ImageIO.write(croppedImage, "png", this)
-      return toByteArray()
+      with(ByteArrayOutputStream()) {
+        ImageIO.write(mapImage, "png", this)
+        val bytes = toByteArray()
+        if (bytes.isEmpty()) error("Full size image was empty")
+        return bytes
+      }
     }
-  }
+  }.onFailure { log.error("Failed to extract map image", it) }.getOrThrow()
 
-  private fun thumbnailImage(fullSizeImage: ByteArray?): ByteArray {
-    if (fullSizeImage?.isEmpty() == true) {
-      log.error("Creating thumbnail image - full size image was empty.")
-      return ByteArray(0)
-    }
-
-    val inputStream = fullSizeImage!!.inputStream()
+  private fun thumbnailImage(fullSizeImage: ByteArray): ByteArray = runCatching {
+    val inputStream = fullSizeImage.inputStream()
     val original = ImageIO.read(inputStream)
     val scaled = original.getScaledInstance(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT, Image.SCALE_DEFAULT)
     val outputImage = BufferedImage(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT, BufferedImage.TYPE_INT_RGB)
@@ -89,7 +73,7 @@ class ExclusionZonePdfExtractBuilder(private var pdfDoc: PDDocument) {
       ImageIO.write(outputImage, "jpg", this)
       return toByteArray()
     }
-  }
+  }.onFailure { log.error("Failed to extract thumbnail image", it) }.getOrThrow()
 
   companion object {
     val log = LoggerFactory.getLogger(this::class.java)
@@ -98,6 +82,7 @@ class ExclusionZonePdfExtractBuilder(private var pdfDoc: PDDocument) {
     const val IMAGE_PADDING = 100
     const val THUMBNAIL_WIDTH = 150
     const val THUMBNAIL_HEIGHT = 200
-    const val WAIT_FOR_IMAGE_READY_MS: Long = 500
   }
 }
+
+class ExclusionZonePdfExtractionError(message: String, throwable: Throwable) : RuntimeException(message, throwable)
