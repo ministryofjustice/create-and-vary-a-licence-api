d3403f7a6a19bffcc76a59477340f0b599b20de8
Louise N <louise.norris@digital.justice.gov.uk>
2025-07-30 09:10:55 +0100

    CVSL-3094 add approver search endpoint (#864)
    * CVSL-3094 add approver search endpoint
    
    * CVSL-3094 lint fix

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/caseload/ApproverCaseloadIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/caseload/ApproverCaseloadIntegrationTest.kt
@@ -16,6 +16,8 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.Integra
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.DeliusMockServer
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.GovUkMockServer
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.ApprovalCase
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request.ApproverSearchRequest
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.response.ApproverSearchResponse
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.typeReference
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceRepository
 import java.time.LocalDate
@@ -137,6 +139,93 @@ class ApproverCaseloadIntegrationTest : IntegrationTestBase() {
       }
       with(caseload.last()) {
         assertThat(name).isEqualTo("Person Eight")
+        assertThat(prisonerNumber).isEqualTo("F2504MG")
+        assertThat(approvedBy).isNotNull()
+        assertThat(approvedOn).isNotNull()
+        assertThat(releaseDate).isBeforeOrEqualTo(LocalDate.now().minusDays(10))
+      }
+    }
+  }
+
+  @Nested
+  inner class ApproverSearchTest {
+    val request = ApproverSearchRequest(
+      prisonCaseloads = listOf("ABC", "AAA"),
+      query = "Person",
+    )
+
+    @Test
+    fun `Get forbidden (403) when incorrect roles are supplied`() {
+      val result = webTestClient.post()
+        .uri("/caseload/prison-approver/case-search")
+        .bodyValue(request)
+        .accept(APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_CVL_WRONG ROLE")))
+        .exchange()
+        .expectStatus().isForbidden
+        .expectStatus().isEqualTo(FORBIDDEN.value())
+        .expectBody(ErrorResponse::class.java)
+        .returnResult().responseBody
+
+      assertThat(result?.userMessage).contains("Access Denied")
+    }
+
+    @Test
+    fun `Unauthorized (401) when no token is supplied`() {
+      webTestClient.post()
+        .uri("/caseload/prison-approver/case-search")
+        .bodyValue(request)
+        .accept(APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isEqualTo(UNAUTHORIZED.value())
+    }
+
+    @Test
+    @Sql(
+      "classpath:test_data/seed-approver-caseload-licences.sql",
+    )
+    fun successfullyRetrieveSubmittedAndRecentlyApprovedCases() {
+      deliusMockServer.stubGetManagersForGetApprovalAndRecentlyApprovedCaseload()
+      deliusMockServer.stubGetStaffDetailsByUsername()
+
+      val result = webTestClient.post()
+        .uri("/caseload/prison-approver/case-search")
+        .bodyValue(request)
+        .accept(APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
+        .exchange()
+        .expectStatus().isEqualTo(OK.value())
+        .expectHeader().contentType(APPLICATION_JSON)
+        .expectBody(typeReference<ApproverSearchResponse>())
+        .returnResult().responseBody!!
+
+      assertThat(result.approvalNeededResponse).hasSize(3)
+      assertThat(result.recentlyApprovedResponse).hasSize(2)
+
+      with(result.approvalNeededResponse.first()) {
+        assertThat(name).isEqualTo("Person Seven")
+        assertThat(prisonerNumber).isEqualTo("A1234BC")
+        assertThat(approvedBy).isNull()
+        assertThat(approvedOn).isNull()
+      }
+
+      with(result.approvalNeededResponse.last()) {
+        assertThat(name).isEqualTo("Person Eight")
+        assertThat(prisonerNumber).isEqualTo("B1234BC")
+        assertThat(approvedBy).isNull()
+        assertThat(approvedOn).isNull()
+      }
+
+      with(result.recentlyApprovedResponse.first()) {
+        assertThat(name).isEqualTo("Person Ten")
+        assertThat(prisonerNumber).isEqualTo("B1234BB")
+        assertThat(approvedBy).isNotNull()
+        assertThat(approvedOn).isNotNull()
+        assertThat(releaseDate).isBeforeOrEqualTo(LocalDate.now().minusDays(10))
+      }
+
+      with(result.recentlyApprovedResponse.last()) {
+        assertThat(name).isEqualTo("Person Eighteen")
         assertThat(prisonerNumber).isEqualTo("F2504MG")
         assertThat(approvedBy).isNotNull()
         assertThat(approvedOn).isNotNull()
