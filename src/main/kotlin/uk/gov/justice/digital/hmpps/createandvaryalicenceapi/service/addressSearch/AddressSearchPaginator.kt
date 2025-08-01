c24e5aa904e8549d15e471007f82ee1c11243fd9
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2025-08-01 15:20:49 +0100

    Cvsl 3155 cvl saved addresses pagination no limit (#873)
    * CVSL-2964 Migrate appointment address as individual fields - other tweaks to remove post codes and towns with no spaces or other separators.
    tn38 0et,tn38-0et,tn380et...,
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit- validation failed
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit- some reformatting of code
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit- some reformatting of code
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit- in wrong package
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit- in wrong package
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit- validation failure
    
    * CVSL-3155 CVL | Saved addresses Pagination No Limit- validation failure / change default max
    
    * CVSL-3155 CVL | Saved addresses Pagination No removed some code

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/addressSearch/AddressSearchPaginator.kt
@@ -0,0 +1,40 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.addressSearch
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.response.AddressSearchResponse
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.mapper.OsPlacesMapperToAddressSearchResponseMapper
+
+@Component
+class AddressSearchPaginator(
+  private val osPlacesApiClient: OsPlacesApiClient,
+  private val addressMapper: OsPlacesMapperToAddressSearchResponseMapper,
+  @Value("\${address.os-places.search.page-size:100}") private val pageSize: Int,
+  @Value("\${address.search.max-total:200}") private val maxTotal: Int,
+) {
+
+  fun searchByText(searchQuery: String): List<AddressSearchResponse> = paginate(
+    fetchCallBack = { page, pageable ->
+      osPlacesApiClient.searchForAddressesByText(pageable, searchQuery)
+    },
+    mapResults = { addressMapper.map(it) },
+  )
+
+  private fun <T, R> paginate(
+    fetchCallBack: (page: Int, pageable: PageRequest) -> List<T>,
+    mapResults: (T) -> R,
+  ): List<R> {
+    val totalResults = mutableListOf<T>()
+    var pageCount = 0
+    while (totalResults.size < maxTotal) {
+      val resultsFromApiCall = fetchCallBack(pageCount, PageRequest.of(pageCount, pageSize))
+      if (resultsFromApiCall.isNotEmpty()) {
+        totalResults.addAll(resultsFromApiCall)
+      }
+      if (resultsFromApiCall.size < pageSize) break
+      pageCount++
+    }
+    return totalResults.take(maxTotal).map { mapResults(it) }
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/addressSearch/OsPlacesApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/addressSearch/OsPlacesApiClient.kt
@@ -3,12 +3,9 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.addressSea
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.PageRequest
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClientResponseException
-import reactor.core.publisher.Mono
 
 @Component
 class OsPlacesApiClient(
@@ -22,35 +19,6 @@ class OsPlacesApiClient(
       .accept(MediaType.APPLICATION_JSON)
       .retrieve()
       .bodyToMono(OsPlacesApiResponse::class.java)
-      .onErrorResume { e ->
-        if (e is WebClientResponseException && e.statusCode == HttpStatus.BAD_REQUEST) {
-          Mono.empty()
-        } else {
-          Mono.error(
-            e,
-          )
-        }
-      }
-      .block()
-    return searchResult?.results?.map { it.dpa } ?: emptyList()
-  }
-
-  fun searchForAddressesByPostcode(pageable: PageRequest, postcode: String): List<DeliveryPointAddress> {
-    val searchResult = osPlacesApiWebClient
-      .get()
-      .uri("/postcode?postcode=$postcode&key=$apiKey&offset=${pageable.offset}&maxresults=${pageable.pageSize}")
-      .accept(MediaType.APPLICATION_JSON)
-      .retrieve()
-      .bodyToMono(OsPlacesApiResponse::class.java)
-      .onErrorResume { e ->
-        if (e is WebClientResponseException && e.statusCode == HttpStatus.BAD_REQUEST) {
-          Mono.empty()
-        } else {
-          Mono.error(
-            e,
-          )
-        }
-      }
       .block()
     return searchResult?.results?.map { it.dpa } ?: emptyList()
   }
