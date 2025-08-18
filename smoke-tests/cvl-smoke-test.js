95b4913f33c7ca070f53c066e9d1233bef4db74e
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2025-08-18 16:11:21 +0100

    CVSL-2870: Add function to get licence by id.

--- smoke-tests/cvl-smoke-test.js
+++ smoke-tests/cvl-smoke-test.js
@@ -8,7 +8,7 @@ import {check, fail} from 'k6';
 const AUTH_URL = __ENV.AUTH_URL;
 const CVL_API_URL = __ENV.CVL_API_URL;
 const CVL_URL = __ENV.CVL_URL;
-
+const AUTH_CREDENTIALS = b64encode(`${__ENV.SYSTEM_CLIENT_ID}:${__ENV.SYSTEM_CLIENT_SECRET}`);
 /*
  * Test config, see https://grafana.com/docs/k6/latest/using-k6/k6-options/
  */
@@ -41,10 +41,13 @@ export function setup() {
  * The main test function, see https://grafana.com/docs/k6/latest/using-k6/test-lifecycle/
  */
 export default async function () {
-    await signIn()
+    // await comSignIn()
+
+    const licence = getLicenceById(686)
+    console.log(`licence: ${JSON.stringify(licence)}`)
 }
 
-const signIn = async () => {
+async function signIn(username, password) {
     let checkData;
     const page = await browser.newPage();
     try {
@@ -55,33 +58,52 @@ const signIn = async () => {
             header: checkData === 'Sign in',
         });
 
-        await page.locator('input[data-element-id=username]').type(await secrets.get('username'))
-        await page.locator('input[data-element-id=password]').type(await secrets.get('password'))
+        await page.locator('input[data-element-id=username]').type(username)
+        await page.locator('input[data-element-id=password]').type(password)
 
         await page.locator('button[data-element-id=continue-button]').click()
-        await page.waitForTimeout(3000);
-        await page.screenshot({path: 'screenshot-test-end.png'});
     } catch (error) {
-        fail(`Failed to load homepage: ${error.message}`);
+        fail(`Failed to sign in: ${error.message}`);
     } finally {
         await page.close();
     }
 }
 
+async function comSignIn() {
+    await signIn(await secrets.get('com_username'), await secrets.get('com_password'))
+}
+
+function getLicenceById(licenceId) {
+    const token = getToken()
+
+    const url = `${CVL_API_URL}/licence/id/${licenceId}`;
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    };
+
+    const res = http.get(url, params);
+    if (res.status !== 200) {
+        fail(`could not get licence by id: ${res.status}`);
+    }
+    return JSON.parse(res.body)
+}
+
 function getToken() {
-    const credentials = b64encode(`${__ENV.SYSTEM_CLIENT_ID}:${__ENV.SYSTEM_CLIENT_SECRET}`);
     const tokenUrl = `${AUTH_URL}/auth/oauth/token?grant_type=client_credentials`;
     const params = {
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Basic ${credentials}`
+            'Authorization': `Basic ${AUTH_CREDENTIALS}`
         },
     };
 
-
     const res = http.post(tokenUrl, null, params);
-    if (res.status === 200) {
-        console.log(`status ${res.status}`);
-        console.log(`token: ${JSON.parse(res.body).access_token}`)
+    if (res.status !== 200) {
+        fail(`could not obtain access token: ${res.status}`);
     }
+
+    return JSON.parse(res.body).access_token
 }
