4e4cc1ad3fa0af6257ed98616f27da80cb7c3cad
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2025-08-22 10:57:18 +0100

    CVSL-2870: Fill in initial meeting details and select no licence conditions for licence.

--- smoke-tests/cvl-smoke-test.js
+++ smoke-tests/cvl-smoke-test.js
@@ -11,8 +11,14 @@ const CVL_API_URL = __ENV.CVL_API_URL;
 const CVL_URL = __ENV.CVL_URL;
 const HOME_URL = CVL_URL + "/";
 const AUTH_CREDENTIALS = b64encode(`${__ENV.SYSTEM_CLIENT_ID}:${__ENV.SYSTEM_CLIENT_SECRET}`);
+
+const LICENCE_APPOINTMENT_NAME = 'SMOKE_TEST_APPOINTMENT';
+const LICENCE_ADDRESS_LINE_1 = 'SMOKE_TEST_ADDRESS_LINE_1';
+const LICENCE_TOWN = 'SMOKE_TEST_TOWN';
+const LICENCE_POSTCODE = 'SMOKE_TEST_POSTCODE';
+const LICENCE_CONTACT_PHONE_NUMBER = '0123455666';
 /*
- * Test config, see https://grafana.com/docs/k6/latest/using-k6/k6-options/
+ * Test config, see https://grafana.com/docs/k6/latest/usingPOSTCODE-k6/k6-options/
  */
 export const options = {
     scenarios: {
@@ -96,6 +102,32 @@ async function createLicence(nomisId) {
     await page.waitForURL(/initial-meeting-name/);
     checkLicenceCreated(nomisId)
 
+    await page.locator('#contactName').type(LICENCE_APPOINTMENT_NAME)
+    await page.locator('button[data-qa=continue]').click()
+
+    await page.waitForURL(/initial-meeting-place/);
+    await page.locator('#addressLine1').type(LICENCE_ADDRESS_LINE_1)
+    await page.locator('#addressTown').type(LICENCE_TOWN)
+    await page.locator('#addressPostcode').type(LICENCE_POSTCODE)
+    await page.locator('button[data-qa=continue]').click()
+
+    await page.waitForURL(/initial-meeting-contact/);
+    await page.locator('#telephone').type(LICENCE_CONTACT_PHONE_NUMBER)
+    await page.locator('button[data-qa=continue]').click()
+
+    await page.waitForURL(/initial-meeting-time/);
+    await page.locator('#appointmentTimeType-2').click()
+    await page.locator('button[data-qa=continue]').click()
+
+    await page.waitForURL(/additional-licence-conditions-question/);
+    await page.locator('#radio-option-2').click()
+    await page.locator('button[data-qa=continue]').click()
+
+    await page.waitForURL(/bespoke-conditions-question/);
+    await page.locator('#radio-option-2').click()
+    await page.locator('button[data-qa=continue]').click()
+
+    await page.waitForURL(/check-your-answers/);
     await page.screenshot({path: 'end.png'});
 
     // await page.waitForTimeout(1000);
