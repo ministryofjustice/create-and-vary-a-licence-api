58b4aa0d96448b1472e865e091c1ea84ef09d3a7
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2025-09-01 11:18:56 +0100

    CVSL-3195 Hibernate update - Remove Entity Kotlin copy and update entity values directly, OmuService (#918)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/OmuService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/OmuService.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service
 
 import jakarta.persistence.EntityNotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.OmuContact
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request.UpdateOmuEmailRequest
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.OmuContactRepository
@@ -17,17 +18,23 @@ class OmuService(private val omuRepository: OmuContactRepository) {
   /**
    * Create or update OMU contact email address belonging to the prison code
    */
-  fun updateOmuEmail(prisonCode: String, contactRequest: UpdateOmuEmailRequest): OmuContact = this.omuRepository.saveAndFlush(
-    this.omuRepository.findByPrisonCode(prisonCode)?.copy(
-      email = contactRequest.email,
-      dateLastUpdated = LocalDateTime.now(),
-    )
-      ?: OmuContact(
+  @Transactional
+  fun updateOmuEmail(prisonCode: String, contactRequest: UpdateOmuEmailRequest): OmuContact {
+    val contact = this.omuRepository.findByPrisonCode(prisonCode)
+    contact?.let {
+      it.email = contactRequest.email
+      it.dateLastUpdated = LocalDateTime.now()
+      return it
+    }
+
+    return this.omuRepository.saveAndFlush(
+      OmuContact(
         prisonCode = prisonCode,
         email = contactRequest.email,
         dateCreated = LocalDateTime.now(),
       ),
-  )
+    )
+  }
 
   /**
    * delete an OMU contact email address if no longer valid
