9e748d5f69f2c93e8a4f566d127403345ca3373b
AledWEvans <aled.evans1@justice.gov.uk>
2025-10-07 14:19:33 +0100

    CVSL-3358 CVL | look into prison caseload performance some extra indexes

--- src/main/resources/migration/common/V60__add_new_staff_index.sql
+++ src/main/resources/migration/common/V60__add_new_staff_index.sql
@@ -2,4 +2,6 @@ CREATE INDEX IF NOT EXISTS idx_staff_kind_id_partial
 	ON public.staff (kind, id)
 	WHERE kind IS NOT NULL;
 
-CREATE INDEX idx_staff_kind ON staff(kind) WHERE kind IS NOT NULL;
+CREATE INDEX IF NOT EXISTS idx_staff_kind
+	ON staff(kind)
+	WHERE kind IS NOT NULL;
