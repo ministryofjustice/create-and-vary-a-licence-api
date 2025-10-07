dc57a002e43a0233ce8ee541bee5d459027fedfd
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2025-10-07 16:03:59 +0100

    Update V60__add_new_staff_index.sql

--- src/main/resources/migration/common/V60__add_new_staff_index.sql
+++ src/main/resources/migration/common/V60__add_new_staff_index.sql
@@ -1,7 +1,5 @@
 CREATE INDEX IF NOT EXISTS idx_staff_kind_id_partial
-	ON public.staff (kind, id)
-	WHERE kind IS NOT NULL;
+	ON public.staff (kind, id);
 
 CREATE INDEX IF NOT EXISTS idx_staff_kind
-	ON staff(kind)
-	WHERE kind IS NOT NULL;
+	ON staff(kind);
