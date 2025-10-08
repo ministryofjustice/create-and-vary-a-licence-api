2765d322ac1d0ed46f7e6475dafa1f8f0a9b8cd1
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-10-08 15:37:36 +0100

    CVSL-3341-CVL-API-Use-kotlin-lib-for-SAR-support

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/publicApi/SubjectAccessRequestService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/publicApi/SubjectAccessRequestService.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.publicApi
 
-import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.AuditEventRepository
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceRepository
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.LicenceService
@@ -10,6 +10,7 @@ import uk.gov.justice.hmpps.kotlin.sar.HmppsPrisonSubjectAccessRequestService
 import uk.gov.justice.hmpps.kotlin.sar.HmppsSubjectAccessRequestContent
 import java.time.LocalDate
 
+@Transactional(readOnly = true)
 @Service
 class SubjectAccessRequestService(
   private val licenceService: LicenceService,
@@ -18,7 +19,6 @@ class SubjectAccessRequestService(
   @param:Value("\${self.api.link}") private val baseUrl: String,
 ) : HmppsPrisonSubjectAccessRequestService {
 
-  @Transactional
   override fun getPrisonContentFor(prn: String, fromDate: LocalDate?, toDate: LocalDate?): HmppsSubjectAccessRequestContent? {
     val licenceIds = licenceRepository.findAllByNomsId(prn).map { it.id }
 
