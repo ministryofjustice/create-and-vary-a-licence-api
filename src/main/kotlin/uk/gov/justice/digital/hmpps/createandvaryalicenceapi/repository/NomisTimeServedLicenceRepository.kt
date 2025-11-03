3f475b22bdb46742f7bb01ad3a9d7384568f2710
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-11-03 09:54:33 +0000

    CVSL-3438 Time-Served-Retain-selection-and-free-text-if-NOMIS-selected

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/NomisTimeServedLicenceRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/NomisTimeServedLicenceRepository.kt
@@ -11,22 +11,4 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.NomisTimeSer
 interface NomisTimeServedLicenceRepository : JpaRepository<NomisTimeServedLicence, Long> {
 
   fun findByNomsIdAndBookingId(nomsId: String, bookingId: Long): NomisTimeServedLicence?
-
-//  @Query(
-//    """
-//    SELECT l.nomsId AS nomsId,
-//           l.bookingId AS bookingId,
-//           l.reason AS reason,
-//           l.prisonCode AS prisonCode,
-//           l.dateCreated AS dateCreated,
-//           l.dateLastUpdated AS dateLastUpdated
-//    FROM NomisTimeServedLicence l
-//    WHERE l.nomsId = :nomsId AND l.bookingId = :bookingId
-// """,
-//  )
-//  fun findLicenceReasonByNomsIdAndBookingId(
-//    @Param("nomsId") nomsId: String,
-//    @Param("bookingId") bookingId: Long,
-//  ): NomisLicenceReasonResponse?
-//  fun findLicenceReasonByNomsIdAndBookingId(nomsId: String, bookingId: Long): NomisTimeServedLicence?
 }
