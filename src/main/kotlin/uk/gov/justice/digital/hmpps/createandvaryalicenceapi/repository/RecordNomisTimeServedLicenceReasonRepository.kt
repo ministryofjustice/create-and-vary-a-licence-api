feb0dbcaf3b146e6e61ffcdd7bd86f8584609068
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-11-05 17:10:47 +0000

    CVSL-3440 Time-Served-CA-Distinguish-CVL-and-NOMIS-time-served-cases

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/RecordNomisTimeServedLicenceReasonRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/RecordNomisTimeServedLicenceReasonRepository.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
@@ -11,4 +13,20 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.RecordNomisT
 interface RecordNomisTimeServedLicenceReasonRepository : JpaRepository<RecordNomisTimeServedLicenceReason, Long> {
 
   fun findByNomsIdAndBookingId(nomsId: String, bookingId: Long): RecordNomisTimeServedLicenceReason?
+
+  @Query(
+    """
+        SELECT r.bookingId AS bookingId, 
+               CASE WHEN COUNT(r) > 0 THEN true ELSE false END AS hasLicence
+        FROM RecordNomisTimeServedLicenceReason r
+        WHERE r.bookingId IN :bookingIds
+        GROUP BY r.bookingId
+        """,
+  )
+  fun getNomisLicenceFlagsByBookingIds(@Param("bookingIds") bookingIds: List<String>): List<LicenceFlagProjection>
+}
+
+interface LicenceFlagProjection {
+  val bookingId: String
+  val hasNomisLicence: Boolean
 }
