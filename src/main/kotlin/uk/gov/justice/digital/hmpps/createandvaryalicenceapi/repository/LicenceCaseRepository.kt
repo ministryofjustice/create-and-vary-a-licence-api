6f6b5fe1d7e33f0c4ab45841ef555e40f60cb5c0
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-11-06 08:02:59 +0000

    CVSL-3416 improving approver caseload performance (#1040)
    * CVSL-3416 improving approver caseload performance
    
    * Apply suggestion from @andrewrlee
    
    * CVSL-3416 improving approver caseload performance - removed need for hardcoded classes
    
    * CVSL-3416 improving approver caseload performance - link validation failures
    
    ---------
    
    Co-authored-by: AledWEvans <aled.evans1@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/LicenceCaseRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/LicenceCaseRepository.kt
@@ -6,13 +6,12 @@ import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.Licence
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model.LicenceApproverCase
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model.LicenceCaCase
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model.LicenceComCase
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model.LicenceSubmitName
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
-
-private const val LICENCE_CASE = "uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model.LicenceCaCase"
-private const val LICENCE_COM_CASE =
-  "uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model.LicenceComCase"
+import java.time.LocalDate
 
 @Transactional(readOnly = true, propagation = Propagation.NOT_SUPPORTED)
 @Repository
@@ -20,7 +19,7 @@ interface LicenceCaseRepository : JpaRepository<Licence, Long> {
 
   @Query(
     """
-        SELECT new $LICENCE_CASE(
+        SELECT 
             l.kind,
             l.idInternal,
             l.versionOfId,
@@ -39,7 +38,6 @@ interface LicenceCaseRepository : JpaRepository<Licence, Long> {
             com.username,
             l.prisonCode,
             l.prisonDescription
-        )
         FROM Licence l
             LEFT JOIN l.responsibleCom com
             LEFT JOIN l.updatedBy updatedBy
@@ -54,7 +52,7 @@ interface LicenceCaseRepository : JpaRepository<Licence, Long> {
 
   @Query(
     """
-        SELECT new $LICENCE_COM_CASE(
+        SELECT 
           l.kind,
           l.idInternal,
           l.versionOfId,
@@ -77,7 +75,6 @@ interface LicenceCaseRepository : JpaRepository<Licence, Long> {
           l.dateCreated,
           l.approvedByName,
           l.approvedDate
-        )
         FROM Licence l
             LEFT JOIN l.responsibleCom com
             LEFT JOIN l.updatedBy updatedBy
@@ -89,4 +86,122 @@ interface LicenceCaseRepository : JpaRepository<Licence, Long> {
     statusCodes: List<LicenceStatus>,
     crns: List<String>,
   ): List<LicenceComCase>
+
+  @Query(
+    """
+        SELECT 
+          l.licenceStartDate,
+          l.kind,
+          l.idInternal,
+          l.versionOfId,
+          l.statusCode,
+          l.nomsId as prisonNumber,
+          l.surname,
+          l.forename,
+          updatedBy.firstName,
+          updatedBy.lastName,
+          com.username,
+          l.sentenceStartDate,
+          l.conditionalReleaseDate,
+          l.actualReleaseDate,
+          l.postRecallReleaseDate,
+          l.approvedByName,
+          l.approvedDate,
+          l.prisonCode,
+          l.prisonDescription,
+          l.variationOfId
+        FROM Licence l
+          LEFT JOIN l.responsibleCom com
+          LEFT JOIN l.updatedBy updatedBy
+        WHERE l.statusCode = 'SUBMITTED'
+          AND l.prisonCode IN :prisons
+        ORDER BY l.licenceStartDate ASC
+    """,
+  )
+  fun findLicenceCasesReadyForApproval(prisons: List<String>): List<LicenceApproverCase>
+
+  @Query(
+    """
+    SELECT
+      l.licenceStartDate,
+      l.kind,
+      l.idInternal,
+      l.versionOfId,
+      l.statusCode,
+      l.nomsId as prisonNumber,
+      l.surname,
+      l.forename,
+      updatedBy.firstName,
+      updatedBy.lastName,
+      com.username,
+      l.sentenceStartDate,
+      l.conditionalReleaseDate,
+      l.actualReleaseDate,
+      l.postRecallReleaseDate,
+      l.approvedByName,
+      l.approvedDate,
+      l.prisonCode,
+      l.prisonDescription,
+      l.variationOfId
+    FROM Licence l
+        LEFT JOIN l.responsibleCom com
+        LEFT JOIN l.updatedBy updatedBy
+        WHERE l.prisonCode IN :prisonCodes
+        AND l.statusCode IN ('ACTIVE','APPROVED') 
+        AND l.licenceStartDate > :releasedAfterDate
+    ORDER BY l.approvedDate DESC
+    """,
+  )
+  fun findRecentlyApprovedLicenceCasesAfter(
+    prisonCodes: List<String>,
+    releasedAfterDate: LocalDate,
+  ): List<LicenceApproverCase>
+
+  @Query(
+    """
+        SELECT 
+          l.licenceStartDate,
+          l.kind,
+          l.idInternal,
+          l.versionOfId,
+          l.statusCode,
+          l.nomsId as prisonNumber,
+          l.surname,
+          l.forename,
+          updatedBy.firstName,
+          updatedBy.lastName,
+          com.username,
+          l.sentenceStartDate,
+          l.conditionalReleaseDate,
+          l.actualReleaseDate,
+          l.postRecallReleaseDate,
+          l.approvedByName,
+          l.approvedDate,
+          l.prisonCode,
+          l.prisonDescription,
+          l.variationOfId
+        FROM Licence l
+          LEFT JOIN l.responsibleCom com
+          LEFT JOIN l.updatedBy updatedBy
+        WHERE l.idInternal = :id
+        ORDER BY l.idInternal
+    """,
+  )
+  fun findLicenceApproverCase(id: Long): LicenceApproverCase
+
+  @Query(
+    value = """
+    SELECT l.id AS licence_id,
+           COALESCE(
+             CONCAT(com.first_name, ' ', com.last_name),
+             CONCAT(ca.first_name, ' ', ca.last_name)
+           ) AS full_name
+    FROM licence l
+    LEFT JOIN staff com ON com.id = l.submitted_by_com_id
+    LEFT JOIN staff ca ON ca.id = l.submitted_by_ca_id
+    WHERE l.id IN :licenceIds
+  """,
+    nativeQuery = true,
+  )
+  fun findSubmittedByNames(licenceIds: List<Long>): List<LicenceSubmitName>
 }
