f20b5c3db43950a16c04816d8c0a84d56a575a5b
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2025-11-06 16:48:33 +0000

    CVSL-3416 improving com vary case load (#1049)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/caseload/com/ComVaryCaseloadService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/caseload/com/ComVaryCaseloadService.kt
@@ -2,15 +2,14 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.caseload.c
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.ComCase
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.LicenceSummary
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.ProbationPractitioner
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceCaseRepository
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model.LicenceComCase
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.CaseloadType.ComVaryStaffCaseload
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.CaseloadType.ComVaryTeamCaseload
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.LicenceService
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.TelemetryService
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.conditions.convertToTitleCase
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.dates.ReleaseDateService
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.SentenceDateHolderAdapter.toSentenceDateHolder
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.DeliusApiClient
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.ManagedOffenderCrn
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.fullName
@@ -23,7 +22,7 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus.
 @Service
 class ComVaryCaseloadService(
   private val deliusApiClient: DeliusApiClient,
-  private val licenceService: LicenceService,
+  private val licenceCaseRepository: LicenceCaseRepository,
   private val releaseDateService: ReleaseDateService,
   private val telemetryService: TelemetryService,
 ) {
@@ -47,7 +46,7 @@ class ComVaryCaseloadService(
     return cases
   }
 
-  fun mapCaseToVaryLicence(cases: List<ManagedOffenderCrn>): Map<ManagedOffenderCrn, LicenceSummary> {
+  fun mapCaseToVaryLicence(cases: List<ManagedOffenderCrn>): Map<ManagedOffenderCrn, LicenceComCase> {
     val licences = findExistingActiveAndVariationLicences(cases.mapNotNull { it.crn })
     return cases.mapNotNull { case ->
       val caseLicences = licences.filter { licence -> case.crn == licence.crn }
@@ -59,7 +58,7 @@ class ComVaryCaseloadService(
     }.toMap()
   }
 
-  private fun getComDetails(casesToLicences: Map<ManagedOffenderCrn, LicenceSummary>): Map<String?, ProbationPractitioner> {
+  private fun getComDetails(casesToLicences: Map<ManagedOffenderCrn, LicenceComCase>): Map<String?, ProbationPractitioner> {
     val comUsernames = casesToLicences.mapNotNull { (_, licence) -> licence.comUsername }.distinct()
     val coms = deliusApiClient.getStaffDetailsByUsername(comUsernames)
     return casesToLicences.map { (case, licence) ->
@@ -80,10 +79,10 @@ class ComVaryCaseloadService(
     }.toMap()
   }
 
-  private fun findExistingActiveAndVariationLicences(crnList: List<String>): List<LicenceSummary> = if (crnList.isEmpty()) {
+  private fun findExistingActiveAndVariationLicences(crnList: List<String>): List<LicenceComCase> = if (crnList.isEmpty()) {
     emptyList()
   } else {
-    licenceService.findLicencesForCrnsAndStatuses(
+    licenceCaseRepository.findLicenceCasesForCom(
       crns = crnList,
       statusCodes = listOf(
         ACTIVE,
@@ -101,31 +100,31 @@ class ComVaryCaseloadService(
     probationTeamCodes.first()
   }
 
-  private fun transformToVaryCaseload(casesToLicences: Map<ManagedOffenderCrn, LicenceSummary>): List<ComCase> {
+  private fun transformToVaryCaseload(casesToLicences: Map<ManagedOffenderCrn, LicenceComCase>): List<ComCase> {
     val comDetails = getComDetails(casesToLicences)
 
     return casesToLicences.map { (case, licence) ->
-      val hardStopKind = releaseDateService.getHardStopKind(licence.toSentenceDateHolder())
+      val hardStopKind = releaseDateService.getHardStopKind(licence)
 
       ComCase(
         licenceId = licence.licenceId,
-        licenceType = licence.licenceType,
-        licenceStatus = licence.licenceStatus,
+        licenceType = licence.typeCode,
+        licenceStatus = licence.statusCode,
         crnNumber = licence.crn,
-        prisonerNumber = licence.nomisId,
+        prisonerNumber = licence.prisonNumber,
         hardStopKind = hardStopKind,
         kind = licence.kind,
         name = "${licence.forename} ${licence.surname}".trim().convertToTitleCase(),
         releaseDate = licence.licenceStartDate,
         probationPractitioner = comDetails[case.crn],
-        isReviewNeeded = licence.isReviewNeeded,
+        isReviewNeeded = licence.isReviewNeeded(),
       )
     }.sortedWith(compareBy<ComCase> { it.releaseDate }.thenBy { it.name })
   }
 
-  private fun findVaryLicenceToDisplay(licences: List<LicenceSummary>): LicenceSummary? = when {
+  private fun findVaryLicenceToDisplay(licences: List<LicenceComCase>): LicenceComCase? = when {
     licences.isEmpty() -> null
-    licences.size > 1 -> licences.find { licence -> licence.licenceStatus != ACTIVE && !licence.isReviewNeeded }
+    licences.size > 1 -> licences.find { licence -> licence.statusCode != ACTIVE && !licence.isReviewNeeded() }
     else -> licences.first()
   }
 }
