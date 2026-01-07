40cb55d374caef95356058c1a048d01f036815dd
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2026-01-07 17:08:28 +0000

    Upgrading test containers version

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/config/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/config/LocalStackContainer.kt
@@ -2,9 +2,9 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.config
 
 import org.slf4j.LoggerFactory
 import org.springframework.test.context.DynamicPropertyRegistry
-import org.testcontainers.containers.localstack.LocalStackContainer
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.localstack.LocalStackContainer
 import org.testcontainers.utility.DockerImageName
 import java.io.IOException
 import java.net.ServerSocket
@@ -19,7 +19,7 @@ object LocalStackContainer {
   val instance by lazy { startLocalstackIfNotRunning() }
 
   fun setLocalStackProperties(localStackContainer: LocalStackContainer, registry: DynamicPropertyRegistry) {
-    val localstackUrl = localStackContainer.getEndpointOverride(LocalStackContainer.Service.SNS).toString()
+    val localstackUrl = localStackContainer.endpoint
     val region = localStackContainer.region
     registry.add("hmpps.sqs.localstackUrl") { localstackUrl }
     registry.add("hmpps.sqs.region") { region }
@@ -31,7 +31,7 @@ object LocalStackContainer {
     return LocalStackContainer(
       DockerImageName.parse("localstack/localstack").withTag("3"),
     ).apply {
-      withServices(LocalStackContainer.Service.SQS, LocalStackContainer.Service.SNS)
+      withServices("sqs", "sns")
       withEnv("DEFAULT_REGION", "eu-west-2")
       waitingFor(
         Wait.forLogMessage(".*Ready.*", 1),
