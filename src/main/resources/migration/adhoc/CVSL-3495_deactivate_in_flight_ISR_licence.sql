005d42e7abbdcae72844b600ed6a23f44b1990c7
AledWEvans <aled.evans1@justice.gov.uk>
2026-01-26 11:57:12 +0000

    CVSL-3495 CVL PROGRESSION - Licence Deactivate for In Progress, Submitted and Approved

---
+++ src/main/resources/migration/adhoc/CVSL-3495_deactivate_in_flight_ISR_licence.sql
@@ -0,0 +1,23 @@
+/*
+
+	ACCEPTANCE CRITERIA
+		AC1 -  Deactivate licences IN PROGRESS, SUBMITTED, APPROVED
+		GIVEN a licence in case list is IN PROGRESS
+		OR SUBMITTED OR APPROVED
+		AND the Release Date is after the Progression model live date (TBC)
+		WHEN The new Version 4 policy is applicable
+		THEN Deactivate the licence
+
+	The progression_model_live_date (TBC) needs to be before this is run format on data param YYYY-MM-DD
+*/
+
+BEGIN;
+
+	UPDATE public.licence l
+		SET status_code = 'INACTIVE',
+			date_last_updated = CURRENT_TIMESTAMP
+			WHERE l.status_code IN ('IN_PROGRESS', 'SUBMITTED', 'APPROVED')
+			  AND l.actual_release_date > :progression_model_live_date
+			  AND l."version" = '4.0';
+
+COMMIT;
