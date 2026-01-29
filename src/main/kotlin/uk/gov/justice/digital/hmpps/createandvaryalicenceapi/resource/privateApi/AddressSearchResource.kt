9ea97a0936e3cb0956b1d48dd97a9ff835a51dee
dave-virden <151018159+dave-virden@users.noreply.github.com>
2026-01-29 12:59:37 +0000

    CVSL-3667: Add POST address search endpoint
    Add a new POST endpoint to better handle queries with / and other
    breaking characters. Deprecate the existing GET endpoint which will be
    removed once the push endpoint is in use in all environments.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/AddressSearchResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/AddressSearchResource.kt
@@ -7,11 +7,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import jakarta.validation.constraints.Size
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.config.ErrorResponse
@@ -24,6 +27,7 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.addressSear
 @Tag(name = Tags.ANCILLARY)
 class AddressSearchResource(private val addressSearchService: AddressSearchService) {
 
+  @Deprecated("Use POST /address/search/by/text/ with request body instead")
   @GetMapping("/address/search/by/text/{searchQuery}")
   @PreAuthorize("hasAnyRole('CVL_ADMIN')")
   @Operation(
@@ -70,6 +74,61 @@ class AddressSearchResource(private val addressSearchService: AddressSearchServi
     @PathVariable(name = "searchQuery")
     searchQuery: String,
   ): List<AddressSearchResponse> = addressSearchService.searchForAddressesByText(searchQuery)
+
+  @PostMapping("/address/search/by/text/")
+  @PreAuthorize("hasAnyRole('CVL_ADMIN')")
+  @Operation(
+    summary = "Searches for addresses that match the given search text",
+    description = "Searches for addresses that match the given search text",
+    security = [SecurityRequirement(name = "ROLE_CVL_ADMIN")],
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Returns addresses matching the given search text",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = AddressSearchResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad Request, invalid input data",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  fun searchForAddressesWithPost(
+    @Valid @RequestBody addressSearchRequest: AddressSearchRequest,
+  ): List<AddressSearchResponse> = addressSearchService.searchForAddressesByText(addressSearchRequest.searchQuery)
 
   @GetMapping("/address/search/by/reference/{reference}")
   @PreAuthorize("hasAnyRole('CVL_ADMIN')")
--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/address/AddressSearchResourceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/address/AddressSearchResourceIntegrationTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import org.springframework.http.MediaType
 import org.springframework.test.json.JsonCompareMode
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.OsPlacesMockServer
@@ -14,6 +15,7 @@ import java.nio.charset.StandardCharsets
 private const val SEARCH_STRING = "Glan-y-mor"
 private const val REFERENCE = "2345678"
 private const val SEARCH_FOR_ADDRESSES_URL = "/address/search/by/text/$SEARCH_STRING"
+private const val SEARCH_FOR_ADDRESSES_POST_URL = "/address/search/by/text/"
 private const val GET_ADDRESS_BY_REFERENCE_URL = "/address/search/by/reference/$REFERENCE"
 private const val OS_API_KEY = "os-places-api-key"
 
@@ -80,6 +82,105 @@ class AddressSearchResourceIntegrationTest : IntegrationTestBase() {
       val result = webTestClient.get()
         .uri(SEARCH_FOR_ADDRESSES_URL)
         .headers(setAuthorisation(roles = listOf(role)))
+        .exchange()
+
+      // Then
+
+      result.expectStatus()
+        .isOk
+        .expectBody().json(serializedContent("address-by-search-text"), JsonCompareMode.STRICT)
+    }
+  }
+
+  abstract inner class BaseAddressSearchTestPost(private val urlToTest: String) {
+
+    @Test
+    fun `should return unauthorized if no token`() {
+      webTestClient.post()
+        .uri(urlToTest)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue("""{"searchQuery": "test"}""")
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    @Test
+    fun `should return forbidden if no role`() {
+      webTestClient.post()
+        .uri(urlToTest)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation())
+        .bodyValue("""{"searchQuery": "test"}""")
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `should return forbidden if wrong role`() {
+      webTestClient.post()
+        .uri(urlToTest)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
+        .bodyValue("""{"searchQuery": "test"}""")
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `should return bad request with empty search query`() {
+      webTestClient.post()
+        .uri(urlToTest)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
+        .bodyValue("""{"searchQuery": ""}""")
+        .exchange()
+        .expectStatus()
+        .isBadRequest
+        .expectBody()
+        .jsonPath("$.developerMessage").value<String> {
+          assert(it.contains("Search query length must be more than 0 and no more than 100")) {
+            "Expected userMessage to contain validation error, but was: $it"
+          }
+        }
+    }
+
+    @Test
+    fun `should return bad request with search query that is too long`() {
+      webTestClient.post()
+        .uri(urlToTest)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
+        .bodyValue("""{"searchQuery": "Search query that is way too long and exceeds the maximum allowed length of one hundred characters which is not acceptable"}""")
+        .exchange()
+        .expectStatus()
+        .isBadRequest
+        .expectBody()
+        .jsonPath("$.developerMessage").value<String> {
+          assert(it.contains("Search query length must be more than 0 and no more than 100")) {
+            "Expected userMessage to contain validation error, but was: $it"
+          }
+        }
+    }
+  }
+
+  @Nested
+  inner class SearchForAddressSearchPost : BaseAddressSearchTestPost(SEARCH_FOR_ADDRESSES_POST_URL) {
+
+    @ParameterizedTest(name = "should return addresses with given search text for role {0}")
+    @ValueSource(strings = ["ROLE_CVL_ADMIN"])
+    fun `should return addresses results with given search text`(role: String) {
+      // Given
+      osPlacesMockServer.stubSearchForAddresses(SEARCH_STRING)
+
+      // When
+      val result = webTestClient.post()
+        .uri(SEARCH_FOR_ADDRESSES_POST_URL)
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisation(roles = listOf(role)))
+        .bodyValue("""{"searchQuery": "$SEARCH_STRING"}""")
         .exchange()
 
       // Then
