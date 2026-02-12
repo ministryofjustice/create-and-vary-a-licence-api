513cc801978ec888698bf17558c0bd80f19f9c77
justin gardiner <justin.gardiner@digital.justice.gov.uk>
2026-02-12 15:01:58 +0000

    CVSL-3633: Add a new table to store info on potential hard stop cases… (#1214)
    * CVSL-3633: Add a new table to store info on potential hard stop cases to be checked by a new job.
    
    * CVSL-3633: Formatting changes only.
    
    * CVSL-3633: Rename table. Remove unnecessary status.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/entity/PotentialHardstopCase.kt
@@ -0,0 +1,31 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+enum class PotentialHardstopCaseStatus {
+  PENDING,
+  PROCESSED,
+}
+
+@Entity
+@Table(name = "potential_hardstop_case")
+class PotentialHardstopCase(
+  @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.REFRESH])
+  @JoinColumn(name = "licence_id", nullable = false)
+  val licence: Licence,
+
+  @Enumerated(EnumType.STRING)
+  var status: PotentialHardstopCaseStatus = PotentialHardstopCaseStatus.PENDING,
+
+  var dateCreated: LocalDateTime = LocalDateTime.now(),
+
+  var dateLastUpdated: LocalDateTime = LocalDateTime.now(),
+) : AbstractIdEntity()
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/UpdateSentenceDatesHardStopJobIntegrationTest.kt
@@ -0,0 +1,143 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.context.jdbc.Sql
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.PotentialHardstopCaseStatus
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.GovUkMockServer
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration.wiremock.PrisonApiMockServer
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.AuditEventRepository
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceEventRepository
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceRepository
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.PotentialHardstopCaseRepository
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.domainEvents.OutboundEventsPublisher
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.SentenceDetail
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.workingDays.WorkingDaysService
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
+import java.time.LocalDate
+import kotlin.jvm.optionals.getOrNull
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@TestPropertySource(properties = ["hardstop.deactivation.job.enabled=true"])
+class UpdateSentenceDatesHardStopJobIntegrationTest : IntegrationTestBase() {
+
+  @Autowired
+  lateinit var auditEventRepository: AuditEventRepository
+
+  @Autowired
+  lateinit var licenceEventRepository: LicenceEventRepository
+
+  @Autowired
+  lateinit var licenceRepository: LicenceRepository
+
+  @Autowired
+  lateinit var potentialHardstopCaseRepository: PotentialHardstopCaseRepository
+
+  @MockitoBean
+  private lateinit var eventsPublisher: OutboundEventsPublisher
+
+  @Autowired
+  lateinit var workingDaysService: WorkingDaysService
+
+  @BeforeEach
+  fun setup() {
+    govUkApiMockServer.stubGetBankHolidaysForEnglandAndWales()
+  }
+
+  @Test
+  @Sql(
+    "classpath:test_data/seed-licence-id-2.sql",
+  )
+  fun `Update sentence dates should add an entry to potential hard stop cases when licence was in hard stop period but is no longer`() {
+    prisonApiMockServer.stubGetHdcLatest()
+    prisonApiMockServer.stubGetCourtOutcomes()
+    mockPrisonerSearchResponse(
+      SentenceDetail(
+        conditionalReleaseDate = LocalDate.now(),
+        confirmedReleaseDate = LocalDate.now(),
+        sentenceStartDate = LocalDate.now().minusYears(2),
+        sentenceExpiryDate = LocalDate.now().plusYears(1),
+        licenceExpiryDate = LocalDate.now().plusYears(1),
+        topupSupervisionStartDate = LocalDate.now().plusYears(1),
+        topupSupervisionExpiryDate = LocalDate.now().plusYears(2),
+      ),
+    )
+
+    val result = webTestClient.put()
+      .uri("/licence/id/2/sentence-dates")
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
+      .exchange()
+
+    result.expectStatus().isOk
+    assertThat(auditEventRepository.count()).isEqualTo(1)
+    assertThat(licenceEventRepository.count()).isEqualTo(1)
+
+    val previousLicence = licenceRepository.findById(2L).getOrNull()
+    assertThat(previousLicence?.statusCode).isEqualTo(LicenceStatus.TIMED_OUT)
+    mockPrisonerSearchResponse(
+      SentenceDetail(
+        conditionalReleaseDate = LocalDate.now().plusWeeks(1),
+        confirmedReleaseDate = LocalDate.now().plusWeeks(1),
+        sentenceStartDate = LocalDate.now().minusYears(2),
+        sentenceExpiryDate = LocalDate.now().plusYears(1),
+        licenceExpiryDate = LocalDate.now().plusYears(1),
+        topupSupervisionStartDate = LocalDate.now().plusYears(1),
+        topupSupervisionExpiryDate = LocalDate.now().plusYears(2),
+      ),
+    )
+
+    webTestClient.put()
+      .uri("/licence/id/2/sentence-dates")
+      .accept(MediaType.APPLICATION_JSON)
+      .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
+      .exchange()
+      .expectStatus().isOk
+
+    val currentLicence = licenceRepository.findById(2L).getOrNull()
+    assertThat(currentLicence).isNotNull
+
+    assertThat(auditEventRepository.count()).isEqualTo(2)
+    assertThat(licenceEventRepository.count()).isEqualTo(1)
+    assertThat(currentLicence?.statusCode).isEqualTo(LicenceStatus.TIMED_OUT)
+
+    assertThat(potentialHardstopCaseRepository.count()).isEqualTo(1)
+    val potentialHardstopCase = potentialHardstopCaseRepository.findById(1L).getOrNull()
+    assertThat(potentialHardstopCase).isNotNull()
+    assertThat(potentialHardstopCase?.status).isEqualTo(PotentialHardstopCaseStatus.PENDING)
+  }
+
+  private fun mockPrisonerSearchResponse(sentenceDetail: SentenceDetail) {
+    prisonApiMockServer.stubGetPrisonerDetail("A1234AA", sentenceDetail)
+  }
+
+  private companion object {
+    val prisonApiMockServer = PrisonApiMockServer()
+    val govUkApiMockServer = GovUkMockServer()
+
+    @JvmStatic
+    @BeforeAll
+    fun startMocks() {
+      prisonApiMockServer.start()
+      prisonApiMockServer.stubGetSentenceAndRecallTypes(123456)
+      govUkApiMockServer.start()
+    }
+
+    @JvmStatic
+    @AfterAll
+    fun stopMocks() {
+      prisonApiMockServer.stop()
+      govUkApiMockServer.stop()
+    }
+  }
+}
