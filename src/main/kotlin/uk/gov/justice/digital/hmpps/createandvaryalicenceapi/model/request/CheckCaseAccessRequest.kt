4609db1219616075427199c477808068af39d54b
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2026-02-24 12:41:10 +0000

    CVSL-3780: Fix schema doc.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/request/CheckCaseAccessRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/request/CheckCaseAccessRequest.kt
@@ -2,7 +2,11 @@ package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "Request object for updating / creating OMU email contact")
+@Schema(
+  description =
+  """"Request object for check whether a user has access to a case. One of crn, nomisId or licenceId must be provided.
+      If multiple are provided the first provided will be used to check case access in this order, crd, nomisId, licenceId.""",
+)
 data class CheckCaseAccessRequest(
   @field:Schema(description = "The case reference number", example = "X12444")
   val crn: String? = null,
