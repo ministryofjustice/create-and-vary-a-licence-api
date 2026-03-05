634af7dce71d6c38fe126a6c03eb1b7ea96383b9
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2026-03-05 13:32:53 +0000

    CVSL-3820 alias-name-fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/hdc/CurrentPrisonerHdcStatus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/hdc/CurrentPrisonerHdcStatus.kt
@@ -1,7 +1,11 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.hdc
 
+import com.fasterxml.jackson.annotation.JsonAlias
+
 data class CurrentPrisonerHdcStatus(
   override val bookingId: Long,
+
+  @JsonAlias("status")
   override val hdcStatus: HdcStatus,
 ) : HdcStatusHolder {
   override fun isHdcRelease() = hdcStatus != HdcStatus.NOT_A_HDC_RELEASE
