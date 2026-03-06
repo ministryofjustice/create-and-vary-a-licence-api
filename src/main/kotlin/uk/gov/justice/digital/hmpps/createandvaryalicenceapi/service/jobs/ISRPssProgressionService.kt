e81d660201c64dc254fce3b1bcfb62999881a75d
AledWEvans <aled.evans1@justice.gov.uk>
2026-03-06 15:49:43 +0000

    CVSL-3879 Progression PSS - Remove Active PSS after 30/04 (Schedule to run 01/05/26)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/ISRPssProgressionService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/ISRPssProgressionService.kt
@@ -6,6 +6,7 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceType
 import java.time.Clock
 import java.time.LocalDate
 import java.time.LocalDateTime
+import kotlin.collections.chunked
 
 @Service
 class ISRPssProgressionService(
@@ -14,7 +15,28 @@ class ISRPssProgressionService(
   private val clock: Clock = Clock.systemDefaultZone(),
 ) {
 
-  fun processApPssLicences() {
+  fun processActiveApPssAndPssLicences() {
+    // There will be around 530 licences in prod of PSS type
+    val activePSSLicences = repository.findActiveLicenceIds(LicenceType.PSS.toString())
+
+    log.info("ISR PSS progression found {} PSS Active licences to process", activePSSLicences.size)
+    // Cut the apPssLicenceIds into batches/chunks to allow smaller transaction sizes
+    activePSSLicences.chunked(BATCH_SIZE).forEach {
+      processActivePssLicenceChunkSafely(it)
+    }
+
+    // There will be around 15177 licenses in prod or AP PSS type
+    val activeApPSSLicences = repository.findActiveLicenceIds(LicenceType.AP_PSS.toString())
+
+    log.info("ISR AP_PSS progression found {} AP_PSS Active licences to process", activePSSLicences.size)
+
+    // Cut the apPssLicenceIds into batches/chunks to allow smaller transaction sizes
+    activeApPSSLicences.chunked(BATCH_SIZE).forEach {
+      processActiveApPssLicenceChunkSafely(it)
+    }
+  }
+
+  fun processInFlightApPssLicences() {
     if (getCurrentDateAndTime().isAfter(CUTOFF_EXECUTION_DEADLINE)) {
       log.info(
         "ISR PSS progression job skipped because cutoff execution deadline {} has passed",
@@ -23,7 +45,7 @@ class ISRPssProgressionService(
       return
     }
 
-    val apPssLicenceIds = repository.findLicenceIds(
+    val apPssLicenceIds = repository.findInFlightLicenceIds(
       CUTOFF_DATE,
       LicenceType.AP_PSS.toString(),
     )
@@ -31,18 +53,38 @@ class ISRPssProgressionService(
     log.info("ISR PSS progression found {} AP PSS licences to process", apPssLicenceIds.size)
     // Cut the apPssLicenceIds into batches/chunks to allow smaller transaction sizes
     apPssLicenceIds.chunked(BATCH_SIZE).forEach {
-      processChunkSafely(it)
+      processApPssInFlightLicenceChunkSafely(it)
     }
   }
 
   private fun getCurrentDateAndTime(): LocalDateTime = LocalDateTime.now(clock)
 
   @Suppress("TooGenericExceptionCaught")
-  private fun processChunkSafely(chunkLicenceIds: List<Long>) {
+  private fun processApPssInFlightLicenceChunkSafely(chunkLicenceIds: List<Long>) {
     try {
-      chunkService.processApPssLicenceChunk(chunkLicenceIds)
+      chunkService.processApPssInFlightLicenceChunk(chunkLicenceIds)
     } catch (ex: Exception) {
-      log.error("ISR PSS progression chunk failed for chunkLicenceIds: {}", chunkLicenceIds, ex)
+      log.error("ISR AP_PSS repealed in flight licence chunk failed for chunkLicenceIds: {}", chunkLicenceIds, ex)
+      throw ex
+    }
+  }
+
+  @Suppress("TooGenericExceptionCaught")
+  private fun processActivePssLicenceChunkSafely(chunkLicenceIds: List<Long>) {
+    try {
+      chunkService.processActivePssLicenceChunkSafely(chunkLicenceIds)
+    } catch (ex: Exception) {
+      log.error("ISR PSS repealed active progression chunk failed for chunkLicenceIds: {}", chunkLicenceIds, ex)
+      throw ex
+    }
+  }
+
+  @Suppress("TooGenericExceptionCaught")
+  private fun processActiveApPssLicenceChunkSafely(chunkLicenceIds: List<Long>) {
+    try {
+      chunkService.processActiveApPssLicenceChunkSafely(chunkLicenceIds)
+    } catch (ex: Exception) {
+      log.error("ISR AP_PSS repealed Active progression chunk failed for chunkLicenceIds: {}", chunkLicenceIds, ex)
       throw ex
     }
   }
