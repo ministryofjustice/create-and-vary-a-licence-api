2ae71a3adf73371c5c0bc9dbf7f19cd8ec353d51
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2026-03-10 11:01:11 +0000

    CVSL-3880-Prog-HDC-Create-First-night-curfew-endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/entity/CurfewTimes.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/entity/CurfewTimes.kt
@@ -14,7 +14,7 @@ import java.time.LocalTime
 
 @Entity
 @Table(name = "curfew_times")
-data class CurfewTimes(
+class CurfewTimes(
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @field:Positive
