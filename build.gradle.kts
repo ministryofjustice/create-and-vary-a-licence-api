566847e4c423faa5d261ec33520b184ec7c4ab85
Louise Norris <louise.norris@digital.justice.gov.uk>
2026-03-25 15:46:26 +0000

    CVSL-3951 spring boot update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.1.0"
   id("org.owasp.dependencycheck") version "12.2.0"
   kotlin("plugin.spring") version "2.3.10"
   kotlin("plugin.jpa") version "2.3.10"
