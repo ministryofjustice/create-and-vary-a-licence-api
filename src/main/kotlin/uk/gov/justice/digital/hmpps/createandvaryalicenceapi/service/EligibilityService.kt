5678b12f08ae8240ad7b958b524eca486aedf79a
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2026-03-25 10:58:52 +0000

    Revert "CVSL-3912 filter out prisoners without a booking id when getting elig…" (#1314)
    This reverts commit fe105f066a2e626658781556619415ee03967cbb.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/EligibilityService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/EligibilityService.kt
@@ -30,9 +30,8 @@ class EligibilityService(
   }
 
   fun getEligibilityAssessments(prisoners: List<PrisonerSearchPrisoner>): Map<String, EligibilityAssessment> {
-    val prisonersWithBookingIds = prisoners.filter { it.bookingId != null }
-    val hdcStatuses = hdcService.getHdcStatus(prisonersWithBookingIds)
-    val nomisIdsToEligibilityAssessments = prisonersWithBookingIds.map { prisoner ->
+    val hdcStatuses = hdcService.getHdcStatus(prisoners)
+    val nomisIdsToEligibilityAssessments = prisoners.map { prisoner ->
       val isExpectedHdcRelease = hdcStatuses.isExpectedHdcRelease(prisoner.bookingId!!.toLong())
       val genericIneligibilityReasons = getGenericIneligibilityReasons(prisoner)
       val crdIneligibilityReasons = getCrdIneligibilityReasons(prisoner, isExpectedHdcRelease)
