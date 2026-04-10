ae2485991e7d8afaa6a4d749e9d737cce791a64d
dave-virden <151018159+dave-virden@users.noreply.github.com>
2026-04-10 13:31:04 +0100

    CVSL-3797 Renamed recall type to differentiate between PRRD Recall Type (#1338)
    * CVSL-3797 Renamed recall type to differentiate between PRRD Recall Type
    
    * fix lint]

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/TestData.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/TestData.kt
@@ -41,9 +41,9 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.Offe
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.PrisonApiPrisoner
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.PrisonerHdcStatus
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.PrisonerSearchPrisoner
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.RecallType
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.SentenceAndRecallType
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.SentenceDetail
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.SentenceRecallType
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.CaseloadResult
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.CommunityManager
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.probation.CommunityManagerWithoutUser
@@ -835,12 +835,12 @@ object TestData {
     ),
   )
 
-  fun aSentenceAndRecallType(recallType: RecallType = aRecallType()) = SentenceAndRecallType(
+  fun aSentenceAndRecallType(sentenceRecallType: SentenceRecallType = aRecallType()) = SentenceAndRecallType(
     sentenceType = "A_SENTENCE",
-    recallType,
+    sentenceRecallType,
   )
 
-  fun aRecallType(isStandardRecall: Boolean = false, isFixedTermRecall: Boolean = true) = RecallType(
+  fun aRecallType(isStandardRecall: Boolean = false, isFixedTermRecall: Boolean = true) = SentenceRecallType(
     recallName = "A_RECALL",
     isStandardRecall,
     isFixedTermRecall,
