cf3d53f13b3ee50d38da807145c43e750c055724
AledWEvans <aled.evans1@justice.gov.uk>
2026-04-17 09:18:01 +0100

    test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/migration/MigrationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/migration/MigrationService.kt
@@ -81,7 +81,7 @@ class MigrationService(
     val licence = HdcLicence(
       // Hard coded values
       version = "3.0",
-      licenceVersion = "1.0", // talk to team about this later!
+      licenceVersion = "1.0",
       // Main states
       typeCode = licence.typeCode,
       statusCode = LicenceStatus.ACTIVE,
