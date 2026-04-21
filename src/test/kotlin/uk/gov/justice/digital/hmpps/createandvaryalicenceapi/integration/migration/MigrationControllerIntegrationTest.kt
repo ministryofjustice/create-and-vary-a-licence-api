face804bd591a505dff8de54af037a4e7211b0c4
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-04-21 10:51:27 +0100

    CVSL-3836 Prog HDC - Migration - Migrate an Active case - homeDetentionCurfewEligibilityDate

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/migration/MigrationControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/migration/MigrationControllerIntegrationTest.kt
@@ -244,6 +244,7 @@ class MigrationControllerIntegrationTest : IntegrationTestBase() {
       licenceActivationDate = LocalDate.parse("2025-05-03"),
       homeDetentionCurfewActualDate = LocalDate.parse("2025-05-04"),
       homeDetentionCurfewEndDate = LocalDate.parse("2025-06-05"),
+      homeDetentionCurfewEligibilityDate = LocalDate.parse("2025-06-06"),
       licenceExpiryDate = LocalDate.parse("2026-05-06"),
       licenceVersion = 1,
       varyVersion = 2,
