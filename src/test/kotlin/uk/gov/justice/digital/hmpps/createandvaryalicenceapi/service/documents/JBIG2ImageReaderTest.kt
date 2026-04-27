198bff6d5fb831b2346329c0235757f3f7974908
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-04-27 16:50:20 +0100

    CVSL-4022 CVL | Cannot read exclusion zone uploads using JBIG2 image
    Have generated a simple PDF and verifies JBIG2 compression for PDFBox testing - see ticket for full script!

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/documents/JBIG2ImageReaderTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/documents/JBIG2ImageReaderTest.kt
@@ -1,7 +1,9 @@
-package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.documents
-
+import org.apache.pdfbox.Loader
+import org.apache.pdfbox.rendering.PDFRenderer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.awt.image.BufferedImage
+import java.io.File
 
 class JBIG2ImageReaderTest {
 
@@ -15,5 +17,33 @@ class JBIG2ImageReaderTest {
 
     // Then
     assertThat(clazz).isNotNull
+  }
+
+  /**
+   * This test is to ensure that the PDFBox JBIG2 plugin is working correctly.
+   *
+   *  Generated a simple PDF and verifies JBIG2 compression - see ticket CVSL-4022 for full script!
+   * Uses ImageMagick (image creation), Ghostscript (PDF generation), and Poppler (JBIG2 verification)
+   *
+   */
+  @Test
+  fun shouldRenderPdfWithoutThrowing() {
+    // Given
+    val url =
+      requireNotNull(
+        javaClass.getResource("/test_data/jbig2.pdf"),
+      ) { "Test PDF not found on classpath" }
+
+    val file = File(url.toURI())
+
+    // When
+    Loader.loadPDF(file).use { document ->
+      val renderer = PDFRenderer(document)
+      val image: BufferedImage = renderer.renderImageWithDPI(0, 150f)
+
+      // Then
+      assertThat(image.width).isGreaterThan(0)
+      assertThat(image.height).isGreaterThan(0)
+    }
   }
 }
