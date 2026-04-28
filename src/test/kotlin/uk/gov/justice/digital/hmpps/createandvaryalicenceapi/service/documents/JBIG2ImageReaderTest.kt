000c1b88db78a0f16e127f10fddc1e00741e75ba
Aled Evans <Aled.Evans1@justice.gov.uk>
2026-04-28 08:32:22 +0100

    Cvsl 4022 cvl | cannot read exclusion zone uploads using jbig2 image (#1378)
    * CVSL-4022 CVL | Cannot read exclusion zone uploads using JBIG2 image
    
    * CVSL-4022 CVL | Cannot read exclusion zone uploads using JBIG2 image
    
    Have generated a simple PDF and verifies JBIG2 compression for PDFBox testing - see ticket for full script!
    
    * CVSL-4022 CVL | Cannot read exclusion zone uploads using JBIG2 image
    
    Have generated a simple PDF and verifies JBIG2 compression for PDFBox testing - see ticket for full script!
    
    * CVSL-4022 CVL | Cannot read exclusion zone uploads using JBIG2 image
    
    Have generated a simple PDF and verifies JBIG2 compression for PDFBox testing - see ticket for full script!
    
    * CVSL-4022 CVL | Cannot read exclusion zone uploads using JBIG2 image
    
    Have generated a simple PDF and verifies JBIG2 compression for PDFBox testing - see ticket for full script!

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/documents/JBIG2ImageReaderTest.kt
@@ -0,0 +1,45 @@
+import org.apache.pdfbox.Loader
+import org.apache.pdfbox.rendering.PDFRenderer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class JBIG2ImageReaderTest {
+
+  @Test
+  fun jbig2PluginShouldBeOnClasspath() {
+    // Given
+    val className = "org.apache.pdfbox.jbig2.JBIG2ImageReader"
+
+    // When
+    val clazz = Class.forName(className)
+
+    // Then
+    assertThat(clazz).isNotNull
+  }
+
+  /**
+   * This test is to ensure that the PDFBox JBIG2 plugin is working correctly.
+   *
+   *  Generated a simple PDF and verifies JBIG2 compression - see ticket CVSL-4022 for full script!
+   * Uses ImageMagick (image creation), Ghostscript (PDF generation), and Poppler (JBIG2 verification)
+   *
+   */
+  @Test
+  fun shouldRenderPdfWithoutThrowing() {
+    // Given
+    val file = requireNotNull(
+      javaClass.getResource("/test_data/jbig2.pdf"),
+    ) { "Test PDF not found on classpath" }
+      .toURI()
+      .let(::File)
+
+    // When
+    val image = Loader.loadPDF(file).use { document ->
+      PDFRenderer(document).renderImageWithDPI(0, 150f)
+    }
+
+    // Then
+    assertThat(image.width).isGreaterThan(0)
+  }
+}
