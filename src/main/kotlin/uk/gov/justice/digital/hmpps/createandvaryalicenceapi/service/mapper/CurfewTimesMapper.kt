5e16576b17ec54e40d91b0a84ca444b987c71478
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-05-12 15:25:20 +0100

    CVSL-4036 Reinstate HDC variations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/mapper/CurfewTimesMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/mapper/CurfewTimesMapper.kt
@@ -1,28 +1,23 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.mapper
 
-import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.entity.CurfewTimes
 
-@Component
-class CurfewTimesMapper {
+object CurfewTimesMapper {
+  fun copy(entity: CurfewTimes?): CurfewTimes? {
+    if (entity == null) return null
 
-  companion object {
-    fun copy(entity: CurfewTimes?): CurfewTimes? {
-      if (entity == null) return null
-
-      return CurfewTimes(
-        id = null,
-        fromDay = entity.fromDay,
-        fromTime = entity.fromTime,
-        untilDay = entity.untilDay,
-        untilTime = entity.untilTime,
-        curfewTimesSequence = entity.curfewTimesSequence,
-      )
-    }
-
-    fun copyList(entities: List<CurfewTimes>?): MutableList<CurfewTimes> = entities
-      ?.mapNotNull { copy(it) }
-      ?.toMutableList()
-      ?: mutableListOf()
+    return CurfewTimes(
+      id = null,
+      fromDay = entity.fromDay,
+      fromTime = entity.fromTime,
+      untilDay = entity.untilDay,
+      untilTime = entity.untilTime,
+      curfewTimesSequence = entity.curfewTimesSequence,
+    )
   }
+
+  fun copyList(entities: List<CurfewTimes>?): MutableList<CurfewTimes> = entities
+    ?.mapNotNull { copy(it) }
+    ?.toMutableList()
+    ?: mutableListOf()
 }
