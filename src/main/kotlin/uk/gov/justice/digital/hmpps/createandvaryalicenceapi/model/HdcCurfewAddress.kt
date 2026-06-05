b4942d9b06c33c0cfdb4291e040a9aba5289c67d
Aled Evans <101728271+AledWEvans@users.noreply.github.com>
2026-06-05 09:58:31 +0100

    Code has several issues with it, had to refactor structure - removed old json parameters

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/HdcCurfewAddress.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/model/HdcCurfewAddress.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Describes a curfew address on a HDC licence")
