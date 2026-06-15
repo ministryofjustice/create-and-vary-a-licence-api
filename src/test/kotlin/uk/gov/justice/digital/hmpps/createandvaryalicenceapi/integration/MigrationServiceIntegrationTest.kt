0b267605da5960047105381ef99270125acd2cbf
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2026-06-15 16:09:13 +0100

    CVSL-4106 Adding migration test

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/MigrationServiceIntegrationTest.kt
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.jdbc.Sql
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.migration.repository.MigrationRepository
+
+class MigrationServiceIntegrationTest : IntegrationTestBase() {
+
+  @Autowired
+  lateinit var repository: MigrationRepository
+
+  @Test
+  @Sql("classpath:test_data/seed-an-hdc-migration.sql")
+  fun `isAMigratedLicence returns true for migrated licences`() {
+    assertThat(repository.isAMigratedLicence(1)).isTrue()
+  }
+
+  @Test
+  @Sql("classpath:test_data/seed-an-hdc-migration.sql")
+  fun `isAMigratedLicence returns false for non-migrated licences`() {
+    assertThat(repository.isAMigratedLicence(2)).isFalse()
+  }
+}
---
+++ src/test/resources/test_data/seed-an-hdc-migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO hdc_migration_meta_data (id, licence_id, hdc_licence_version_id, licence_version, vary_version)
+VALUES (1, 1, 1, 1, 0);
