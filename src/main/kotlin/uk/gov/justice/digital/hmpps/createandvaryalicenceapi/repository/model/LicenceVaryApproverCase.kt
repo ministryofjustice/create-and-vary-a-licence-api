b2da93c1327cf9258696a8e08c7b065d71e2da9b
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2026-07-01 16:09:58 +0100

    CVSL-4085 Adding release date label to PDU Head Caseload

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/model/LicenceVaryApproverCase.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/repository/model/LicenceVaryApproverCase.kt
@@ -1,15 +1,30 @@
 package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.model
 
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceKind
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceStatus
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.util.LicenceType
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 class LicenceVaryApproverCase(
-  val licenceId: Long,
   val crn: String,
-  val licenceStartDate: LocalDate?,
-  val prisonNumber: String?,
-  val comUsername: String?,
   var typeCode: LicenceType?,
   var dateCreated: LocalDateTime?,
-)
+  override val kind: LicenceKind,
+  override val licenceId: Long,
+  override val versionOfId: Long?,
+  override val statusCode: LicenceStatus,
+  override val surname: String?,
+  override val forename: String?,
+  override val updatedByFirstName: String?,
+  override val updatedByLastName: String?,
+  override val sentenceStartDate: LocalDate?,
+  override val conditionalReleaseDate: LocalDate?,
+  override val actualReleaseDate: LocalDate?,
+  override val postRecallReleaseDate: LocalDate?,
+  override val licenceStartDate: LocalDate?,
+  override val prisonNumber: String?,
+  override val comUsername: String?,
+  override val homeDetentionCurfewActualDate: LocalDate?,
+  override val homeDetentionCurfewEligibilityDate: LocalDate?,
+): LicenceCase
