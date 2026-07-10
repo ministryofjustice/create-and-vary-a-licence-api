895f40f993a7d2f2b33cdc2dbceb27f2ca73e425
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2026-07-10 16:41:00 +0100

    CVSL-4336 Removing mistaken log statements

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/LicencePolicyIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/LicencePolicyIntegrationTest.kt
@@ -63,7 +63,7 @@ class LicencePolicyIntegrationTest : IntegrationTestBase() {
 
   @Test
   fun currentPolicy() {
-    val result = webTestClient.get()
+    webTestClient.get()
       .uri("/licence-policy/active?licenceStartDate=2026-02-18")
       .accept(MediaType.APPLICATION_JSON)
       .headers(setAuthorisation(roles = listOf("ROLE_CVL_ADMIN")))
@@ -72,7 +72,5 @@ class LicencePolicyIntegrationTest : IntegrationTestBase() {
       .expectHeader().contentType(MediaType.APPLICATION_JSON)
       .expectBody()
       .json(policy("V4"), STRICT)
-
-    println(result)
   }
 }
