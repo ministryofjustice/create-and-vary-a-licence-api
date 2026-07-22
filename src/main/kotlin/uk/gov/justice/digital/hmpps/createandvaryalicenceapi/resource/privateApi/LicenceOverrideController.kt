ee3f6935f4182d49cafd6c9c4951a45156d9bb74
Nishanth <129746957+nishanthmahasamudram@users.noreply.github.com>
2026-07-22 12:45:08 +0100

    Cvsl 4153 remove licence type override functionality (#1554)
    * CVSL-4153 Remove licence type override functionality
    
    * CVSL-4153 Remove licence type override functionality
    
    ---------
    
    Co-authored-by: Andrew Lee <1517745+andrewrlee@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/LicenceOverrideController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/resource/privateApi/LicenceOverrideController.kt
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ProblemDetail
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -24,18 +23,13 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.config.ErrorRespons
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request.OverrideLicenceDatesRequest
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request.OverrideLicencePrisonerDetailsRequest
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request.OverrideLicenceStatusRequest
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.model.request.OverrideLicenceTypeRequest
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.resource.Tags
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.support.LicenceOverrideService
-import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.support.LicenceTypeOverrideService
 
 @Tag(name = Tags.ADMIN)
 @RestController
 @RequestMapping("/licence/id/{licenceId}/override", produces = [MediaType.APPLICATION_JSON_VALUE])
-class LicenceOverrideController(
-  private val licenceOverrideService: LicenceOverrideService,
-  private val licenceTypeOverrideService: LicenceTypeOverrideService,
-) {
+class LicenceOverrideController(private val licenceOverrideService: LicenceOverrideService) {
   @PostMapping(value = ["/status"])
   @PreAuthorize("hasAnyRole('CVL_ADMIN')")
   @ResponseBody
@@ -126,52 +120,6 @@ class LicenceOverrideController(
     request: OverrideLicenceDatesRequest,
   ) {
     licenceOverrideService.changeDates(licenceId, request)
-  }
-
-  @PostMapping(value = ["/type"])
-  @PreAuthorize("hasAnyRole('CVL_ADMIN')")
-  @ResponseBody
-  @ResponseStatus(HttpStatus.ACCEPTED)
-  @Operation(
-    summary = "Override a licence type",
-    description = "Override the type for an exising licence. Only to be used in exceptional circumstances." +
-      " Requires ROLE_CVL_ADMIN.",
-    security = [SecurityRequirement(name = "ROLE_CVL_ADMIN")],
-  )
-  @ApiResponses(
-    value = [
-      ApiResponse(
-        responseCode = "202",
-        description = "Status has been updated",
-      ),
-      ApiResponse(
-        responseCode = "400",
-        description = "Bad request, request body must be valid",
-        content = [Content(mediaType = "application/json", schema = Schema(implementation = ProblemDetail::class))],
-      ),
-      ApiResponse(
-        responseCode = "401",
-        description = "Unauthorised, requires a valid Oauth2 token",
-        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
-      ),
-      ApiResponse(
-        responseCode = "403",
-        description = "Forbidden, requires an appropriate role",
-        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
-      ),
-      ApiResponse(
-        responseCode = "404",
-        description = "The licence for this ID was not found.",
-        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
-      ),
-    ],
-  )
-  fun changeType(
-    @PathVariable("licenceId") licenceId: Long,
-    @RequestBody @Valid
-    request: OverrideLicenceTypeRequest,
-  ) {
-    licenceTypeOverrideService.changeType(licenceId, request.licenceType, request.reason)
   }
 
   @PostMapping(value = ["/prisoner-details"])
