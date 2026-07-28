e6ecd60f70d1e307a415a91a2132739f857a4e63
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-07-28 14:51:29 +0100

    CVSL-4357 fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/policies/LicencePolicyService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/policies/LicencePolicyService.kt
@@ -69,10 +69,9 @@ class LicencePolicyService(
     policies = listOf(POLICY_V1_0, POLICY_V2_0, POLICY_V2_1, POLICY_V3_0, POLICY_V4_0)
   }
 
-  fun currentPolicy(licenceStartDate: LocalDate? = null): LicencePolicy =
-    progressionModelPolicyStartDate?.let { policyStartDate ->
-      if (licenceStartDate?.isOnOrAfter(policyStartDate) == true) POLICY_V4_0 else POLICY_V3_0
-    } ?: POLICY_V3_0
+  fun currentPolicy(licenceStartDate: LocalDate? = null): LicencePolicy = progressionModelPolicyStartDate?.let { policyStartDate ->
+    if (licenceStartDate?.isOnOrAfter(policyStartDate) == true) POLICY_V4_0 else POLICY_V3_0
+  } ?: POLICY_V3_0
 
   fun currentPolicyVersion(licenceStartDate: LocalDate? = null): String = currentPolicy(licenceStartDate).version
 
