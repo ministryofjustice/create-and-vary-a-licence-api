574bb7a908661c538caeb2d2609f065905bd469d
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-07-28 14:53:48 +0100

    CVSL-4357 fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/UpdateSentenceDateService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/UpdateSentenceDateService.kt
@@ -275,14 +275,13 @@ class UpdateSentenceDateService(
   private fun shouldInactivateForPolicyVersionChange(
     licence: Licence,
     dateChanges: DateChanges,
-  ): Boolean {
-    val cutoffDate = progressionModelPolicyStartDate ?: return false
-    return isEligibleForPolicyVersionCheck(licence) &&
+  ): Boolean = progressionModelPolicyStartDate?.let { cutoffDate ->
+    isEligibleForPolicyVersionCheck(licence) &&
       dateChanges.firstOrNull { it.type == LicenceDateType.LSD }
         ?.let { getValidLsdDates(it) }
         ?.let { (oldLsd, newLsd) -> crossesPolicyVersionCutoff(oldLsd, newLsd, cutoffDate) }
         ?: false
-  }
+  } ?: false
 
   private fun isEligibleForPolicyVersionCheck(licence: Licence): Boolean = licence.version == V4_0.version && licence.statusCode in inFlightStatuses
 
