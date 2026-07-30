e43e710e0224baa0b7d36ef9dc1fcbce8a02d19e
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-07-30 08:39:29 +0100

    CVSL-4357 inactivate-V4-for-null-policy-start-date

--- build.gradle.kts
+++ build.gradle.kts
@@ -147,6 +147,7 @@ tasks {
     filter {
       excludeTestsMatching("*.integration.*")
     }
+    jvmArgs("-Xmx2g")
   }
 
   register<Test>("initialiseDatabase", fun Test.() {
@@ -163,6 +164,7 @@ tasks {
     filter {
       includeTestsMatching("*IntegrationTest*")
     }
+    jvmArgs("-Xmx2g")
   }
 
   val sarSnapshotFiles = listOf(
@@ -182,6 +184,7 @@ tasks {
     }
     environment("SAR_GENERATE_ACTUAL", "true")
     outputs.upToDateWhen { false }
+    jvmArgs("-Xmx2g")
   }
 
   register<Copy>("updateSarSnapshots") {
