e7deef757c5fb11fb4a51fad2393474a1f39365c
Gurps Singh <gurpreet.singh@justice.gov.uk>
2026-08-11 17:04:46 +0100

    CVSL-4042 add logic to activation job to activate remand licences

--- src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/LicenceActivationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/jobs/LicenceActivationService.kt
@@ -8,6 +8,8 @@ import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.repository.LicenceR
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.HdcService
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.IS91DeterminationService
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.LicenceService
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.RemandDeterminationService
+import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.TelemetryService
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.PrisonerSearchApiClient
 import uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.prison.PrisonerSearchPrisoner
 import java.time.LocalDate
@@ -24,6 +26,8 @@ class LicenceActivationService(
   private val hdcService: HdcService,
   private val prisonerSearchApiClient: PrisonerSearchApiClient,
   private val iS91DeterminationService: IS91DeterminationService,
+  private val remandDeterminationService: RemandDeterminationService,
+  private val telemetryService: TelemetryService,
 ) {
 
   @Transactional
@@ -36,15 +40,20 @@ class LicenceActivationService(
     val matchedLicences = prisonerSearchApiClient.searchPrisonersByBookingIds(potentialLicences.keys)
       .map { LicenceWithPrisoner(potentialLicences[it.bookingId?.toLong()]!!, it) }
     val (eligibleLicences, ineligibleLicences) = determineActivationEligibility(matchedLicences)
-    val (iS91licencesToActivate, standardLicencesToActivate) = findLicencesToActivate(eligibleLicences)
+    val (iS91licencesToActivate, remandLicencesToActivate, standardLicencesToActivate) = findLicencesToActivate(eligibleLicences)
 
     log.info(
       "Licence activation job: activating ${iS91licencesToActivate.size} IS91 licences, " +
+        "${remandLicencesToActivate.size} remand licences, " +
         "${standardLicencesToActivate.size} standard licences, " +
         "inactivating ${ineligibleLicences.size} licences",
     )
 
     licenceService.activateLicences(iS91licencesToActivate, IS91_LICENCE_ACTIVATION)
+    licenceService.activateLicences(remandLicencesToActivate, REMAND_LICENCE_ACTIVATION)
+    remandLicencesToActivate.forEach { licence ->
+      telemetryService.recordLicenceForPrisonerOnRemandActivatedEvent(licence)
+    }
     licenceService.activateLicences(standardLicencesToActivate, LICENCE_ACTIVATION)
     licenceService.inactivateLicences(ineligibleLicences.map { it.licence }, LICENCE_DEACTIVATION)
   }
@@ -58,18 +67,21 @@ class LicenceActivationService(
     return filteredLicences.partition { hdcStatus.canBeActivated(it.licence.kind, it.bookingId) }
   }
 
-  private fun findLicencesToActivate(licences: List<LicenceWithPrisoner>): Pair<List<Licence>, List<Licence>> {
-    val (iS91Licences, standardLicences) = filterLicencesIntoTypes(licences)
+  private fun findLicencesToActivate(licences: List<LicenceWithPrisoner>): Triple<List<Licence>, List<Licence>, List<Licence>> {
+    val (iS91Licences, remandLicences, standardLicences) = filterLicencesIntoTypes(licences)
     val iS91LicencesToActivate = iS91Licences.filter { isPassedLicenceStartDate(it.licence.licenceStartDate) }
+    val remandLicencesToActivate = remandLicences.filter { isPassedLicenceStartDate(it.licence.licenceStartDate) }
     val standardLicencesToActivate = standardLicences.filter { it.isStandardLicenceForActivation() }
-    return iS91LicencesToActivate.map { it.licence } to standardLicencesToActivate.map { it.licence }
+    return Triple(iS91LicencesToActivate.map { it.licence }, remandLicencesToActivate.map { it.licence }, standardLicencesToActivate.map { it.licence })
   }
 
-  private fun filterLicencesIntoTypes(licences: List<LicenceWithPrisoner>): Pair<List<LicenceWithPrisoner>, List<LicenceWithPrisoner>> {
+  private fun filterLicencesIntoTypes(licences: List<LicenceWithPrisoner>): Triple<List<LicenceWithPrisoner>, List<LicenceWithPrisoner>, List<LicenceWithPrisoner>> {
     val prisoners = licences.map { it.prisoner }
     val iS91RelatedIds = iS91DeterminationService.getIS91AndExtraditionBookingIds(prisoners)
-    val (iS91AndExtraditionLicences, standardLicences) = licences.partition { iS91RelatedIds.contains(it.bookingId) }
-    return iS91AndExtraditionLicences to standardLicences
+    val remandRelatedIds = remandDeterminationService.getRemandBookingIds(prisoners)
+    val (iS91Licences, otherLicences) = licences.partition { iS91RelatedIds.contains(it.bookingId) }
+    val (remandLicences, standardLicences) = otherLicences.partition { remandRelatedIds.contains(it.bookingId) }
+    return Triple(iS91Licences, remandLicences, standardLicences)
   }
 
   private fun LicenceWithPrisoner.isStandardLicenceForActivation(): Boolean = (
@@ -82,6 +94,7 @@ class LicenceActivationService(
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
     const val IS91_LICENCE_ACTIVATION = "IS91 licence automatically activated via repeating job"
+    const val REMAND_LICENCE_ACTIVATION = "Remand licence automatically activated via repeating job"
     const val LICENCE_ACTIVATION = "Licence automatically activated via repeating job"
     const val LICENCE_DEACTIVATION = "Licence automatically deactivated as booking ID has approved HDC licence"
   }
