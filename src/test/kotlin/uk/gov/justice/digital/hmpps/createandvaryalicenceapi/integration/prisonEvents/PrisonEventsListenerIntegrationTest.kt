d31cc92cb83b4428b43ffdcb847dc25e50120ad4
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-09-14 13:08:55 +0100

    CVSL-4494 Allow-CVL-to-reflect-CRD-updates-where-case-is-in-its-HDC-period

--- src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/prisonEvents/PrisonEventsListenerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/integration/prisonEvents/PrisonEventsListenerIntegrationTest.kt
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.extension.RegisterExtension
 import org.mockito.kotlin.any
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
-import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
