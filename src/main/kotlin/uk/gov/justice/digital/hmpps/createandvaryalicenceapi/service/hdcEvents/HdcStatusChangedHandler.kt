75ba284f2d3aa1946554dedfd62dc84290c6aea7
Nishanth <129746957+nishanthmahasamudram@users.noreply.github.com>
2026-09-24 14:49:51 +0100

    CVSL-3945-CVL-Listen-for-HDC-status-change (#1665)
    * CVSL-3945-CVL-Listen-for-HDC-status-change
    
    * CVSL-3945-CVL-Listen-for-HDC-status-change
    
    * CVSL-3945-CVL-Listen-for-HDC-status-change
    
    * CVSL-3945-CVL-Listen-for-HDC-status-change

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/createandvaryalicenceapi/service/hdcEvents/HdcStatusChangedHandler.kt
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.hmpps.createandvaryalicenceapi.service.hdcEvents
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import tools.jackson.databind.ObjectMapper
+
+@Service
+class HdcStatusChangedHandler(
+  private val mapper: ObjectMapper,
+) {
+  companion object {
+    private val log = LoggerFactory.getLogger(HdcStatusChangedHandler::class.java)
+  }
+
+  fun handleOptout(message: String) {
+    val event = mapper.readValue(message, HdcStatusChangedEvent::class.java)
+
+    log.info(
+      "HDC opt-out processed: eventType={} occurredAt={} licenceId={} bookingId={} nomsNumber={} triggeredBy={} reason={}",
+      event.eventType,
+      event.occurredAt,
+      event.licenceId,
+      event.bookingId,
+      event.nomsNumber,
+      event.triggeredBy,
+      event.reason,
+    )
+  }
+}
